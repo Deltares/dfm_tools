2b1d44d2b4a4c7befc3c1aef40dc1b9782cf6426
veenstrajelmer <veenstrajelmer@gmail.com>
2020-02-03 15:57:46 +0100

    added howto_git.txt

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+#requirements = [ ]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup_requirements = ['pytest-runner', ]
 
