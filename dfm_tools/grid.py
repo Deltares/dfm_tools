29d6df79060f80fb59dfa36a6ec825f7d8ad5a78
veenstrajelmer <Jelmer.Veenstra@Deltares.nl>
2020-02-07 21:41:17 +0100

    plotting grids and mapvalues is now possible on certain depths and certain times, still very buggy and a lot left to do

--- dfm_tools/grid.py
+++ dfm_tools/grid.py
@@ -1,139 +1,242 @@
 import numpy as np
 class UGrid:
     """Unstructured grid"""
-    def __init__(self, netnodex, netnodey, netnodez=None, netelemnode=None, *args, **kwargs):
-        self.netnodex = netnodex
-        self.netnodey = netnodey
-        if netnodez is not None:
-            self.netnodez = netnodez
+    def __init__(self, mesh2d_node_x, mesh2d_node_y, mesh2d_face_nodes, verts, mesh2d_node_z=None, *args, **kwargs):
+        self.mesh2d_node_x = mesh2d_node_x
+        self.mesh2d_node_y = mesh2d_node_y
+        self.mesh2d_face_nodes = mesh2d_face_nodes
+        self.verts = verts
+        if mesh2d_node_z is not None:
+            self.mesh2d_node_z = mesh2d_node_z
         else:
-            self.netnodez = np.zeros(self.netnodex.shape)
-        self.netelemnode = netelemnode
+            self.mesh2d_node_z = np.zeros(self.mesh2d_node_x.shape)
     @staticmethod
-    def fromfile(filename):
+    def fromfile(filename_nc):
         import netCDF4
         from dfm_tools.get_varname_mapnc import get_varname_mapnc
-        ds = netCDF4.Dataset(filename)
-        varname_netnodex = get_varname_mapnc(ds,'NetNode_x')
-        varname_netnodey = get_varname_mapnc(ds,'NetNode_y')
-        varname_netnodez = get_varname_mapnc(ds,'NetNode_z')
-        varname_netelemnode = get_varname_mapnc(ds,'NetElemNode')
-        #varname_netlink = get_varname_mapnc(ds,'NetLink')
-        netnodex = ds.variables[varname_netnodex][:]
-        netnodey = ds.variables[varname_netnodey][:]
-        netnodez = ds.variables[varname_netnodez][:]
-        netelemnode = ds.variables[varname_netelemnode][:]
-        #netlink = ds.variables[varname_netlink][:]
-        ds.close()
-        ugrid = UGrid(netnodex, netnodey, netnodez, netelemnode)
+        
+        def nodexyfaces2verts(node_x,node_y, faces):
+            quatrangles = faces-1 #convert 1-based indexing to 0-based indexing
+            #https://stackoverflow.com/questions/49640311/matplotlib-unstructered-quadrilaterals-instead-of-triangles
+            #https://stackoverflow.com/questions/52202014/how-can-i-plot-2d-fem-results-using-matplotlib
+            yz = np.c_[node_x,node_y]
+            verts= yz[quatrangles]
+            verts[quatrangles.mask==True,:] = np.nan #remove all masked values by making them nan
+            return verts
+        
+        data_nc = netCDF4.Dataset(filename_nc)
+
+        mesh2d_node_x = data_nc.variables[get_varname_mapnc(data_nc,'mesh2d_node_x')][:]
+        mesh2d_node_y = data_nc.variables[get_varname_mapnc(data_nc,'mesh2d_node_y')][:]
+        #mesh2d_node_z = data_nc.variables[get_varname_mapnc(data_nc,'mesh2d_node_z')][:]
+        mesh2d_face_nodes = data_nc.variables[get_varname_mapnc(data_nc,'mesh2d_face_nodes')][:, :]
+        verts = nodexyfaces2verts(mesh2d_node_x, mesh2d_node_y, mesh2d_face_nodes) #xy coordinates of face nodes
+
+        #remove ghost cells
+        varn = get_varname_mapnc(data_nc,'mesh2d_flowelem_domain')
+        if varn != []: # domain variable is present, so there are multiple domains
+            domain = data_nc.variables[varn][:]
+            domain_no = np.bincount(domain).argmax() #meest voorkomende domeinnummer
+            verts = verts[domain==domain_no]
+            mesh2d_face_nodes = mesh2d_face_nodes[domain==domain_no]
+        
+        
+        data_nc.close()
+        ugrid = UGrid(mesh2d_node_x, mesh2d_node_y, mesh2d_face_nodes, verts, mesh2d_node_z=None)
         return ugrid
-    @staticmethod
-    def frombmi(bmi):
-        raise NotImplemented('todo')
-    def celltypes(self):
-        return (~self.netelemnode.mask).sum(1)
-    def cellcoords(self):
-        """Create a list of coordinates (xy) per cell"""
-        # Create split locations
-        splitidx = np.cumsum(np.r_[self.celltypes()][:-1])
-        # Convert to 1d filled idx
-        idx = self.netelemnode[(~self.netelemnode.mask)]-1
-        xpoly = np.array(np.split(self.netnodex[idx],splitidx)) # x vector per poly
-        ypoly = np.array(np.split(self.netnodey[idx],splitidx))
-        zpoly = np.array(np.split(self.netnodez[idx],splitidx))
-        cellxycoords = np.concatenate([xpoly[:,np.newaxis],ypoly[:,np.newaxis],zpoly[:,np.newaxis]],1)
-        polycoords = [np.c_[xyz[0],xyz[1], xyz[2]] for xyz in cellxycoords]
-        return polycoords
-    def export(self, drivername='Memory', filename='', epsg=None):
-        """create a file using ogr"""
-        import ogr
-        import osr
-        driver = ogr.GetDriverByName(drivername)
-        datasource = driver.CreateDataSource(filename)
-        wgs = osr.SpatialReference()
-        if epsg is None:
-            def project(*args):
-                return args
-        else:
-            wgs.ImportFromEPSG(4326)
-            rd = osr.SpatialReference()
-            rd.ImportFromEPSG(epsg)
-            transform = osr.CoordinateTransformation(rd, wgs)
-            project = transform.TransformPoint
 
-        # Add fields here, if required...
-        fielddefn = ogr.FieldDefn()
-        fielddefn.SetName('cellid')
-        fielddefn.SetType(ogr.OFTInteger)
-        layer = datasource.CreateLayer('cells', wgs, ogr.wkbPolygon25D)
-        layer.CreateField(fielddefn)
 
-        featuredefn = ogr.FeatureDefn()
-        featuredefn.AddFieldDefn(fielddefn)
-        for i, cell in enumerate(self.cellcoords()):
-            feature = ogr.Feature(featuredefn)
-            linearring = ogr.Geometry(ogr.wkbLinearRing)
-            for point in cell:
-                linearring.AddPoint(*project(point[0], point[1], point[2]))
-            # close ring
-            linearring.AddPoint(*project(point[0], point[1], point[2]))
-            geometry = ogr.Geometry(ogr.wkbPolygon25D)
-            geometry.AddGeometry(linearring)
-            feature.SetGeometry(geometry)
-            feature.SetField(0, i)
-            layer.CreateFeature(feature)
-            feature.Destroy()
-        datasource.SyncToDisk()
-        if drivername == 'Memory':
-            featurestr = ",\n".join([layer.GetFeature(i).ExportToJson() for i in range(layer.GetFeatureCount())])
-            return '{ "type": "FeatureCollection",\n"features": [\n%s\n] }' % (featurestr, )
-        datasource= None
+def get_mapfilelist(file_nc, multipart=None):
+    #get list of mapfiles
+    import re
+    import glob
+    lastpart = file_nc.split('_')[-2]
+    if multipart != False and len(lastpart) == 4 and lastpart.isdigit(): #if part before '_map.nc' is eg '0000'
+        filename_start = re.compile('(.*)_([0-9]+)_map.nc').search(file_nc).group(1)
+        #filename_number = re.compile('(.*)_([0-9]+)_map.nc').search(file_nc).group(2)
+        #file_ncs = [file_nc.replace('_%s_map.nc','_%04d_map.nc'%(filename_number, domain_id)) for domain_id in range(ndomains)]
+        file_ncs = glob.glob('%s*_map.nc'%(filename_start))
+    else:
+        file_ncs = [file_nc]
+    return file_ncs
 
 
 
-def plot_net(file_nc, var_values=None):
-    import netCDF4
-    import matplotlib.pyplot as plt
-    #plt.close('all')
-    import matplotlib.collections
-    #import matplotlib.cm as cm
+def get_mapmodeldata(file_nc, var_values=None, multipart=None, timestep=None, lay=None):
+    from netCDF4 import Dataset
     import numpy as np
-
-    data_nc = netCDF4.Dataset(file_nc)
-    node_x = data_nc.variables['mesh2d_node_x'][:]
-    node_y = data_nc.variables['mesh2d_node_y'][:]
-    faces = data_nc.variables['mesh2d_face_nodes'][:, :]
-    if var_values == None:
-        values = None
-    else:
-        values = data_nc.variables[var_values][0, :]
-    #list(data_nc.variables.keys())
-
-    quatrangles = faces-1 #convert 1-based indexing to 0-based indexing
-    def quatplot(node_x,node_y, quatrangles, values, ax=None, **kwargs):
-        #https://stackoverflow.com/questions/49640311/matplotlib-unstructered-quadrilaterals-instead-of-triangles
-        #https://stackoverflow.com/questions/52202014/how-can-i-plot-2d-fem-results-using-matplotlib
-        if not ax: ax=plt.gca()
-        yz = np.c_[node_x,node_y]
-        verts= yz[quatrangles]
-        verts[quatrangles.mask==True,:] = np.nan #remove all masked values by making them nan
-        pc = matplotlib.collections.PolyCollection(verts, **kwargs)
-        pc.set_array(values)
-        ax.add_collection(pc)
-        ax.autoscale()
-        return pc
+    
+    from dfm_tools.get_varname_mapnc import get_varname_mapnc
+    from dfm_tools.grid import get_mapfilelist
+    
+    if timestep == None:
+        raise Exception('ERROR: paramter timestep not privided')
+    if lay == None:
+        raise Exception('ERROR: paramter lay not privided')
+    
+    #convert timestep to list if it is not already
+    if type(timestep)==int:
+        list_timestep = [timestep]
+    elif type(timestep)==list:
+        list_timestep = timestep
+    elif type(timestep)==range:
+        list_timestep = list(timestep)
+    #TODO: add check for list of ints
+    #TODO: add option for all timesteps
+    #TODO: add option for timesteps by timestamp instead of integer
+    #TODO: add check for minmax timesteps (not lower than 0 and not higher than ntimesteps)
+    #TODO: 0/1-based indexing for timesteps?
+    
+    #convert layer to list if it is not already
+    if type(lay)==int:
+        list_lay = [lay]
+    elif type(lay)==list:
+        list_lay = lay
+    elif type(lay)==range:
+        list_lay = list(lay)
+    #TODO: add check for list of ints
+    #TODO: add option for all layers
+    #TODO: add check for minmax layers (not lower than 0 and not higher than nlayers)
+    #TODO: 0/1-based indexing for layers?
+    
+    #TODO: add retrieval via depth (then dflowutil.mesh can be removed?)
     
     
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
+    file_ncs = get_mapfilelist(file_nc, multipart)
+    for iF, file_nc_sel in enumerate(file_ncs):
+        print('processing mapdata from domain %04d of %04d'%(iF, len(file_ncs)-1))
+        data_nc = Dataset(file_nc_sel)
+        #list(data_nc.variables.keys())
+        
+        #values = get_mapmodeldata(data_nc, var_values=var_values, multipart=multipart, timestep=timestep, lay=lay)
+        nc_values = data_nc.variables[var_values]
+        #nc_values_shape = nc_values.shape
+        nc_values_dims = nc_values.dimensions
+        nc_values_ndims = len(nc_values_dims)
+    
 
-    if var_values == None:
-        pc = quatplot(node_x, node_y, quatrangles, values, ax=None, linewidth=0.5, color="crimson", facecolor="None")
-    else:
-        pc = quatplot(node_x, node_y, quatrangles, values, ax=None, linewidth=0.5)#, cmap="jet")
-        fig.colorbar(pc, ax=ax)
-    #plt.plot(node_x,node_y, marker="o", ls="", color="crimson")
-    plt.title('This is the plot for: quad')
-    plt.xlabel('Y Axis')
-    plt.ylabel('Z Axis')
+        #select values
+        if nc_values_ndims == 2:
+            values = nc_values[list_timestep,:]
+        elif nc_values_ndims == 3:
+            values = nc_values[list_timestep,:,list_lay]
+        else:
+            raise Exception('incorrect number of dimensions: %s'%(nc_values_ndims))
+        
+        if iF == 0:
+            #setup initial array
+            if nc_values_ndims == 2:
+                values_all = np.ma.empty((len(list_timestep),0))
+                concat_axis = 1
+            elif nc_values_ndims == 3:
+                values_all = np.ma.empty((len(list_timestep),0,len(list_lay)))
+                concat_axis = 1
+            else:
+                raise Exception('incorrect number of dimensions: %s'%(nc_values_ndims))
+
+        #filter ghost cells
+        varn = get_varname_mapnc(data_nc,'mesh2d_flowelem_domain')
+        if varn != []: # domain variable is present, so there are multiple domains
+            domain = data_nc.variables['mesh2d_flowelem_domain'][:]
+            domain_no = np.bincount(domain).argmax() #meest voorkomende domeinnummer
+            nonghost_ids = domain==domain_no
+        
+            values_all = np.ma.concatenate([values_all,values[:,nonghost_ids,:]],axis=concat_axis)
+        else:
+            values_all = np.ma.concatenate([values_all,values],axis=concat_axis)
+    
+    #TODO: add requested times and layers to outputdata        
+    return values_all
+
+
+
+
+
+
+def get_mapnetdata(file_nc, var_values=None, multipart=None, timestep=0, lay=None):
+    from dfm_tools.grid import get_mapfilelist
+
+    file_ncs = get_mapfilelist(file_nc, multipart)
+    #get all data
+    num_nodes = [0]
+    for iF, file_nc_sel in enumerate(file_ncs):
+        print('processing netdata from domain %04d of %04d'%(iF, len(file_ncs)-1))
+        #data_nc = Dataset(file_nc_sel)
+        #list(data_nc.variables.keys())
+        
+        ugrid = UGrid.fromfile(file_nc_sel)
+        node_x = ugrid.mesh2d_node_x
+        node_y = ugrid.mesh2d_node_y
+        faces = ugrid.mesh2d_face_nodes
+        verts = ugrid.verts
+
+        #setup initial array
+        if iF == 0:
+            node_x_all = np.ma.empty((0,))
+            node_y_all = np.ma.empty((0,))
+            verts_all = np.ma.empty((0,verts.shape[1],verts.shape[2]))
+            faces_all = np.ma.empty((0,faces.shape[1]),dtype='int32')
+        
+        #TODO: simplify the two blocks below
+        #increase size of verts_all if too small for verts
+        if verts_all.shape[1] < verts.shape[1]:
+            verts_all_cordimsize = np.ma.zeros((verts_all.shape[0],verts.shape[1],verts_all.shape[2]))
+            verts_all_cordimsize[:,:-1,:] = verts_all
+            verts_all_cordimsize.mask = True
+            verts_all_cordimsize.mask[:,:-1,:] = faces_all.mask
+            faces_all_cordimsize = np.ma.zeros((faces_all.shape[0],faces.shape[1]),dtype='int32')
+            faces_all_cordimsize[:,:-1] = faces_all
+            faces_all_cordimsize.mask = True
+            faces_all_cordimsize.mask[:,:-1] = faces_all.mask
+        else:
+            verts_all_cordimsize = verts_all
+            faces_all_cordimsize = faces_all
+            
+        #increase size of verts if too small for verts_all
+        if verts.shape[1] < verts_all.shape[1]:
+            verts_cordimsize = np.ma.zeros((verts.shape[0],verts_all.shape[1],verts.shape[2]))
+            verts_cordimsize[:,:-1,:] = verts
+            verts_cordimsize.mask = True
+            verts_cordimsize.mask[:,:-1,:] = faces.mask
+            faces_cordimsize = np.ma.zeros((faces.shape[0],faces_all.shape[1]),dtype='int32')
+            faces_cordimsize[:,:-1] = faces
+            faces_cordimsize.mask = True
+            faces_cordimsize.mask[:,:-1] = faces.mask
+        else:
+            verts_cordimsize = verts
+            faces_cordimsize = faces
+
+        #merge all
+        node_x_all = np.ma.concatenate([node_x_all,node_x])
+        node_y_all = np.ma.concatenate([node_y_all,node_y])
+        verts_all = np.ma.concatenate([verts_all_cordimsize,verts_cordimsize])
+        faces_all = np.ma.concatenate([faces_all_cordimsize,faces_cordimsize+np.sum(num_nodes)])
+        num_nodes.append(node_x.shape[0])
+
+        
+    #set all invalid values to the same value (tends to differ between partitions)
+    faces_all.data[faces_all.mask] = -999
+    faces_all.fill_value = -999
+    
+    ugrid_all = UGrid(node_x_all, node_y_all, faces_all, verts_all)
+    
+    return ugrid_all
+
+
+def plot_netmapdata(verts, values=None, ax=None, **kwargs):
+    #https://stackoverflow.com/questions/52202014/how-can-i-plot-2d-fem-results-using-matplotlib
+    #https://stackoverflow.com/questions/49640311/matplotlib-unstructered-quadrilaterals-instead-of-triangles
+    import matplotlib.pyplot as plt
+    import matplotlib.collections
+
+    if not ax: ax=plt.gca()
+    pc = matplotlib.collections.PolyCollection(verts, **kwargs)
+    pc.set_array(values)
+    ax.add_collection(pc)
+    ax.autoscale()
+    return pc
+
+
+
+
 
--- tests/test_dfm_tools.py
+++ tests/test_dfm_tools.py
@@ -58,19 +58,96 @@ def Test_grid_UGrid(self):
     assert 1==1
 
 
-def Test_grid_plotnet(self):
-    from dfm_tools.grid import plot_net
     
-    #plt.close('all')
+    
+    
+def Test_grid_getnetdata_plotnet(self):
+    """
+    this test retrieves grid data and plots it
+    """
+    import matplotlib.pyplot as plt
+    plt.close('all')
+
+    from dfm_tools.grid import get_mapnetdata, plot_netmapdata
+
     file_net = r'n:\My Documents\werkmap\vanJulien_shortmodelfiles\DFM_3D_z_Grevelingen\computations\run01\Grevelingen_FM_grid_20190603_net.nc'
-    file_map = r'n:\My Documents\werkmap\vanJulien_shortmodelfiles\DFM_3D_z_Grevelingen\computations\run01\DFM_OUTPUT_Grevelingen-FM\Grevelingen-FM_0000_map.nc'
+    file_net_rmm = r'p:\11205258-006-kpp2020_rmm-g6\C_Work\01_Rooster\final_totaalmodel\rooster_rmm_v1p5_net.nc'
+
+    print('plot only grid from net.nc')
+    ugrid = get_mapnetdata(file_net)
+    fig, ax = plt.subplots()
+    pc = plot_netmapdata(ugrid.verts, values=None, ax=None, linewidth=0.5, color="crimson", facecolor="None")
+    ax.set_aspect('equal')
+
+    print('plot only grid from net.nc (RMM)')
+    ugrid = get_mapnetdata(file_net_rmm)
+    fig, ax = plt.subplots()
+    pc = plot_netmapdata(ugrid.verts, values=None, ax=None, linewidth=0.5, color="crimson", facecolor="None")
+    ax.set_aspect('equal')
+   
+
+
+def Test_grid_getmapnetdata_getmapmodeldata_plotnetmap(self):
+    """
+    this test retrieves grid data, retrieves map data, and plots it
+    """
+    import matplotlib.pyplot as plt
+    plt.close('all')
     
-    file_nc = file_net
-    plot_net(file_nc)
-    file_nc = file_map
-    plot_net(file_nc,var_values='mesh2d_s1')
+    from dfm_tools.grid import get_mapnetdata, get_mapmodeldata, plot_netmapdata
     
+    file_map8 = r'n:\My Documents\werkmap\vanJulien_shortmodelfiles\DFM_3D_z_Grevelingen\computations\run01\DFM_OUTPUT_Grevelingen-FM\Grevelingen-FM_0000_map.nc'
     
+    print('plot only grid from mapdata')
+    ugrid_all = get_mapnetdata(file_map8)#,multipart=False)
+    fig, ax = plt.subplots()
+    pc = plot_netmapdata(ugrid_all.verts, values=None, ax=None, linewidth=0.5, color="crimson", facecolor="None")
+    ax.set_aspect('equal')
+    
+    print('plot grid and values from mapdata')
+    data_frommap = get_mapmodeldata(file_map8, var_values='mesh2d_sa1', timestep=3, lay=33)#, multipart=False)
+    data_frommap_flat = data_frommap.flatten()
+    fig, ax = plt.subplots()
+    pc = plot_netmapdata(ugrid_all.verts, values=data_frommap_flat, ax=None, linewidth=0.5, cmap="jet")
+    pc.set_clim([28,30.2])
+    fig.colorbar(pc, ax=ax)
+    #plt.plot(node_x,node_y, marker="o", ls="", color="crimson")
+    ax.set_aspect('equal')
+
+    
+
+
+
+def Test_maplora(self):
+    from netCDF4 import Dataset
+    import matplotlib.pyplot as plt
+    plt.close('all')
+    
+    from dfm_tools.grid import get_mapnetdata, plot_netmapdata
+
+    file_maplora = r'p:\11201806-sophie\Oosterschelde\WAQ\r02\postprocessing\oost_tracer_2_map.nc'
+    
+    ugrid_lora = get_mapnetdata(file_maplora)
+
+    data_nc = Dataset(file_maplora)
+    list(data_nc.variables.keys())
+    
+    var_names = ['mesh2d_FColi','mesh2d_HIWAI','mesh2d_Pharma']
+    var_clims = [None,[0,100000000000],[0,10000]]
+    #pc.set_clim(var_clims[0])
+    for var_name, var_clim in zip(var_names, var_clims):
+        data_fromnc = data_nc.variables[var_name]
+        
+        fig, ax = plt.subplots()
+        pc = plot_netmapdata(ugrid_lora.verts, values=data_fromnc, ax=None, linewidth=0.5, cmap="jet")
+        if var_clim != None:
+            pc.set_clim(var_clim)
+        fig.colorbar(pc, ax=ax)
+        ax.set_aspect('equal')
+        ax.set_xlabel(var_name)
+
+    
+
     
     
 @pytest.fixture
