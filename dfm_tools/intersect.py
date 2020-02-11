9767949660f21df069b61a26661d249452c4d996
veenstrajelmer <Jelmer.Veenstra@Deltares.nl>
2020-02-11 21:14:09 +0100

    updates of intersect function, works but should be transfered to function

--- dfm_tools/intersect.py
+++ dfm_tools/intersect.py
@@ -27,26 +27,40 @@ class LineBuilder:
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
 
-
-def nodexyfaces2verts(node_x,node_y, faces):
-    quatrangles = faces-1 #convert 1-based indexing of cell numbering in ugrid to 0-based indexing
-    #https://stackoverflow.com/questions/49640311/matplotlib-unstructered-quadrilaterals-instead-of-triangles
-    #https://stackoverflow.com/questions/52202014/how-can-i-plot-2d-fem-results-using-matplotlib
-    yz = np.c_[node_x,node_y]
-    verts= yz[quatrangles]
-    verts[quatrangles.mask==True,:] = np.nan #remove all masked values by making them nan
-    return verts
-
-
+# Convert lat/lon corrdinates to x/y mercator projection
+def merc(lat, lon):
+    r_major = 6378137.000
+    x = r_major * np.radians(lon)
+    scale = x/lon
+    y = 180.0/np.pi * np.log(np.tan(np.pi/4.0 + lat * (np.pi/180.0)/2.0)) * scale
+    #x = lon
+    #y = lat
+    return (x, y)
+#merc = np.vectorize(merc)
 
 
 from dfm_tools.grid import get_netdata, get_hismapmodeldata, plot_netmapdata
 
 file_map = r'c:\DATA\werkmap\vanJulien_shortmodelfiles\DFM_sigma_curved_bend\DFM_OUTPUT_cb_3d\cb_3d_map.nc'
 file_map = r'c:\DATA\werkmap\vanJulien_shortmodelfiles\DFM_3D_z_Grevelingen\computations\run01\DFM_OUTPUT_Grevelingen-FM\Grevelingen-FM_0000_map.nc'
+#file_map = r'p:\11203379-mwra-new-bem-model\waq_model\simulations\A31_1year_20191219\DFM_OUTPUT_MB_02_waq\MB_02_waq_0000_map.nc'
+#TODO: get access to MWRA mapfiles
 
-#CURVIBEND (datetime)
-print('plot grid and values from mapdata (constantvalue, 1 dim)')
+if 'cb_3d_map' in file_map:
+    timestep = 72
+    layno = 5
+    laytyp = 'sigmalayer'
+elif 'Grevelingen' in file_map:
+    timestep = 3
+    layno = 35
+    laytyp = 'zlayer'
+elif 'DFM_OUTPUT_MB_02_waq' in file_map:
+    timestep = 30
+    layno = 5
+    laytyp = 'sigmalayer'
+else:
+    raise Exception('ERROR: no settings provided for this mapfile')
+
 ugrid = get_netdata(file_nc=file_map)#,multipart=False)
 fig, ax = plt.subplots()
 pc = plot_netmapdata(ugrid.verts, values=None, ax=ax, linewidth=0.5, color='crimson', facecolor="None")
@@ -56,6 +70,7 @@ if 0:
     line, = ax.plot([], [],'o-')  # empty line
     linebuilder = LineBuilder(line)
     line_array = linebuilder.line_array
+
 else:
     if 'cb_3d_map' in file_map:
         line_array = np.array([[ 185.08667065, 2461.11775254],
@@ -67,7 +82,11 @@ else:
                                [ 64053.73427496, 419407.58239502]])
         line_array = np.array([[ 53181.96942503, 424270.83361629],
                                [ 55160.15232593, 416913.77136685]])
-    
+    elif 'DFM_OUTPUT_MB_02_waq' in file_map:
+        line_array = np.array([[42.38, 42.38],
+                               [-70.98, -70.25]])
+
+print('finding crossing flow links')
 #allpol = []
 all_gridnos = []
 all_intersect = []
@@ -83,14 +102,9 @@ for iP, pol_data in enumerate(ugrid.verts):
     #print(iP)
 #allpol_multi = MultiPolygon(allpol)
 #intersection_line = allpol_multi.intersection(line_section).coords #does not work
+print('done finding crossing flow links')
 
 
-if 'cb_3d_map' in file_map:
-    timestep = 72
-    layno = 5
-elif 'Grevelingen' in file_map:
-    timestep = 3
-    layno = 35
 data_frommap = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_sa1', timestep=timestep, lay='all')#, multipart=False)
 data_frommap_flat = data_frommap[0,all_gridnos,layno]
 #fig, ax = plt.subplots()
@@ -115,16 +129,25 @@ crs_ystop = [x[1][1] for x in all_intersect]
 
 nlay = data_frommap.shape[2]
 
-crs_dist_starts = np.sqrt((crs_xstart - line_array[0,0])**2 + (crs_ystart - line_array[0,1])**2)
-crs_dist_stops = np.sqrt((crs_xstop - line_array[0,0])**2 + (crs_ystop - line_array[0,1])**2)
+if 'DFM_OUTPUT_MB_02_waq' in file_map:
+    line_array_x0_merc, line_array_y0_merc = merc(line_array[0,0],line_array[0,1])
+    crs_xstart_merc, crs_ystart_merc = merc(crs_xstart,crs_ystart)
+    crs_xstop_merc, crs_ystop_merc = merc(crs_xstart,crs_ystart)
+    crs_dist_starts = np.sqrt((crs_xstart_merc - line_array_x0_merc)**2 + (crs_ystart_merc - line_array_y0_merc)**2)
+    crs_dist_stops = np.sqrt((crs_xstop_merc - line_array_x0_merc)**2 + (crs_ystop_merc - line_array_y0_merc)**2)
+else:
+    crs_dist_starts = np.sqrt((crs_xstart - line_array[0,0])**2 + (crs_ystart - line_array[0,1])**2)
+    crs_dist_stops = np.sqrt((crs_xstop - line_array[0,0])**2 + (crs_ystop - line_array[0,1])**2)
+#TODO: distance is now from first clicked point, but should be distance along line
 
-data_nc = Dataset(file_map)
-if 'cb_3d_map' in file_map:
-    zvals_cen = np.linspace(data_frommap_bl_sel,data_frommap_wl3_sel,nlay)
+
+if laytyp == 'sigmalayer':
+    #zvals_cen = np.linspace(data_frommap_bl_sel,data_frommap_wl3_sel,nlay)
     zvals_interface = np.linspace(data_frommap_bl_sel,data_frommap_wl3_sel,nlay+1)
-elif 'Grevelingen' in file_map:
+elif laytyp == 'zlayer':
     #zvals_cen = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_layer_z', lay='all')#, multipart=False)
     #zvals_interface = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_interface_z')#, multipart=False)
+    data_nc = Dataset(file_map)
     zvals_interface = data_nc.variables['mesh2d_interface_z'][:]
 
 
@@ -136,9 +159,9 @@ for iL in range(nlay):
     zval_lay_bot = zvals_interface[iL]
     zval_lay_top = zvals_interface[iL+1]
     crs_verts_x = np.array([[crs_dist_starts,crs_dist_stops,crs_dist_stops,crs_dist_starts]]).T
-    if 'cb_3d_map' in file_map:
+    if laytyp == 'sigmalayer':
         crs_verts_z = np.array([[zval_lay_bot,zval_lay_bot,zval_lay_top,zval_lay_top]]).T
-    elif 'Grevelingen' in file_map:
+    elif laytyp == 'zlayer':
         crs_verts_z = np.repeat(np.array([[zval_lay_bot,zval_lay_bot,zval_lay_top,zval_lay_top]]).T[np.newaxis],repeats=crs_verts_x.shape[0],axis=0)
         #top z-layer is extended to water level, if wl is higher than zval_lay_top
         if iL == nlay-1:
@@ -160,7 +183,6 @@ pc = plot_netmapdata(crs_verts, values=data_frommap_sel_flat, ax=ax_crs, linewid
 fig.colorbar(pc, ax=ax_crs)
 ax_crs.set_ylim([-25,5])
 
-#data_frommap = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_sa12', timestep=timestep, lay='all')#, multipart=False)
 
 
 
