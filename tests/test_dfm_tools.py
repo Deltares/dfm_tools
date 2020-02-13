96682444af2cd933fe3f03771cc6cc617f158bb6
veenstrajelmer <Jelmer.Veenstra@Deltares.nl>
2020-02-13 11:31:27 +0100

    work jelmer

--- tests/test_dfm_tools.py
+++ tests/test_dfm_tools.py
@@ -19,6 +19,8 @@ def Test_mdu(self):
     assert 1==1
 
 
+
+
 def Test_getvarnamemapnc(self):
     from netCDF4 import Dataset
     
@@ -43,6 +45,8 @@ def Test_getvarnamemapnc(self):
     
     # 4. Vefiry final expectations
     assert 1==1
+
+
 
 
 def Test_grid_UGrid(self):
@@ -88,7 +92,30 @@ def Test_grid_getnetdata_plotnet(self):
     ax.set_aspect('equal')
  
     
+    
+    
 
+def Test_foufiles(self):
+    import matplotlib.pyplot as plt
+    plt.close('all')
+    
+    from dfm_tools.grid import get_netdata, plot_netmapdata, get_hismapmodeldata
+    
+    file_net_rmm = r'p:\11205258-006-kpp2020_rmm-g6\C_Work\01_Rooster\final_totaalmodel\rooster_rmm_v1p5_net.nc'
+    file_fou = r'c:\DATA\werkmap\vanJulien_shortmodelfiles\DFM_fou_RMM\RMM_dflowfm_0000_fou.nc'
+
+    ugrid = get_netdata(file_nc=file_net_rmm)
+    data_fromfou = get_hismapmodeldata(file_nc=file_fou, varname='mesh2d_fourier003_mean')#, multipart=False)
+
+    fig, ax = plt.subplots()
+    plot_netmapdata(ugrid.verts, values=data_fromfou, ax=None, linewidth=0.5, color="crimson", facecolor="None")
+    ax.set_aspect('equal')
+
+        
+    
+    
+    
+    
 def Test_grid_gethismodeldata_Nithin(self):
     """
     this test retrieves his data#, and plots it
@@ -318,6 +345,7 @@ def Test_grid_get_modeldata_onintersection(self):
     file_map = r'c:\DATA\werkmap\vanJulien_shortmodelfiles\DFM_sigma_curved_bend\DFM_OUTPUT_cb_3d\cb_3d_map.nc'
     file_map = r'c:\DATA\werkmap\vanJulien_shortmodelfiles\DFM_3D_z_Grevelingen\computations\run01\DFM_OUTPUT_Grevelingen-FM\Grevelingen-FM_0000_map.nc'
     #file_map = r'p:\11203379-mwra-new-bem-model\waq_model\simulations\A31_1year_20191219\DFM_OUTPUT_MB_02_waq\MB_02_waq_0000_map.nc'
+    file_map = r'p:\11205258-006-kpp2020_rmm-g6\jelmer_mwra\MB_02_waq_0000_map.nc'
     
     if 'cb_3d_map' in file_map:
         timestep = 72
@@ -340,12 +368,14 @@ def Test_grid_get_modeldata_onintersection(self):
                                [ 55017.72655174, 416403.77313703],
                                [ 65288.43784807, 419360.49305567]])
         clim = [-25,5]
-    elif 'DFM_OUTPUT_MB_02_waq' in file_map:
+    elif 'DFM_OUTPUT_MB_02_waq' in file_map or 'jelmer_mwra' in file_map:
         timestep = 30
         layno = 5
         convert2merc = True
-        line_array = np.array([[42.38, 42.38],
-                               [-70.98, -70.25]])
+        line_array = np.array([[-71.10395926,  42.3404146 ],
+                               [-69.6762489 ,  42.38341792]])
+        line_array = np.array([[-70.87382752,  42.39103758], #dummy for partition 0000
+                               [-70.42078633,  42.24876018]])
         clim = None
     else:
         raise Exception('ERROR: no settings provided for this mapfile')
@@ -377,23 +407,83 @@ def Test_grid_get_modeldata_onintersection(self):
     data_frommap_bl = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_flowelem_bl')#, multipart=False)
     pc = plot_netmapdata(ugrid.verts, values=data_frommap_bl, ax=ax_input, linewidth=0.5, cmap="jet")
 
-    #get data to plot and select data for cross section (gridnos)
+    #get data to plot
     data_frommap = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_sa1', timestep=timestep, lay='all')#, multipart=False)
-    data_frommap_sel = data_frommap[0,intersect_gridnos,:]
-    data_frommap_sel_flat = data_frommap_sel.T.flatten()
     
     #plot crossed cells (gridnos) in first plot
     data_frommap_flat = data_frommap[0,intersect_gridnos,layno]
     pc = plot_netmapdata(ugrid.verts[intersect_gridnos,:,:], values=data_frommap_flat, ax=ax_input, linewidth=0.5, cmap="jet")
     
     #plot cross section
+    data_frommap_sel = data_frommap[0,intersect_gridnos,:]
+    data_frommap_sel_flat = data_frommap_sel.T.flatten()
     fig, ax = plt.subplots()
     pc = plot_netmapdata(crs_verts, values=data_frommap_sel_flat, ax=ax, linewidth=0.5, cmap='jet')
     fig.colorbar(pc, ax=ax)
     ax.set_ylim(clim)
     
 
+def Test_grid_get_modeldata_onintersection_DCSM(self):
+    import matplotlib.pyplot as plt
+    plt.close('all')
+    import numpy as np
+    
+    from dfm_tools.grid import get_netdata, get_hismapmodeldata, get_modeldata_onintersection, plot_netmapdata
+    from dfm_tools.polygon import LineBuilder#, Polygon
 
+    file_map = r'p:\1204257-dcsmzuno\2013-2017\3D-DCSM-FM\A17b\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc'
+    
+    if 'DCSM-FM_0_5nm' in file_map:
+        timestep = 731
+        layno = 5
+        convert2merc = None
+        line_array = np.array([[ 0.97452229, 51.13407643],
+                               [ 1.89808917, 50.75191083]])
+        line_array = np.array([[10.17702481, 57.03663877],
+                               [12.38583134, 57.61284917]])
+        clim = None
+    else:
+        raise Exception('ERROR: no settings provided for this mapfile')
+
+    ugrid = get_netdata(file_nc=file_map,multipart=False)
+    
+    #create plot with ugrid and cross section line
+    fig, ax_input = plt.subplots()
+    pc = plot_netmapdata(ugrid.verts, values=None, ax=ax_input, linewidth=0.5, color='crimson', facecolor="None")
+    #pc.set_clim([28,30.2])
+    #fig.colorbar(pc, ax=ax)
+    ax_input.set_aspect('equal')
+    if 0: #click interactive polygon
+        #pol_frominput = Polygon.frominteractive(ax)
+        line, = ax_input.plot([], [],'o-')  # empty line
+        linebuilder = LineBuilder(line)
+        line_array = linebuilder.line_array
+    ax_input.plot(line_array[:,0],line_array[:,1])
+
+    #intersect function, find crossed cell numbers (gridnos) and coordinates of intersection (2 per crossed cell)
+    intersect_gridnos, intersect_coords = ugrid.polygon_intersect(line_array)
+    
+    #derive vertices from cross section (distance from first point)
+    crs_verts = get_modeldata_onintersection(file_nc=file_map, line_array=line_array, intersect_gridnos=intersect_gridnos, intersect_coords=intersect_coords, timestep=timestep, convert2merc=convert2merc)
+    
+    #get bed layer
+    data_frommap_bl = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_flowelem_bl', multipart=False)
+    pc = plot_netmapdata(ugrid.verts, values=data_frommap_bl, ax=ax_input, linewidth=0.5, cmap="jet")
+
+    #get data to plot
+    data_frommap = get_hismapmodeldata(file_nc=file_map, varname='mesh2d_sa1', timestep=timestep, lay='all', multipart=False)
+    
+    #plot crossed cells (gridnos) in first plot
+    data_frommap_flat = data_frommap[0,intersect_gridnos,layno]
+    pc = plot_netmapdata(ugrid.verts[intersect_gridnos,:,:], values=data_frommap_flat, ax=ax_input, linewidth=0.5, cmap="jet")
+    
+    #plot cross section
+    data_frommap_sel = data_frommap[0,intersect_gridnos,:]
+    data_frommap_sel_flat = data_frommap_sel.T.flatten()
+    fig, ax = plt.subplots()
+    pc = plot_netmapdata(crs_verts, values=data_frommap_sel_flat, ax=ax, linewidth=0.5, cmap='jet')
+    fig.colorbar(pc, ax=ax)
+    ax.set_ylim(clim)
 
     
 @pytest.fixture
