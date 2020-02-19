16c723fe0bd497030d43d1ea067282544e6d7d49
veenstrajelmer <Jelmer.Veenstra@Deltares.nl>
2020-02-19 19:42:32 +0100

    added first version of polygon reader

--- dfm_tools/polygon.py
+++ dfm_tools/polygon.py
@@ -24,14 +24,67 @@ class LineBuilder:
 
 
 class Polygon:
-    def __init__(self, x, y):
+    def __init__(self, x, y, name):
         import numpy as np
         self.x = x
         self.y = y
-        self.line_array = np.c_[self.xs, self.ys]
+        self.name = name
+        self.line_array = np.c_[self.x, self.y]
         
     def fromfile(file_pol):
-        raise Exception('ERROR: reading polygon from file is not yet possible')
+        import numpy as np
+        print('WARNING: this function is to be improved, outputformat will change')
+        '''
+        reads a pli boundary into an array
+        '''
+        def row2array(text):
+            '''
+            takes a string of space seperated floats and returns an array
+            '''
+            text = text.split()
+            arr = []
+            for ch in text:
+                try:
+                    val = float(ch)
+                    arr.append(val)
+                except:
+                    val = str(ch)
+                    arr.append(val)
+            return np.array(arr)
+
+        with open(file_pol) as plifile:
+            lines = plifile.readlines()
+            X = list([]) #list of pol x
+            Y = list([])#list of pol y
+            NAME=list([])
+            countpol=0       
+            count=0
+            
+            while count<=len(lines)-1:
+                row=lines[count]
+                count=count+1
+                line = row2array(row)
+                if type(line[0]) is np.str_ or type(line[0]) is np.string_ or type(line[0]) is str:
+                    if not line[0].startswith('*'):
+                        countpol=countpol+1
+                        #add to new polygon
+                        namei=str(line[0])
+                        line=row2array(lines[count])
+                        count=count+1
+                        len_pol=int(line[0])
+                        xpol=list([])
+                        ypol=list([])
+                        for ipoint in np.arange(len_pol):
+                            xpol.append(row2array(lines[count])[0])
+                            ypol.append(row2array(lines[count])[1])             
+                            count=count+1                
+                        #pol finished, add to total
+                        X.append(xpol)
+                        Y.append(ypol)
+                        NAME.append(namei)
+        data_pol = Polygon(x=X, y=Y, name=NAME)
+        return data_pol
+        
 
     def frominteractive(fig, ax):
         raise Exception('ERROR: interactive polygon from file is not yet possible')
---
+++ tests/test_polygon.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 19:27:21 2020
+
+@author: veenstra
+"""
+
+import pytest
+import inspect
+import os
+
+dir_tests = os.path.join(os.path.realpath(__file__), os.pardir)
+dir_testoutput = os.path.join(dir_tests,'test_output')
+if not os.path.exists(dir_testoutput):
+    os.mkdir(dir_testoutput)
+dir_testinput = os.path.join(r'c:/DATA/werkmap','dfm_tools_testdata')
+
+@pytest.mark.parametrize("file_pol", [pytest.param(os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pliz'), id='Grevelingen'),
+                                      pytest.param(os.path.join(dir_testinput,r'world.ldb'), id='world')])
+@pytest.mark.unittest    
+def test_readpolygon(file_pol):
+    """
+    this test tests if a netcdf varname can be retrieved from the 'dictionary' and if the variable can be retrieved from de netcdf
+    """
+    
+    from dfm_tools.polygon import Polygon
+    
+    #file_pol = os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pliz')
+    #file_pol = os.path.join(dir_testinput,r'world.ldb')
+    
+    data_polX, data_polY, data_polNAMES = Polygon.fromfile(file_pol)
+    
+    
+    
