c589486d8603d563a9308e0a3437221291d24486
veenstrajelmer <Jelmer.Veenstra@Deltares.nl>
2020-02-20 10:52:24 +0100

    added readpolygon function, made more generic

--- dfm_tools/polygon.py
+++ dfm_tools/polygon.py
@@ -24,66 +24,53 @@ class LineBuilder:
 
 
 class Polygon:
-    def __init__(self, x, y, name):
-        import numpy as np
-        self.x = x
-        self.y = y
+    def __init__(self, data, name):
+        #import numpy as np
+        self.data = data
         self.name = name
-        self.line_array = np.c_[self.x, self.y]
+        #self.line_array = np.c_[self.x, self.y]
         
     def fromfile(file_pol):
         import numpy as np
-        print('WARNING: this function is to be improved, outputformat will change')
+        print('WARNING: the function dfm_tools.polygon.Polygon.fromfile() will be improved, outputformat will change')
         '''
         reads a pli boundary into an array
         '''
-        def row2array(text):
-            '''
-            takes a string of space seperated floats and returns an array
-            '''
-            text = text.split()
-            arr = []
-            for ch in text:
-                try:
-                    val = float(ch)
-                    arr.append(val)
-                except:
-                    val = str(ch)
-                    arr.append(val)
-            return np.array(arr)
 
         with open(file_pol) as plifile:
             lines = plifile.readlines()
-            X = list([]) #list of pol x
-            Y = list([])#list of pol y
-            NAME=list([])
-            countpol=0       
-            count=0
-            
-            while count<=len(lines)-1:
-                row=lines[count]
-                count=count+1
-                line = row2array(row)
-                if type(line[0]) is np.str_ or type(line[0]) is np.string_ or type(line[0]) is str:
-                    if not line[0].startswith('*'):
-                        countpol=countpol+1
-                        #add to new polygon
-                        namei=str(line[0])
-                        line=row2array(lines[count])
-                        count=count+1
-                        len_pol=int(line[0])
-                        xpol=list([])
-                        ypol=list([])
-                        for ipoint in np.arange(len_pol):
-                            xpol.append(row2array(lines[count])[0])
-                            ypol.append(row2array(lines[count])[1])             
-                            count=count+1                
-                        #pol finished, add to total
-                        X.append(xpol)
-                        Y.append(ypol)
-                        NAME.append(namei)
-        data_pol = Polygon(x=X, y=Y, name=NAME)
-        return data_pol
+            pol_data_list = []
+            pol_name_list = []
+            #pol_object_list = []
+            iLine=0
+            while iLine<=len(lines)-1:
+                line_str = lines[iLine].split()
+                iLine=iLine+1
+                try:
+                    float(line_str[0]) #this line should start with a string, so this should fail
+                    raise Exception('ERROR: line_str[0] is not a string: (%s, somewhere around line %d), polygon file format might be wrong'%(line_str, iLine))
+                except:
+                    pass
+                if not line_str[0].startswith('*'):
+                    #the start of a new polygon
+                    pol_name_list.append(line_str[0]) #get name from name line_str
+                    
+                    #convert shape numbers to int
+                    line_shp = [int(x) for x in lines[iLine].split()]
+                    iLine=iLine+1
+                    len_pol=int(line_shp[0])
+                    
+                    #retrieve data numbers as floats
+                    data_pol_str = lines[iLine:iLine+len_pol]
+                    iLine = iLine+len_pol
+                    data_pol = np.array([x.split() for x in data_pol_str],dtype=float)
+                    data_pol_nanbool = data_pol==999.999
+                    data_pol[data_pol_nanbool] = np.nan
+                    pol_data_list.append(data_pol)
+                    
+                    #pol_object = Polygon(data=data_pol, name=line_str[0])
+                    #pol_object_list.append(pol_object)
+        return pol_data_list, pol_name_list#, pol_object_list
         
 
     def frominteractive(fig, ax):
--- tests/test_polygon.py
+++ tests/test_polygon.py
@@ -15,7 +15,8 @@ if not os.path.exists(dir_testoutput):
     os.mkdir(dir_testoutput)
 dir_testinput = os.path.join(r'c:/DATA/werkmap','dfm_tools_testdata')
 
-@pytest.mark.parametrize("file_pol", [pytest.param(os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pliz'), id='Grevelingen'),
+@pytest.mark.parametrize("file_pol", [pytest.param(os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pliz'), id='Grevelingen pliz'),
+                                      pytest.param(os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pli'), id='Grevelingen pli'),
                                       pytest.param(os.path.join(dir_testinput,r'world.ldb'), id='world')])
 @pytest.mark.unittest    
 def test_readpolygon(file_pol):
@@ -23,16 +24,20 @@ def test_readpolygon(file_pol):
     this test tests if a netcdf varname can be retrieved from the 'dictionary' and if the variable can be retrieved from de netcdf
     """
     
+    import matplotlib.pyplot as plt
+    plt.close('all')
+    
     from dfm_tools.polygon import Polygon
     
-    #file_pol = os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pli')
-    #file_pol = os.path.join(dir_testinput,r'world.ldb')
+    """
+    file_pol = os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pliz')
+    file_pol = os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\geometry\structures\Grevelingen-FM_BL_fxw.pli')
+    file_pol = os.path.join(dir_testinput,r'world.ldb')
+    """
     
-    data_pol = Polygon.fromfile(file_pol)
-    data_pol_x = data_pol.x
-    data_pol_y = data_pol.y
-    data_pol_names = data_pol.name
-    data_pol_linearray = data_pol.line_array
+    pol_data_list, pol_name_list = Polygon.fromfile(file_pol)
+    fig, ax = plt.subplots()
+    for iP, pol_data in enumerate(pol_data_list):
+        ax.plot(pol_data[:,0],pol_data[:,1],'-',linewidth=0.5)
 
-    
     
