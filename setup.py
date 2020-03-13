15549383857a56d95c79213d53936ee91ddf1fb1
Jelmer Veenstra <Jelmer.Veenstra@deltares.nl>
2020-03-13 23:18:09 +0100

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/openearth/dfm_tools',
-    version='0.6.1',
+    version='0.6.2',
     zip_safe=False,
 )
