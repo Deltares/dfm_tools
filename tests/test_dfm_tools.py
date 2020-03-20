cf734cef5eb7bd2292ff77f28008b94393df8da0
veenstra <Jelmer.Veenstra@deltares.nl>
2020-03-20 19:23:04 +0100

    moved testutils

--- tests/test_dfm_tools.py
+++ tests/test_dfm_tools.py
@@ -7,7 +7,7 @@ import pytest
 import os
 
 dir_testinput = os.path.join(r'c:/DATA','dfm_tools_testdata')
-#from tests.TestUtils import getmakeoutputdir
+#from dfm_tools.testutils import getmakeoutputdir
 
 
 statement_list = ['import os','import sys','import glob','import shutil','import scipy','import numpy','import datetime','import pandas','import matplotlib','import netCDF4','import click','import shapely','import shapely.geometry','from shapely.geometry import Point']
--- tests/test_get_nc.py
+++ tests/test_get_nc.py
@@ -10,7 +10,7 @@ import inspect
 import os
 
 dir_testinput = os.path.join(r'c:/DATA','dfm_tools_testdata')
-from tests.TestUtils import getmakeoutputdir
+from dfm_tools.testutils import getmakeoutputdir
 
 
 @pytest.mark.parametrize("file_nc, expected_size", [pytest.param(os.path.join(dir_testinput,r'DFM_3D_z_Grevelingen\computations\run01\DFM_OUTPUT_Grevelingen-FM\Grevelingen-FM_0000_map.nc'), 5599, id='from 1 map partion Grevelingen'),
