06c13c1ce4a4d998d2fe04c520aeb4df849e4a08
veenstra <Jelmer.Veenstra@deltares.nl>
2020-04-24 10:55:12 +0200

    added merge_netCDF_time

--- dfm_tools/io/netCDF_utils.py
+++ dfm_tools/io/netCDF_utils.py
@@ -5,46 +5,46 @@ Created on Fri Apr 24 09:50:42 2020
 @author: veenstra
 """
 
-def merge_netCDF_time(tstart, tstop, dir_data, list_subfolders=None, nc_prefix=''):
+def merge_netCDF_time(tstart, tstop, tstep_sec, dir_data, nc_prefix, fn_match_pattern, fn_dateformat, subfolders=[''], dir_out=None, varn_time='time', dimn_time='time'):
+    """
+    this script works well for daily HYCOM data but should be made more generic for other types of (meteo) data.
+    """
     
     from netCDF4 import Dataset
     import glob
     import os
     import re
     import datetime as dt
-    
-    #SETTINGS
-    tstart = dt.datetime(2016,4,28)
-    tstop = dt.datetime(2016,5,3)
-    dir_data = r'n:\Deltabox\Postbox\Veenstra, Jelmer\vanReimer\GLBu0.08_expt_91.2'
-    list_subfolders = ''
-    nc_prefix = 'HYCOM_ST_GoO_'
-    file_to = os.path.join(dir_data, 'merged', '%s%sto%s.nc'%(nc_prefix, merge_tstart.strftime('%Y%m%d'), merge_tstop.strftime('%Y%m%d'))
+        
+    if dir_out is None:
+        dir_out = dir_data
+    if not os.path.exists(dir_out):
+        os.makedirs(dir_out)
+        
     file_src_listall = glob.glob(os.path.join(dir_data, '%s*.nc'%(nc_prefix)))
-    fn_dateformat = '%Y%m%d'
-    fn_match_pattern_dates = '%s(.*).nc'%(nc_prefix)
-    ###############
+    file_to = os.path.join(dir_out, '%s%sto%s.nc'%(nc_prefix, tstart.strftime(fn_dateformat), tstop.strftime(fn_dateformat)))
+    p = re.compile(fn_match_pattern)
     
-    p = re.compile(fn_match_pattern_dates)
     try:
         data_to.close()
     except:
         pass
-    merge_ndays = ((merge_tstop-merge_tstart).total_seconds()/3600/24)+1
+    merge_ntimesteps = ((tstop-tstart).total_seconds()/tstep_sec)+1
 
     file_src_list = []
     for iF, file_src in enumerate(file_src_listall):
         fn_result = p.search(file_src)
         fn_timestamp = fn_result.group(1)
         fn_timestamp_dt = dt.datetime.strptime(fn_timestamp,fn_dateformat)
-        if (fn_timestamp_dt >= merge_tstart) & (fn_timestamp_dt <= merge_tstop):
+        if (fn_timestamp_dt >= tstart) & (fn_timestamp_dt <= tstop):
             file_src_list.append(file_src)
     if file_src_list == []:
-        raise Exception('no files found between %s and %s'%(merge_tstart, merge_tstop))
-    if len(file_src_list) != merge_ndays:
-        raise Exception('number of found files (%d) is not equal to number of requested days (%d)'%(len(file_src_list), merge_ndays))
+        raise Exception('no files found between %s and %s'%(tstart, tstop))
+    if len(file_src_list) != merge_ntimesteps:
+        raise Exception('number of found files (%d) is not equal to number of requested days (%d)'%(len(file_src_list), merge_ntimesteps))
         
     for iF, file_src in enumerate(file_src_list):
+        print('processing: %s'%(file_src))
         data_src = Dataset(file_src)
         if iF == 0: #initiate empty file
             data_to = Dataset(file_to, 'w', format="NETCDF3_CLASSIC")
@@ -55,11 +55,11 @@ def merge_netCDF_time(tstart, tstop, dir_data, list_subfolders=None, nc_prefix='
                 if ncattrname not in ['variables','dimensions']:
                     data_to.setncattr(ncattrname, data_src.getncattr(ncattrname))
             
-            #copy dimensions (make time unlimited)
+            #copy dimensions (make dimn_time unlimited)
             data_src_dimlist = list(data_src.dimensions.keys())
             for dimname in data_src_dimlist:
                 data_src_dimsize = data_src.dimensions[dimname].size
-                if dimname == 'time':
+                if dimname == dimn_time: #this one is unlimited, necessary for merging
                     data_to.createDimension(dimname, None)
                 else:
                     data_to.createDimension(dimname, data_src_dimsize)
@@ -70,41 +70,42 @@ def merge_netCDF_time(tstart, tstop, dir_data, list_subfolders=None, nc_prefix='
                 data_src_var=data_src.variables[varname]
                 data_src_dims=data_src_var.dimensions
                 data_src_type=data_src_var.dtype.str
+                timedep_varlist = []
+                if dimn_time in data_src_dims:
+                    data_src_var_timedep = True
+                    timedep_varlist.append(varname)
+                else:
+                    data_src_var_timedep = False
                 if '_FillValue' in data_src_var.ncattrs():
                     data_to.createVariable(varname,data_src_type,data_src_dims, fill_value=data_src_var.getncattr('_FillValue'))
                 else:
                     data_to.createVariable(varname,data_src_type,data_src_dims)
                 for attr_name in data_src_var.ncattrs():
-                    if attr_name != '_FillValue': #to avoid error with netCDF4 library version 1.5.3, fill values are not convenient for dflowfm
+                    if attr_name != '_FillValue': #_FillValue should be and is already set upon creating variable
                         #print(attr_name,'=',data_src_var.getncattr(attr_name))
                         data_to.variables[varname].setncattr(attr_name,data_src_var.getncattr(attr_name))
-                if varname in ['depth', 'lat', 'lon']:
+                if not data_src_var_timedep:
                     data_to.variables[varname][:] = data_src.variables[varname][:]
-    
+        
+        #check if all attributes in both files are equal (mainly relevant for scale_factor and add_offset)
         for attr_name in data_src.variables[varname].ncattrs():
             if data_src.variables[varname].getncattr(attr_name) != data_to.variables[varname].getncattr(attr_name):
                 raise Exception('attribute %s not equal for variable %s'%(attr_name,varname))
     
     
-        data_src_ntimes = data_src.variables['time'].shape[0]
-        data_to_ntimes = data_to.variables['time'].shape[0]
+        data_src_ntimes = data_src.variables[varn_time].shape[0]
+        data_to_ntimes = data_to.variables[varn_time].shape[0]
         
         #append current data to netcdf files
-        data_to.variables['time'][data_to_ntimes:data_to_ntimes+data_src_ntimes] = data_src.variables['time'][:]
-        data_to.variables['salinity'][data_to_ntimes:data_to_ntimes+data_src_ntimes,...] = data_src.variables['salinity'][:]
-        data_to.variables['water_temp'][data_to_ntimes:data_to_ntimes+data_src_ntimes,...] = data_src.variables['water_temp'][:]
+        for varname in timedep_varlist:
+            data_to.variables[varname][data_to_ntimes:data_to_ntimes+data_src_ntimes,...] = data_src.variables[varname][:]
         data_src.close()
-    data_to.close()
-        
-        
-    """
-    from dfm_tools.get_nc import get_netdata, get_ncmodeldata, plot_netmapdata
-    from dfm_tools.get_nc_helpers import get_ncvardimlist#, get_ncfilelist
-    
-    file_nc = file_to
-    vars_pd, dims_pd = get_ncvardimlist(file_nc=file_nc)
-    
-    data_fromnc = get_ncmodeldata(file_nc=file_nc, varname='salinity', timestep='all', layer='all')
-    
-    
-    """
+    #data_to.close()
+    return data_to
+
+
+
+
+
+
+
--- tests/test_io.py
+++ tests/test_io.py
@@ -136,3 +136,68 @@ def test_readnoosfile():
 
 
 
+
+
+
+@pytest.mark.acceptance
+def test_mergenetCDFtime():
+    dir_output = getmakeoutputdir(__file__,inspect.currentframe().f_code.co_name)
+    """
+    this test tests merging a (eg meteo) netcdf over time
+
+    dir_output = './test_output'
+    """
+
+
+    import datetime as dt
+
+    from dfm_tools.io.netCDF_utils import merge_netCDF_time
+    """
+    if mode == 'meteo':
+        dir_data = 'P:\\1204257-dcsmzuno\\2014\\data\\meteo'
+        subfolders = ['HIRLAM72_2011','HIRLAM72_2012','HIRLAM72_2013','HIRLAM72_2014','HIRLAM72_2015','HIRLAM72_2016','HIRLAM72_2017','HIRLAM72_2018','HIRLAM72_2019']
+        ignorelist = ['P:\\1204257-dcsmzuno\\2014\\data\\meteo\\HIRLAM72_2016\\h72_2016_merged.nc']
+        vars_orig = ['air_pressure_fixed_height','northward_wind','eastward_wind'] #voor meteo folder
+        vars_dest = vars_orig
+        convert_vars = [0,0,0]
+    elif mode == 'meteo_heatflux':
+        dir_data = 'P:\\1204257-dcsmzuno\\2014\\data\\meteo-heatflux'
+        subfolders = ['HIRLAM72_2011','HIRLAM72_2012','HIRLAM72_2013','HIRLAM72_2014','HIRLAM72_2015','HIRLAM72_2016','HIRLAM72_2017','HIRLAM72_2018','HIRLAM72_2019']
+        ignorelist = []
+        vars_orig = ['dew_point_temperature','air_temperature','cloud_area_fraction'] #voor meteo-heatflux folder
+        vars_dest = vars_orig
+        convert_vars = [1,1,2] #0 is no conversion, 1 is K to C (incl unit), 2 is cloud cover fraction (0-1) to cloud cover percentage (0-100) (unit is al %)
+    else:
+        raise Exception('ERROR: wrong mode %s'%(mode))
+    """
+    
+    #SETTINGS
+    tstart = dt.datetime(2016,4,28)
+    tstop = dt.datetime(2016,5,3)
+    tstep_sec = 3600*24
+    dir_data = r'n:\Deltabox\Postbox\Veenstra, Jelmer\vanReimer\GLBu0.08_expt_91.2'
+    nc_prefix = 'HYCOM_ST_GoO_'
+    fn_match_pattern = '%s(.*).nc'%(nc_prefix)
+    fn_dateformat = '%Y%m%d'
+    #subfolders = ''
+    dir_out = r'n:\Deltabox\Postbox\Veenstra, Jelmer\vanReimer\merged_new'
+    ###############
+    data_to = merge_netCDF_time(tstart=tstart, tstop=tstop, tstep_sec=tstep_sec, dir_data=dir_data, nc_prefix=nc_prefix, 
+                                fn_match_pattern=fn_match_pattern, fn_dateformat=fn_dateformat, dir_out=dir_out)
+    
+    varlist = data_to.variables.keys()
+    #data_to.variables['salinity'].setncattr('standard_name','sea_water_salinity')
+    #data_to.variables['water_temp'].setncattr('standard_name','sea_water_potential_temperature')
+    data_to.renameVariable('salinity','so')
+    data_to.renameVariable('water_temp','thetao')
+    
+    """
+    from dfm_tools.get_nc import get_netdata, get_ncmodeldata, plot_netmapdata
+    from dfm_tools.get_nc_helpers import get_ncvardimlist#, get_ncfilelist
+    
+    file_nc = file_to
+    vars_pd, dims_pd = get_ncvardimlist(file_nc=file_nc)
+    
+    data_fromnc = get_ncmodeldata(file_nc=file_nc, varname='salinity', timestep='all', layer='all')
+
+    """
