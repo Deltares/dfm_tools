ae0746553ab9a0098f2803b73804277b21c79ca6
veenstra <Jelmer.Veenstra@deltares.nl>
2020-06-04 13:29:46 +0200

    redesigned read_bcfile and write_bcfile

--- dfm_tools/io/bc.py
+++ dfm_tools/io/bc.py
@@ -10,41 +10,152 @@ Created on Mon Apr 20 21:01:08 2020
 
 
 
-def write_bcfile(filepath_bc, data_pd, pliname, refdate, tzone=0, mode='w', data_format='%6.2f'):
+def write_bcfile(filename, datablocks, metadatas, refdate=None, tzone=0, float_format='%6.2f'):
+    """
+    
+
+    Parameters
+    ----------
+    filename : TYPE
+        DESCRIPTION.
+    datablocks : (list of) MultiIndex pandas dataframe
+        The DataFrame should have MultiIndex column names with quantity names on level 0 and unit names on level 1.
+    metadatas : (list of) dict
+        the metadata dictionary contains all metadata, except for quantity and unit (they are in the dataframe columns.
+    refdate : TYPE, optional
+        DESCRIPTION. The default is None.
+    tzone : TYPE, optional
+        DESCRIPTION. The default is 0.
+    data_format : TYPE, optional
+        DESCRIPTION. The default is '%6.2f'.
+
+    Raises
+    ------
+    Exception
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
     #import numpy as np
     
-    data_pd_out = data_pd.copy()
-    pd_columns = data_pd_out.columns.tolist()
-    if 'time' in pd_columns:
-        times_wrtref_min = (data_pd_out['time']-refdate).dt.total_seconds()/60
-        if tzone >= 0:
-            tzone_sign = '+'
-        else:
-            tzone_sign = '-'
-        times_unit = 'minutes since %s %s%02d:00'%(refdate.strftime('%Y-%m-%d %H:%M:%S'), tzone_sign, tzone)
-        data_pd_out['time'] = times_wrtref_min
+    if type(datablocks) is not list:
+        datablocks = [datablocks]
+        metadatas = [metadatas]
     
-    with open(filepath_bc,mode) as file_bc:
-        file_bc.write('[forcing]\n')
-        file_bc.write('Name                            = %s\n'%(pliname))
-        file_bc.write('Function                        = timeseries\n')
-        file_bc.write('Time-interpolation              = linear\n')
-        for iC, pd_colname in enumerate(pd_columns):
-            if pd_colname in ['time']:
-                unit = times_unit
-            elif pd_colname in ['dischargebnd','discharge','lateral_discharge']:
-                unit = 'm3/s'
+    #clear file
+    with open(filename, 'w') as file_bc:
+        pass
+    
+    for metadata_dict, data_pd in zip(metadatas, datablocks):
+        data_pd_out = data_pd.copy()
+        pd_columns = data_pd.columns.tolist()
+        if ('time','datetime') in pd_columns:
+            if refdate is None:
+                raise Exception('datetime as units in input data, but no refdate provided')
+            time_colid = pd_columns.index(('time','datetime'))
+            print('datetime values are replaced by minutes since')
+            times_wrtref_min = (data_pd[('time','datetime')]-refdate).dt.total_seconds()/60
+            if tzone >= 0:
+                tzone_sign = '+'
             else:
-                unit = '-'
+                tzone_sign = '-'
+            times_unit = 'minutes since %s %s%02d:00'%(refdate.strftime('%Y-%m-%d %H:%M:%S'), tzone_sign, tzone)
             
-            file_bc.write('Quantity                        = %s\n'%(pd_colname))
-            file_bc.write('Unit                            = %s\n'%(unit))
+            #rename datetime unit
+            pd_columns_lev1 = [y for x,y in pd_columns]
+            pd_columns_lev1[time_colid] = times_unit
+            data_pd_out.columns.set_levels(pd_columns_lev1,level=1,inplace=True)
+            
+            #replace datetime values by minutes since
+            data_pd_out[('time',times_unit)] = times_wrtref_min
+        pd_out_columns = data_pd_out.columns.tolist()
+       
+        with open(filename, 'a') as file_bc:
+            file_bc.write('[forcing]\n')
+            for key in metadata_dict.keys():
+                file_bc.write('%-32s= %s\n'%(key,metadata_dict[key]))
+            for iC, pd_colname in enumerate(pd_out_columns):
+                quan = pd_colname[0]
+                unit = pd_colname[1]
+                """
+                if pd_colname in ['time']:
+                    unit = times_unit
+                elif pd_colname in ['dischargebnd','discharge','lateral_discharge']:
+                    unit = 'm3/s'
+                else:
+                    unit = '-'
+                """
+                file_bc.write('Quantity                        = %s\n'%(quan))
+                file_bc.write('Unit                            = %s\n'%(unit))
+            
+        #    for timestamp, line_time, line_value in zip(times_pd, times_wrtref_min, values):
+        #        file_bc.write('%15d %15E\n'%(line_time, line_value))
+        data_pd_out.to_csv(filename, header=None, index=None, sep='\t', mode='a', float_format=float_format)
+        with open(filename,'a') as file_bc:
+            file_bc.write('\n')
+
+
+
+def read_bcfile(filename):
+
+    import pandas as pd
+    import numpy as np
+    
+    with open(filename, 'r') as bc:
+        page = bc.readlines()
+    lines_blockstarts = np.array([],dtype=int)
+    lines_quantity = np.array([],dtype=int)
+    lines_unit = np.array([],dtype=int)
+    lines_n = len(page)
+    for iL, line in enumerate(page):
+        if '[forcing]' in line.lower():
+            lines_blockstarts = np.append(lines_blockstarts,iL)
+        if 'quantity' in line.lower():
+            lines_quantity = np.append(lines_quantity,iL)
+        if 'unit' in line.lower():
+            lines_unit = np.append(lines_unit,iL)
+    if len(lines_quantity) != len(lines_unit):
+        raise Exception('something wrong with bc-file, amount of quantity lines is not the same as amount of unit lines')
+    
+    metadata_list = []
+    datablock_list = []
+    for iB in range(len(lines_blockstarts)):
+        line_blockstart = lines_blockstarts[iB]
+        if iB == len(lines_blockstarts)-1:
+            line_blockend = lines_n
+        else:
+            line_blockend = lines_blockstarts[iB+1]
         
-    #    for timestamp, line_time, line_value in zip(times_pd, times_wrtref_min, values):
-    #        file_bc.write('%15d %15E\n'%(line_time, line_value))
-    data_pd_out.to_csv(filepath_bc, header=None, index=None, sep='\t', mode='a', float_format=data_format)
-    with open(filepath_bc,'a') as file_bc:
-        file_bc.write('\n')
+        line_blockquantities = lines_quantity[(lines_quantity>line_blockstart) & (lines_quantity<line_blockend)]
+        line_blockunits = lines_unit[(lines_unit>line_blockstart) & (lines_unit<line_blockend)]
+        line_datastart = line_blockunits[-1]+1
+        
+        #get metadata (without quantities and units)
+        metadata_str = page[line_blockstart:line_blockquantities[0]]
+        metadata = {}
+        for metadata_line in metadata_str:
+            if metadata_line.startswith('['):
+                pass#metadata[metadata_line.strip()] = ''
+            else:
+                meta_name = metadata_line.split('=')[0].strip()
+                meta_val = metadata_line.split('=')[1].strip()
+                metadata[meta_name] = meta_val
+        metadata_list.append(metadata)
+        
+        #define column names of quantities and units
+        colnames = []
+        for line_quan,line_un in zip(line_blockquantities,line_blockunits):
+            quan_val = page[line_quan].split('=')[1].strip()
+            un_val = page[line_un].split('=')[1].strip()
+            colnames.append((quan_val,un_val))
+        
+        #read data block
+        data_block_pd = pd.read_csv(filepath_or_buffer=filename, names=colnames, skiprows=line_datastart, delim_whitespace=True, skipfooter=lines_n-line_blockend, engine='python')
+        datablock_list.append(data_block_pd)
 
+                    
 
-
+    return datablock_list, metadata_list
--- tests/test_io.py
+++ tests/test_io.py
@@ -199,3 +199,53 @@ def test_mergenetCDFtime():
     print(varlist)
     #data_to.variables['salinity'].setncattr('standard_name','sea_water_salinity')
     #data_to.variables['water_temp'].setncattr('standard_name','sea_water_potential_temperature')
+    
+    
+    
+    
+@pytest.mark.acceptance
+def test_readwrite_bc():
+    dir_output = getmakeoutputdir(__file__,inspect.currentframe().f_code.co_name)
+    """
+    this test tests merging a (eg meteo) netcdf over time
+
+    dir_output = './test_output'
+    """
+    import datetime as dt
+    import pandas as pd
+    
+    from dfm_tools.io.polygon import Polygon
+    from dfm_tools.io.bc import read_bcfile, write_bcfile    
+    
+    #RMM: read tekfile and convert to bc file
+    file_outbc = os.path.join(dir_output,'lateralen_RMM.bc');
+    
+    refdate = dt.datetime(2007,1,1)
+    
+    file_tek = os.path.join(dir_testinput,'Gouda.tek');
+    
+    data_tek_pd = Polygon.fromfile(file_tek,pd_output=True)[0]
+    
+    datesel_bool = (data_tek_pd['datetime'] >= refdate) & (data_tek_pd['datetime'] <= dt.datetime(2014,1,1))
+    data_tek_pd_sel = data_tek_pd[datesel_bool]
+    
+    data_pd_tobc = pd.DataFrame({('time','datetime'):data_tek_pd_sel['datetime'], ('lateral_discharge','m3/s'): data_tek_pd_sel.iloc[:,3]})
+    
+    metadata_dict = {'Name':'HY_0.77_C_ONB_Gouda', 'Function':'timeseries', 'Time-interpolation':'linear'}
+    write_bcfile(filename=file_outbc, datablocks=data_pd_tobc, metadatas=metadata_dict, refdate=refdate, tzone=1, float_format='%6.2f')
+    
+    
+    #read and write bc file
+    bc_in = os.path.join(dir_testinput,'lateralen_20072008_maas.bc')
+    bc_out = os.path.join(dir_output,'lateralen_20072008_maas_dfmtoolscopy.bc')
+    
+    datablock_list, metadata_list = read_bcfile(filename=bc_in)
+    
+    write_bcfile(filename=bc_out, datablocks=datablock_list, metadatas=metadata_list, float_format='%.2f')
+
+    
+    
+    
+    
+    
+    
