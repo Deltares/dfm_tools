37a6557916686a8796ea2517ad2d750c6e2ec95e
veenstrajelmer <veenstrajelmer@gmail.com>
2022-09-12 21:42:56 +0200

    Bump version: 0.7.78 → 0.7.79

--- dfm_tools/__init__.py
+++ dfm_tools/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Jelmer Veenstra"""
 __email__ = 'jelmer.veenstra@deltares.nl'
-__version__ = '0.7.78'
+__version__ = '0.7.79'
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/openearth/dfm_tools',
-    version='0.7.78',
+    version='0.7.79',
     zip_safe=False,
 )
