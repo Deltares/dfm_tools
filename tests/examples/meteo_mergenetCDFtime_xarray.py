7722b3d3358ee9b0ed9046124522274fa0bd0ee2
veenstrajelmer <veenstrajelmer@gmail.com>
2022-09-30 23:24:19 +0200

    meteo merge, added first steps for ERA5

--- tests/examples/meteo_mergenetCDFtime_xarray.py
+++ tests/examples/meteo_mergenetCDFtime_xarray.py
@@ -4,23 +4,26 @@ Created on Wed Oct 27 22:07:34 2021
 
 @author: veenstra
 
-this test tests merging a (eg meteo) netcdf over time
+script to merge netcdf file from several folders into one file (concatenate time dimension)
+too slow? Change fn_match_pattern to match less files if possible, or provide a list of (less) files as file_nc
 
 """
 
 from netCDF4 import Dataset
 import glob
+import re
 import datetime as dt
 import os
 import xarray as xr
-#import matplotlib.pyplot as plt
-#plt.close('all')
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.close('all')
 #from dfm_tools.get_nc_helpers import get_ncvardimlist#, get_ncfilelist
 #from dfm_tools.io.netCDF_utils import merge_netCDF_time
 
 
-mode = 'ERA5'# 'HIRLAM_meteo' 'HIRLAM_meteo_heatflux' 'HYCOM'
-all_tstart = dt.datetime(2013,12,30) # HIRLAM
+mode = 'ERA5_heat_model'# 'HIRLAM_meteo' 'HIRLAM_meteo-heatflux' 'HYCOM' 'ERA5_wind_pressure' ERA5_heat_model ERA5_radiation ERA5_rainfall
+all_tstart = dt.datetime(2013,12,30) # HIRLAM and ERA5
 all_tstop = dt.datetime(2014,1,1)
 #all_tstart = dt.datetime(2016,4,28) # HYCOM
 #all_tstop = dt.datetime(2016,5,3)
@@ -30,19 +33,32 @@ script_tstart = dt.datetime.now()
 if 'HIRLAM' in mode:
     if mode == 'HIRLAM_meteo': #1year voor meteo crasht (HIRLAM72_*\\h72_*) door conflicting dimension sizes, sourcefolders opruimen? meteo_heatflux folders zijn schoner dus daar werkt het wel
         dir_data = 'P:\\1204257-dcsmzuno\\2014\\data\\meteo\\HIRLAM72_*' #files contain: ['air_pressure_fixed_height','northward_wind','eastward_wind']
-    elif mode == 'HIRLAM_meteo_heatflux':
+    elif mode == 'HIRLAM_meteo-heatflux':
         dir_data = 'P:\\1204257-dcsmzuno\\2014\\data\\meteo-heatflux\\HIRLAM72_*' # files contain: ['dew_point_temperature','air_temperature','cloud_area_fraction']
     fn_match_pattern = 'h72_20131*.nc'
+    file_out_prefix = 'h72_'
     drop_variables = ['x','y'] #will be added again as longitude/latitude, this is a workaround
     rename_variables = None
 elif mode == 'HYCOM':
     dir_data = 'c:\\DATA\\dfm_tools_testdata\\GLBu0.08_expt_91.2'
     fn_match_pattern = 'HYCOM_ST_GoO_*.nc'
+    file_out_prefix = fn_match_pattern.replace('*.nc','')
     drop_variables = None
     rename_variables = {'salinity':'so', 'water_temp':'thetao'}
-elif mode == 'ERA5':
-    data_dir = 'p:\\metocean-data\\open\\ERA5\\data\\Irish_North_Baltic_Sea'
-    
+elif 'ERA5' in mode: #TODO: generates "PerformanceWarning: Slicing is producing a large chunk.", probably because of lots of files but probably also solveable
+    if mode=='ERA5_wind_pressure':
+        varkey_list = ['chnk','mslp','u10n','v10n'] #charnock, mean_sea_level_pressure, 10m_u_component_of_neutral_wind, 10m_v_component_of_neutral_wind
+    elif mode=='ERA5_heat_model':
+        varkey_list = ['d2m','t2m','tcc'] # 2m_dewpoint_temperature, 2m_temperature, total_cloud_cover
+    elif mode=='ERA5_radiation':
+        varkey_list = ['ssr','strd'] # surface_net_solar_radiation, surface_thermal_radiation_downwards
+    elif mode=='ERA5_rainfall':
+        varkey_list = ['mer','mtpr'] # mean_evaporation_rate, mean_total_precipitation_rate
+    dir_data = 'p:\\metocean-data\\open\\ERA5\\data\\Irish_North_Baltic_Sea\\*' #TODO: add other vars with * (support separate files)
+    fn_match_pattern = f'era5_.*({"|".join(varkey_list)})_.*\.nc' #to filter with re
+    file_out_prefix = f'era5_{"_".join(varkey_list)}'
+    drop_variables = None
+    rename_variables = None
 else:
     raise Exception('ERROR: wrong mode %s'%(mode))
 
@@ -51,9 +67,20 @@ if not os.path.exists(dir_output):
     os.makedirs(dir_output)
 
 
+#generating file list #TODO: can be more efficient
+if '.*' in fn_match_pattern: #regex complex stuff
+    file_pd_allnc = pd.Series(glob.glob(os.path.join(dir_data,'*.nc'))) #all nc files
+    file_pd_allnc_varnames = file_pd_allnc.str.extract(fn_match_pattern)[0].str.replace('_','')
+    file_pd_allnc_df = pd.DataFrame({'file':file_pd_allnc,'varname':file_pd_allnc_varnames})
+    file_pd_pattern = file_pd_allnc_df.loc[~file_pd_allnc_df['varname'].isnull()]
+    file_nc = file_pd_pattern['file'].tolist()
+    file_list = file_nc
+else:
+    file_nc = os.path.join(dir_data,fn_match_pattern)
+    file_list = glob.glob(file_nc)
+
+print(f'opening multifile dataset of {len(file_list)} files matching "{fn_match_pattern}" (can take a while with lots of files)',end='')
 #open_mfdataset when dropping x/y since both varname as dimname, which is not possible in xarray
-file_nc = os.path.join(dir_data,fn_match_pattern)
-print(f'opening multifile dataset of {len(glob.glob(file_nc))} files (can take a while with lots of files)',end='')
 data_xr = xr.open_mfdataset(file_nc,
                             drop_variables=drop_variables, #necessary since dims/vars with equal names are not allowed by xarray, add again later and requested matroos to adjust netcdf format.
                             parallel=True, #speeds up the process
@@ -119,9 +146,7 @@ if 'cloud_area_fraction' in varkeys:
 
 #write to netcdf file
 print('writing file (can take a while)')
-file_prefix = fn_match_pattern.replace('_*.nc','').replace('*.nc','')
-
-file_out = os.path.join(dir_output, f'{file_prefix}_{tstart_str}to{tstop_str}_{mode}.nc')
+file_out = os.path.join(dir_output, f'{file_out_prefix}_{tstart_str}to{tstop_str}_{mode}.nc')
 data_xr_tsel.to_netcdf(file_out) #TODO: maybe add different reftime?
 
 print('loading outputfile')
@@ -129,22 +154,21 @@ with xr.open_dataset(file_out) as data_xr_check:
     #data_xr_check.close()
     if 1: #optionally plot to check
         print('plotting')
-        import matplotlib.pyplot as plt
-        plt.close('all')
-        fig,ax1 = plt.subplots()
-        if 'northward_wind' in varkeys: # HIRLAM
-            sel_var = 'northward_wind'
-        elif 'air_temperature' in varkeys: # HIRLAM
-            sel_var = 'air_temperature'
-        elif 'so' in varkeys: # HYCOM
-            sel_var = 'so'
-        if 'HIRLAM' in mode:
-            data_xr_check[sel_var].isel(time=0).plot(ax=ax1,x='longitude',y='latitude') #x/y are necessary since coords are not 1D and dims
-        else:
-            data_xr_check[sel_var].isel(time=0).sel(depth=0).plot(ax=ax1)
+        for varkey in varkeys:
+            if 'time' not in data_xr_tsel[varkey].coords:
+                continue
+            if varkey=='time':
+                continue
+            fig,ax1 = plt.subplots()
+            if 'HIRLAM' in mode:
+                data_xr_check[varkey].isel(time=0).plot(ax=ax1,x='longitude',y='latitude') #x/y are necessary since coords are not 1D and dims
+            elif 'depth' in data_xr_tsel[varkey].coords:
+                data_xr_check[varkey].isel(time=0).sel(depth=0).plot(ax=ax1)
+            else:
+                data_xr_check[varkey].isel(time=0).plot(ax=ax1)
 
-script_telapsed = (dt.datetime.now()-script_tstart).total_seconds()/60
-print(f'elapsed time: {script_telapsed/60:.2f} hours or {script_telapsed:.2f} minutes')
+script_telapsed = (dt.datetime.now()-script_tstart)
+print(f'elapsed time: {script_telapsed}')
 
 
 
