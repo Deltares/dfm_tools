b79ca481aeaba2c261ac47f8eca5f9ae5040c34c
veenstrajelmer <veenstrajelmer@gmail.com>
2022-10-17 15:22:42 +0200

    renamed ini example scripts

--- tests/examples/ini_cmems_to_nc.py
+++ tests/examples/ini_cmems_to_nc.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Sep 27 12:14:14 2022
 
-@author: laan_st
+@author: veenstra
 """
 
 import os
@@ -11,12 +11,12 @@ import datetime as dt
 import xarray as xr
 
 tSimStart = dt.datetime(1998,1,1)
-pathBase  = r'p:\i1000668-tcoms\03_newModel\01_input\02_bnd\CMEMS\download\data' #folder containing CMEMS so and thetao netcdf files
+dir_data  = r'p:\i1000668-tcoms\03_newModel\01_input\02_bnd\CMEMS\download\data' #folder containing CMEMS so and thetao netcdf files
 
 dir_out = '.'
 
-file_nc_list_so = glob.glob(f'{pathBase}\\so_*.nc')
-file_nc_list_thetao = glob.glob(f'{pathBase}\\thetao_*.nc')
+file_nc_list_so = glob.glob(f'{dir_data}\\so_*.nc')
+file_nc_list_thetao = glob.glob(f'{dir_data}\\thetao_*.nc')
 file_nc_list = file_nc_list_so + file_nc_list_thetao
 
 data_xr = xr.open_mfdataset(file_nc_list)
