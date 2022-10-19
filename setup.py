7505e286b6fc07beee38bcf30030113105ff3ca7
veenstrajelmer <veenstrajelmer@gmail.com>
2022-10-19 14:33:21 +0200

    bumpversion fix

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/openearth/dfm_tools',
-    version='version='0.7.94'',
+    version='0.7.94',
     zip_safe=False,
 )
