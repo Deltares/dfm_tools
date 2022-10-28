53003999bdd5c1f498936e435e1358de962ac330
veenstrajelmer <veenstrajelmer@gmail.com>
2022-10-28 16:12:26 +0200

    improved preprocess_hisnc() to fix label based indexing for stations/crs/gs

--- dfm_tools/xarray_helpers.py
+++ dfm_tools/xarray_helpers.py
@@ -9,7 +9,7 @@ from netCDF4 import Dataset
 import xarray as xr
 
 
-def preprocess_hisnc(ds, drop_duplicate_stations=True):
+def preprocess_hisnc(ds, drop_duplicate_stations=True, silent=True):
     """
     Loop over keys of dict (potential dimensions) and set as Dataset index if present, to enable label based indexing. If duplicate labels are found (like duplicate stations), these are dropped to avoid indexing issues.
     
@@ -28,13 +28,23 @@ def preprocess_hisnc(ds, drop_duplicate_stations=True):
     """
     dim_name_dict = {'stations':'station_name',
                      'cross_section':'cross_section_name',
-                     'general_structures':'general_structure_id'}
+                     'general_structures':'general_structure_id',
+                     'source_sink':'source_sink_name',
+                     }
+    
+    for ds_dim in ds.dims.keys():
+        if ds_dim in ['time']:#,'laydim','laydimw','source_sink_pts','nFlowElemWithBnd','nFlowElemContourPts','nNetLink','nNetLinkPts','nFlowLink','nFlowLinkPts','station_geom_nNodes','source_sink_geom_nNodes']:
+            continue
+        if not ds_dim in dim_name_dict.keys():
+            if silent:
+                continue
+            print(f'WARNING: dimension "{ds_dim}" does not have an index yet, you could extend the dim_name_dict')
     
     for dim in dim_name_dict.keys():
         if not dim in ds.dims:
             continue
         name = dim_name_dict[dim]
-        name_str = f'{name}_str'
+        name_str = f'{name}_str' #avoid losing the original variable by creating a new name
         ds[name_str] = ds[name].load().str.decode('utf-8',errors='ignore').str.strip() #.load() is essential to convert not only first letter of string.
         ds = ds.set_index({dim:name_str})
         
