3bbfbb9eb790d25a6bb213b57188d5a83666aac8
veenstrajelmer <veenstrajelmer@gmail.com>
2022-10-28 17:18:17 +0200

    added dailymean to gethismodeldata

--- tests/examples/postprocess_gethismodeldata.py
+++ tests/examples/postprocess_gethismodeldata.py
@@ -17,9 +17,9 @@ from dfm_tools.xarray_helpers import preprocess_hisnc
 dir_testinput = r'c:\DATA\dfm_tools_testdata'
 dir_output = '.'
 
-file_nc_list = [#os.path.join(dir_testinput,'vanNithin','tttz_0000_his.nc'),
-                #os.path.join(dir_testinput,'DFM_3D_z_Grevelingen\\computations\\run01\\DFM_OUTPUT_Grevelingen-FM\\Grevelingen-FM_0000_his.nc'),
-                #r'p:\11202512-h2020_impaqt\07_Mediterranean_model\MedSea_impaqt_model\computations_final\r013_waq\DFM_OUTPUT_MedSea_impaqt_FM\MedSea_impaqt_FM_0000_his.nc',
+file_nc_list = [os.path.join(dir_testinput,'vanNithin','tttz_0000_his.nc'),
+                os.path.join(dir_testinput,'DFM_3D_z_Grevelingen\\computations\\run01\\DFM_OUTPUT_Grevelingen-FM\\Grevelingen-FM_0000_his.nc'),
+                r'p:\11202512-h2020_impaqt\07_Mediterranean_model\MedSea_impaqt_model\computations_final\r013_waq\DFM_OUTPUT_MedSea_impaqt_FM\MedSea_impaqt_FM_0000_his.nc',
                 r'p:\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_206\results\RMM_dflowfm_0000_his.nc', #added since there are duplicate stations which are dropped
                 ]
 
