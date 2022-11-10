2e36443869f942e756b9fe7e1bb7f312c61c40a7
veenstrajelmer <veenstrajelmer@gmail.com>
2022-11-10 11:34:43 +0100

    fixed cmems download script and function

--- dfm_tools/download.py
+++ dfm_tools/download.py
@@ -75,19 +75,30 @@ def download_CMEMS(varkey,
                    date_min, date_max,
                    longitude_min, longitude_max, latitude_min, latitude_max, 
                    dir_output='.', overwrite=False,
-                   source_combination=None, #str from source_dict.keys(). Or provide motu_url/service/product arguments
-                   motu_url=None, service=None, product=None, #or provide source_combination argument
-                   credentials=None, #credentials=['username','password'], or create "%USER%/motucredentials.txt" with username on line 1 and password on line 2. Register at: https://resources.marine.copernicus.eu/registration-form'
-                   timeout=30, #in seconds #TODO: set timeout back to 300?
-                   opendap=True):
+                   dataset_id=None,
+                   credentials=None): #credentials=['username','password'], or create "%USER%/motucredentials.txt" with username on line 1 and password on line 2. Register at: https://resources.marine.copernicus.eu/registration-form'
+
     """
-    How to get motu_url/service/product:
-        - find your service+product on https://resources.marine.copernicus.eu/products
-        - go to the data access tab, e.g.: https://resources.marine.copernicus.eu/product-detail/GLOBAL_MULTIYEAR_PHY_001_030/DATA-ACCESS
-        - click on the API button in the SUBS column, e.g.: https://my.cmems-du.eu/motu-web/Motu?action=describeproduct&service=GLOBAL_MULTIYEAR_PHY_001_030-TDS&product=cmems_mod_glo_phy_my_0.083_P1D-m
-        - from this url you see the motu_url (https://my.cmems-du.eu), the service (GLOBAL_MULTIYEAR_PHY_001_030-TDS) and the product (cmems_mod_glo_phy_my_0.083_P1D-m)
-        - some example combinations are available in source_dict
-    Some examples can be found in source_dict
+    How to get the opendap dataset_id:
+        - https://data.marine.copernicus.eu/products
+        - go to the data access tab of a product, e.g.: https://data.marine.copernicus.eu/product-detail/GLOBAL_MULTIYEAR_PHY_001_030/DATA-ACCESS
+        - click the opendap link of the dataset of your choice
+        - the dataset_id is the last part of the url (excl .html), e.g.: cmems_mod_glo_phy_my_0.083_P1D-m
+        
+        Some examples:
+            'multiyear_physchem':{'motu_url':'https://my.cmems-du.eu', # multiyear reanalysis data (time extent was once 01-01-1993 12:00 till 31-05-2020 12:00)
+                                  'product': 'GLOBAL_MULTIYEAR_PHY_001_030',
+                                  'dataset_id': 'cmems_mod_glo_phy_my_0.083_P1D-m'},
+            'multiyear_bio':     {'motu_url':'https://my.cmems-du.eu',
+                                  'product': 'GLOBAL_MULTIYEAR_BGC_001_029',
+                                  'dataset_id': 'cmems_mod_glo_bgc_my_0.25_P1D-m'},
+            'forecast_physchem': {'motu_url':'https://nrt.cmems-du.eu', # operational forecast data (time extent was once 01-01-2019 12:00 till now + several days)
+                                  'product': 'GLOBAL_ANALYSIS_FORECAST_PHY_001_024',
+                                  'dataset_id': 'global-analysis-forecast-phy-001-024'},
+            'forecast_bio':      {'motu_url':'https://nrt.cmems-du.eu',
+                                  'product': 'GLOBAL_ANALYSIS_FORECAST_BIO_001_028',
+                                  'dataset_id': 'global-analysis-forecast-bio-001-028-daily'},
+    
     """
     
     #get credentials
@@ -101,70 +112,46 @@ def download_CMEMS(varkey,
     else:
         username,password = credentials
     
-    source_dict =   {'multiyear_physchem':{'motu_url':'https://my.cmems-du.eu', # multiyear reanalysis data (01-01-1993 12:00 till 31-05-2020 12:00)
-                                           'service': 'GLOBAL_MULTIYEAR_PHY_001_030-TDS',
-                                           'product': 'cmems_mod_glo_phy_my_0.083_P1D-m'},
-                     'multiyear_bio':     {'motu_url':'https://my.cmems-du.eu',
-                                           'service': 'GLOBAL_MULTIYEAR_BGC_001_029-TDS',
-                                           'product': 'cmems_mod_glo_bgc_my_0.25_P1D-m'},
-                     'forecast_physchem': {'motu_url':'https://nrt.cmems-du.eu', # operational forecast data (01-01-2019 12:00 till now + several days)
-                                           'service': 'GLOBAL_ANALYSIS_FORECAST_PHY_001_024-TDS',
-                                           'product': 'global-analysis-forecast-phy-001-024'},
-                     'forecast_bio':      {'motu_url':'https://nrt.cmems-du.eu',
-                                           'service': 'GLOBAL_ANALYSIS_FORECAST_BIO_001_028-TDS',
-                                           'product': 'global-analysis-forecast-bio-001-028-daily'},
-                     }
-    
-    if source_combination is not None:
-        if (motu_url is not None) or (service is not None) or (product is not None):
-            raise Exception('motu_url, service and/or product arguments provided while source_combination is also provided.')
-        if not source_combination in source_dict.keys():
-            raise Exception(f'provided source_combination={source_combination} argument is not valid, options are {list(source_dict.keys())}. Alternatively provide motu_url/service/product arguments.')
-        motu_url = source_dict[source_combination]['motu_url']
-        service = source_dict[source_combination]['service']
-        product = source_dict[source_combination]['product']
-    
-    if opendap:
+    def copernicusmarine_datastore(dataset, username, password):
         #https://help.marine.copernicus.eu/en/articles/5182598-how-to-consume-the-opendap-api-and-cas-sso-using-python
-        def copernicusmarine_datastore(dataset, username, password, motu_url):
-            from pydap.client import open_url #TODO: add pydap as dependency (pip or conda?)
-            from pydap.cas.get_cookies import setup_session
-            cas_url = 'https://cmems-cas.cls.fr/cas/login'
-            session = setup_session(cas_url, username, password)
-            cookies_dict = session.cookies.get_dict()
-            if not 'CASTGC' in cookies_dict.keys():
-                raise Exception('CASTGC key missing from session cookies_dict, probably authentication failure')
-            session.cookies.set("CASTGC", cookies_dict['CASTGC'])
-            database = ['my', 'nrt']
-            url = f'https://{database[0]}.cmems-du.eu/thredds/dodsC/{dataset}'
-            try: #TODO: add check for wrong dataset_id
-                DAP_dataset = open_url(url, session=session)#, user_charset='utf-8') # TODO: user_charset needs PyDAP >= v3.3.0 see https://github.com/pydap/pydap/pull/223/commits 
-            except:
-                url = f'https://{database[1]}.cmems-du.eu/thredds/dodsC/{dataset}'
-                DAP_dataset = open_url(url, session=session)#, user_charset='utf-8') # TODO: user_charset needs PyDAP >= v3.3.0 see https://github.com/pydap/pydap/pull/223/commits 
-            data_store = xr.backends.PydapDataStore(DAP_dataset)
-            return data_store
-
-        dataset_id = product #service/product
-        #opendap_url = f'{motu_url}/thredds/dodsC/{dataset_id}'
-        print('opening connection to opendap general dataset and opening with xarray') #TODO: do this only once (outside of loop)
-        data_store = copernicusmarine_datastore(dataset=dataset_id, username=username, password=password, motu_url=motu_url)
-        data_xr = xr.open_dataset(data_store)
-        
-        print('subsetting data (lon/lat extents) with xarray')
-        if varkey not in data_xr.data_vars:
-            raise Exception(f'{varkey} not found in dataset, available are: list(data_xr.data_vars)\n{data_xr.data_vars}')
-        data_xr_var = data_xr[[varkey]]
-        data_xr_var = data_xr_var.sel(longitude=slice(longitude_min,longitude_max), #TODO: add depth selection?
-                                      latitude=slice(latitude_min,latitude_max))
-        print(data_xr_var.time.to_series()) #TODO: add check if all times are available
-    else:
-        import motuclient #used in motu_commands, so has to be importable. conda install -c conda-forge motuclient #TODO: move to top of script (then make dependency of dfm_tools)
-        #test if supplied motu_url is valid
-        requests.get(motu_url)
+        from pydap.client import open_url #TODO: add pydap as dependency (pip or conda?)
+        from pydap.cas.get_cookies import setup_session
+        cas_url = 'https://cmems-cas.cls.fr/cas/login'
+        session = setup_session(cas_url, username, password)
+        cookies_dict = session.cookies.get_dict()
+        if not 'CASTGC' in cookies_dict.keys():
+            raise Exception('CASTGC key missing from session cookies_dict, probably authentication failure')
+        session.cookies.set("CASTGC", cookies_dict['CASTGC'])
+        try: #TODO: add check for wrong dataset_id
+            url = f'https://my.cmems-du.eu/thredds/dodsC/{dataset}'
+            DAP_dataset = open_url(url, session=session)#, user_charset='utf-8') # TODO: user_charset needs PyDAP >= v3.3.0 see https://github.com/pydap/pydap/pull/223/commits 
+        except:
+            url = f'https://nrt.cmems-du.eu/thredds/dodsC/{dataset}'
+            DAP_dataset = open_url(url, session=session)#, user_charset='utf-8') # TODO: user_charset needs PyDAP >= v3.3.0 see https://github.com/pydap/pydap/pull/223/commits 
+        finally:
+            print(f'opendap dataset found at {url}.html')
+        data_store = xr.backends.PydapDataStore(DAP_dataset)
+        return data_store
     
+    print('opening connection to opendap dataset and opening dataset with xarray')
+    data_store = copernicusmarine_datastore(dataset=dataset_id, username=username, password=password)
+    data_xr = xr.open_dataset(data_store)
+    
+    print('xarray subsetting data (lon/lat extents)')
+    if varkey not in data_xr.data_vars:
+        raise Exception(f'{varkey} not found in dataset, available are: list(data_xr.data_vars)\n{data_xr.data_vars}')
+    data_xr_var = data_xr[[varkey]]
+    data_xr_var = data_xr_var.sel(longitude=slice(longitude_min,longitude_max), #TODO: add depth selection?
+                                  latitude=slice(latitude_min,latitude_max))
+    data_xr_times = data_xr_var.time.to_series()
+    print(f'available time range in dataset from {data_xr_times.index[0]} to {data_xr_times.index[-1]}')
     period_range = pd.period_range(date_min,date_max,freq='D')
-    print(f'retrieving data from {period_range[0]} to {period_range[-1]} (freq={period_range.freq})')
+    
+    #check if date_min/date_max are available in dataset
+    if not (data_xr_times.index[0] < period_range[0].to_timestamp() < data_xr_times.index[-1]):
+        raise Exception(f'date_min ({period_range[0]}) is outside available time range in dataset: {data_xr_times.index[0]} to {data_xr_times.index[-1]}')
+    if not (data_xr_times.index[0] < period_range[-1].to_timestamp() < data_xr_times.index[-1]):
+        raise Exception(f'date_max ({period_range[-1]}) is outside available time range in dataset: {data_xr_times.index[0]} to {data_xr_times.index[-1]}')
     
     for date in period_range:
         date_str = date.strftime('%Y-%m-%d')
@@ -173,36 +160,13 @@ def download_CMEMS(varkey,
         if file_out.is_file() and not overwrite:
             print(f'"{name_output}" found and overwrite=False, continuing.')
             continue
-        print(f'retrieving variable {varkey} for {date_str}: {name_output}')
-        if opendap:
-            print('subsetting data (time extent) with xarray')
-            data_xr_var_seltime = data_xr_var.sel(time=slice(date_str,date_str)) #+' 12:00:00', 
-            print('writing netcdf file with xarray')
-            data_xr_var_seltime.to_netcdf(os.path.join(dir_output,name_output))
-        else: #TODO: remove motuclient
-            #TODO: implement retry after a brief timeout in case of server failure?
-            motu_command = ' '.join(['motuclient', '--motu', f'{motu_url}/motu-web/Motu', '--service-id', service, '--product-id', product,
-                                     '--longitude-min', str(longitude_min), '--longitude-max', str(longitude_max),
-                                     '--latitude-min', str(latitude_min), '--latitude-max', str(latitude_max),
-                                     '--date-min', date_str, '--date-max', date_str, #+' 12:00:00',
-                                     '--depth-min', '0', '--depth-max', '2e31',
-                                     '--variable', str(varkey),
-                                     '--out-dir', dir_output, '--out-name', name_output,
-                                     '--user', username, '--pwd', password])
+        
+        print(f'xarray subsetting data per {period_range.freq}: {date_str}')
+        data_xr_var_seltime = data_xr_var.sel(time=slice(date_str,date_str)) #+' 12:00:00', 
             
-            try: #this try/except is necessary to capture/raise the motuclient logging in the 'finally' of the loop. Otherwise user only gets unclear error message: "CalledProcessError: Command [] returned non-zero exit status 1." 
-                out = subprocess.run(f'python -m {motu_command}', capture_output=True, check=True, universal_newlines=True, timeout=timeout)
-            except Exception as e: # capture error and save as out for the finally loop
-                out = e 
-                raise Exception(e)
-            finally: #to capture also ERRORS/WARNINGS in logging that are not raised by motuclient
-                if ('ERROR' in out.stdout) or ('WARNING' in out.stdout): #catch all other errors, and the relevant information in TimeoutExpired and CalledProcessError
-                    if 'variable not found' in out.stdout:
-                        raise Exception(f'ERROR/WARNING found in motuclient logging:\nOUT: {out.stdout}\nERR: {out.stderr}\n\nNetCDF variable {varkey} not found: check available variables at: {motu_url}/motu-web/Motu?action=describeproduct&service={service}&product={product}')
-                    else:
-                        raise Exception(f'ERROR/WARNING found in motuclient logging:\nOUT: {out.stdout}\nERR: {out.stderr}')
-                #else:
-                #    print(f'OUT: {out.stdout}\nERR: {out.stderr}')
-
+        print(f'writing netcdf file with xarray: {name_output}')
+        data_xr_var_seltime.to_netcdf(os.path.join(dir_output,name_output)) #TODO: add chunks={'time':1} or only possible with opening?
+        data_xr_var_seltime.close()
+    
     print('done')
     return
--- tests/examples/download_meteodata_oceandata.py
+++ tests/examples/download_meteodata_oceandata.py
@@ -18,19 +18,19 @@ from dfm_tools.download import download_ERA5, download_CMEMS
 #TODO: add GFS (but opendap is not an archive: https://stackoverflow.com/questions/65031973/how-to-select-specific-data-variables-from-xarray-dataset)
 #TODO: add click?
 
-overwrite = False # always set to True when changing the domain
+overwrite = True # always set to True when changing the domain
 
 # domain
 longitude_min, longitude_max, latitude_min, latitude_max =    2,   4,  50, 52 #test domain
 #longitude_min, longitude_max, latitude_min, latitude_max = -180, 180, -90, 90 #global
 
 #dates as understood by pandas.period_range(). ERA5 has freq='M' (month) and CMEMS has freq='D' (day)
-date_min = '2010-01-01'
-date_max = '2010-01-02'
+date_min = '2010-01-02'
+date_max = '2010-01-03'
 
 #variables per model will be written to separate netcdf files. Set to [] to skip model.
-variables_era5 = ['v10n'] # supply arbitrary string to get error with available variable names
-varlist_cmems = ['bottomT','thetao','no3'] # avaliable variables differ per source_combination, check cmems loop for some options
+variables_era5 = []#'v10n'] # supply arbitrary string to get error with available variable names
+varlist_cmems = ['bottomT']#,'thetao','no3'] # avaliable variables differ per source_combination, check cmems loop for some options
 
 #output directories per model
 dir_output_era5 = './era5_temp'
@@ -61,28 +61,26 @@ date_min_cmems = pd.Timestamp(date_min)-pd.Timedelta(days=1) #CMEMS has daily no
 for varkey in varlist_cmems:
     Path(dir_output).mkdir(parents=True, exist_ok=True)
     
-    if varkey in ['bottomT','mlotst','siconc','sithick','so','thetao','uo','usi','vo','vsi','zos']: #for multiyear_physchem and forecast_physchem
-        source_combination='multiyear_physchem'
-    else: # ['chl','no3','nppv','o2','po4','si'] for multiyear_bio and ['chl','fe','no3','nppv','o2','ph','phyc','po4','si','spco2'] for forecast_bio
-        source_combination='multiyear_bio'
+    if varkey in ['bottomT','mlotst','siconc','sithick','so','thetao','uo','usi','vo','vsi','zos']: #for physchem: 'cmems_mod_glo_phy_my_0.083_P1D-m' and 'global-analysis-forecast-phy-001-024'
+        dataset_id='cmems_mod_glo_phy_my_0.083_P1D-m'
+    else: # ['chl','no3','nppv','o2','po4','si'] for bio: 'cmems_mod_glo_bgc_my_0.25_P1D-m' and ['chl','fe','no3','nppv','o2','ph','phyc','po4','si','spco2'] for 'global-analysis-forecast-bio-001-028-daily'
+        dataset_id='cmems_mod_glo_bgc_my_0.25_P1D-m'
     
     download_CMEMS(varkey,
                    date_min=date_min_cmems, date_max=date_max,
                    longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
                    dir_output=dir_output, overwrite=overwrite,
-                   source_combination=source_combination, #'multiyear_physchem', #str from source_dict.keys(). Or provide motu_url/service/product arguments
-                   #motu_url='https://my.cmems-du.eu', service='GLOBAL_MULTIYEAR_PHY_001_030-TDS', product='cmems_mod_glo_phy_my_0.083_P1D-m', #or provide source_combination argument
-                   credentials=None, #credentials=['username','password'], or create "%USER%/motucredentials.txt" with username on line 1 and password on line 2. Register at: https://resources.marine.copernicus.eu/registration-form'
-                   timeout=200) #default is 30, but this might be not always be enough to avoid TimeoutExpired error.
+                   dataset_id=dataset_id,
+                   credentials=None) #credentials=['username','password'], or create "%USER%/motucredentials.txt" with username on line 1 and password on line 2. Register at: https://resources.marine.copernicus.eu/registration-form'
     
     #open mfdataset to check folder contents and plot first field of each variable
     ds = xr.open_mfdataset(os.path.join(dir_output,f'cmems_{varkey}_*.nc'))
-    ds.close()
     fig,ax = plt.subplots()
     if 'depth' in ds[varkey].dims:
         ds[varkey].isel(time=0,depth=0).plot(ax=ax)
     else:
         ds[varkey].isel(time=0).plot(ax=ax)
+    ds.close()
 
 
 
