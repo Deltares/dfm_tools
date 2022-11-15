5af7763a076d31f0d2c8ca5aa25ca37b728ece83
veenstrajelmer <veenstrajelmer@gmail.com>
2022-11-15 08:06:58 +0100

    added star imports in init file

--- dfm_tools/__init__.py
+++ dfm_tools/__init__.py
@@ -5,3 +5,14 @@
 __author__ = """Jelmer Veenstra"""
 __email__ = 'jelmer.veenstra@deltares.nl'
 __version__ = '0.7.129'
+
+from download import *
+from get_nc import *
+from get_nc_helpers import *
+from hydrolib_helpers import *
+from interpolate_grid2bnd import *
+from linebuilder import *
+from modplot import *
+from regulargrid import *
+from ugrid import *
+from xarray_helpers import *
