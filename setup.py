1facfc33b5743e13712b6a3c89ce2075fc10a51e
veenstrajelmer <veenstrajelmer@gmail.com>
2022-11-19 00:57:28 +0100

    Bump version: 0.8.0 → 0.8.1

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/openearth/dfm_tools',
-    version='0.8.0',
+    version='0.8.1',
     zip_safe=False,
 )
