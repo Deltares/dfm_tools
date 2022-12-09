f8d7c70f3963f49b2e151da1e48801b67e644fae
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-09 17:15:38 +0100

    get value on z-value example script

--- tests/examples_workinprogress/workinprogress_map_getfixeddepth.py
+++ tests/examples_workinprogress/workinprogress_map_getfixeddepth.py
@@ -16,13 +16,16 @@ import dfm_tools as dfmt
 dir_testinput = r'c:\DATA\dfm_tools_testdata'
 dir_output = '.'
 
-file_nc_list = [#os.path.join(dir_testinput,'DFM_sigma_curved_bend\\DFM_OUTPUT_cb_3d\\cb_3d_map.nc'), #sigmalayer
+file_nc_list = [os.path.join(dir_testinput,'DFM_sigma_curved_bend\\DFM_OUTPUT_cb_3d\\cb_3d_map.nc'), #sigmalayer
                 #os.path.join(dir_testinput,'DFM_3D_z_Grevelingen','computations','run01','DFM_OUTPUT_Grevelingen-FM','Grevelingen-FM_0000_map.nc'), #zlayer
-                r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc', #fullgrid
+                #r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc', #fullgrid
                 #r'p:\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0000_map.nc', #2D model
                 #r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0000_map.nc',
                 ]
 timestep = 3
+varname = 'mesh2d_sa1'
+fixed_depth = -50
+clim_sal = [32,36] #should be taken care of automatically, but max salinity >200 for some reason
 
 for file_nc in file_nc_list:
     print('processing %s'%(os.path.basename(file_nc)))
@@ -30,47 +33,71 @@ for file_nc in file_nc_list:
     
     
     data_frommap_merged = dfmt.open_partitioned_dataset(file_nc.replace('_0000_','_0*_')) #TODO: make starred default, but not supported by older code
-    """
-    #example for how subsetting works
-    data_xr_grid = data_frommap_merged.ugrid.grid.to_dataset()
-    face_nos = data_xr_grid.mesh2d_face_nodes.isel(mesh2d_nMax_face_nodes=0).load()
-    print(face_nos)
-    face_nnodecoords_x = data_xr_grid.mesh2d_node_x.isel(mesh2d_nNodes=face_nos)
-    """
+    
+    dtstart_all = dt.datetime.now()
+    
     #get ugrid data, vars informatin and grid units (latter from bedlevel coordinates)
     vars_pd = dfmt.get_ncvarproperties(file_nc=file_nc)
     
     print('plot grid and values from mapdata (salinity on layer, 3dim, on cell centers) >> on fixed depth (currently only for z-layer models)')
-    data_frommap_sel = data_frommap_merged['mesh2d_sa1'].isel(time=timestep) #select data for all layers
+    data_frommap_timesel = data_frommap_merged.isel(time=timestep) #select data for all layers
     
-    #TODO: retrieve on z-depth is not possible yet for fullgrid/zsigma (mesh2d_flowelem_zw) or sigma (mesh2d_layer_sigma)
-    fixed_depth = -50
-    clim_sal = [32,36] #should be taken care of automatically, but max salinity >200 for some reason
-
+    def get_mapdata_onfixedepth(data_xr_map, z, varname=None):
+        """
+        data_xr_map:
+            has to be dataset, otherwise mesh2d_flowelem_zw etc are not available (interface z values)
+            in case of zsigma layers (or fullgrid), it cannot contain a time dimension
+        #TODO: retrieve on z-depth is not possible yet for sigma (mesh2d_layer_sigma)
+        """
+        
+        print('>> subsetting data on fixed depth: ',end='')
+        dtstart = dt.datetime.now()
+        
+        if not 'nmesh2d_layer' in data_xr_map.dims: #TODO: maybe raise exception?
+            print(f'{(dt.datetime.now()-dtstart).total_seconds():.2f} sec')
+            print('WARNING: depth variable not found, returning input Dataset')
+            return data_xr_map
+        
+        if varname is not None:
+            data_xr_map_var = data_xr_map[varname]
+        else:
+            data_xr_map_var = data_xr_map
+        #from dask.diagnostics import ProgressBar
+        #with ProgressBar():
+        if 'mesh2d_flowelem_zcc' in data_xr_map.coords:
+            print('[zsigma/fullgrid model]',end='')
+            if 'time' in data_xr_map.dims:
+                raise Exception('ERROR: supply dataset with only one time for models with zsigma/sigma layers')
+            bool_valid = data_xr_map.mesh2d_flowelem_zw.min(dim='mesh2d_nInterfaces') <= z #TODO suppress warning: C:\Users\veenstra\Anaconda3\envs\dfm_tools_env\lib\site-packages\dask\array\reductions.py:640: RuntimeWarning: All-NaN slice encountered. return np.nanmax(x_chunk, axis=axis, keepdims=keepdims)
+            bool_mindist = data_xr_map.nmesh2d_layer==abs(data_xr_map.mesh2d_flowelem_zcc - z).argmin(dim='nmesh2d_layer').load()
+            print('performing .where() on fixed depth for zsigma/fullgrid model')
+            data_frommap_ondepth = data_xr_map_var.where(bool_valid&bool_mindist).max(dim='nmesh2d_layer') #set all layers but one to nan, followed by an arbitrary reduce (max in this case)
+        elif 'mesh2d_layer_z' in data_xr_map.coords: #TODO: would be better to take interfaces into account also (it currently interpolates between z-center values)
+            print('[z model]',end='')
+            dict_layer_num2z = {'nmesh2d_layer':'mesh2d_layer_z'} #TODO: also transfer attrs from z to lay
+            data_xr_map_var = data_xr_map_var.set_index(dict_layer_num2z).rename(dict_layer_num2z) #set depth as index on layers, to be able to interp to depths instead of layernumbers
+            data_frommap_ondepth = data_xr_map_var.interp(mesh2d_layer_z=z,kwargs=dict(bounds_error=True)) #interpolate to fixed z-depth
+        elif 'mesh2d_layer_sigma' in data_xr_map.coords:
+            print('[sigma model]',end='')
+            raise Exception('get_mapdata_onfixedepth does not support sigma-layer models yet (mesh2d_layer_sigma), let us know if you need it')
+            if 'time' in data_xr_map.dims:
+                raise Exception('ERROR: supply dataset with only one time for models with zsigma/sigma layers')
+        else:
+            raise Exception('layers present, but unknown layertype')
+        print(f'{(dt.datetime.now()-dtstart).total_seconds():.2f} sec')
+        
+        return data_frommap_ondepth
+    
+    if 1: #subset variable after onfixeddepth function
+        data_frommap_timesel_ondepth = get_mapdata_onfixedepth(data_xr_map=data_frommap_timesel, z=fixed_depth)
+        data_frommap_timesel_ondepth = data_frommap_timesel_ondepth[varname]
+    else:
+        data_frommap_timesel_ondepth = get_mapdata_onfixedepth(data_xr_map=data_frommap_timesel, z=fixed_depth, varname=varname)
     
     fig, ax = plt.subplots()
-    if 'mesh2d_flowelem_zcc' in data_frommap_sel.coords: #TODO: maybe do it a bit more exact with mesh2d_flowelem_zw (mesh2d_nInterfaces) instead
-        bool_valid = data_frommap_merged.isel(time=timestep).mesh2d_flowelem_zw.min(dim='mesh2d_nInterfaces') <= fixed_depth #TODO: C:\Users\veenstra\Anaconda3\envs\dfm_tools_env\lib\site-packages\dask\array\reductions.py:640: RuntimeWarning: All-NaN slice encountered. return np.nanmax(x_chunk, axis=axis, keepdims=keepdims)
-        bool_mindist = data_frommap_sel.nmesh2d_layer==abs(data_frommap_sel.mesh2d_flowelem_zcc - fixed_depth).argmin(dim='nmesh2d_layer').load()
-        from dask.diagnostics import ProgressBar
-        print('performing isel on fixed depth for zsigma/fullgrid model')
-        with ProgressBar():
-            #data_frommap_ondepth = data_frommap_sel.isel(nmesh2d_layer=isel_array) #TODO: this results in <xarray.DataArray 'mesh2d_sa1' (mesh2d_nFaces: 289713, nmesh2d_layer: 289713)> (should not have layer dimension) #Huite commented that .where(boolean) works here, not .isel(ints): https://stackoverflow.com/questions/66027917/python-xarray-vectorized-indexing/66126022#66126022
-            data_frommap_ondepth = data_frommap_sel.where(bool_valid&bool_mindist).max(dim='nmesh2d_layer') #set all layers but one to nan, than reduce (take max value in this case, but is arbitrary)
-        if len(data_frommap_ondepth.shape)>1:
-            raise Exception('should have 1 dimension')
-        
-    elif 'mesh2d_layer_z' in data_frommap_sel.coords:
-        data_frommap_sel = data_frommap_sel.set_index({'nmesh2d_layer':'mesh2d_layer_z'}) #set depth as index on layers
-        data_frommap_ondepth = data_frommap_sel.interp(nmesh2d_layer=fixed_depth) #interpolate to fixed z-depth
-    elif 'mesh2d_layer_sigma' in data_frommap_sel.coords:
-        raise Exception('mesh2d_layer_sigma not yet supported')
-    else:
-        print('WARNING: depth variable not found, returning the same dataarray')
-        data_frommap_ondepth = data_frommap_sel
-    
-    pc = data_frommap_ondepth.ugrid.plot(edgecolor='face',cmap='jet')
+    pc = data_frommap_timesel_ondepth.ugrid.plot(edgecolor='face',cmap='jet')
     pc.set_clim(clim_sal)
     ax.set_aspect('equal')
     fig.tight_layout()
-    fig.savefig(os.path.join(dir_output,f'{basename}_mesh2d_sa1_onfixeddepth'))
+    #fig.savefig(os.path.join(dir_output,f'{basename}_mesh2d_sa1_onfixeddepth'))
+    print(f'script runtime (excl opening mapfile): {(dt.datetime.now()-dtstart_all).total_seconds():.2f} sec')
