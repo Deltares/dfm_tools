d5a1cc8c6dd10794d79522acd22cc658b507b255
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-10 00:28:44 +0100

    added reference levels for get_mapatdepth

--- tests/examples_workinprogress/workinprogress_getatdepth_reference_z_wl_bed.py
+++ tests/examples_workinprogress/workinprogress_getatdepth_reference_z_wl_bed.py
@@ -17,8 +17,8 @@ import dfm_tools as dfmt
 dir_testinput = r'c:\DATA\dfm_tools_testdata'
 dir_output = '.'
 
-file_nc_list = [#os.path.join(dir_testinput,'DFM_sigma_curved_bend\\DFM_OUTPUT_cb_3d\\cb_3d_map.nc'), #sigmalayer
-                os.path.join(dir_testinput,'DFM_3D_z_Grevelingen','computations','run01','DFM_OUTPUT_Grevelingen-FM','Grevelingen-FM_0000_map.nc'), #zlayer
+file_nc_list = [os.path.join(dir_testinput,'DFM_sigma_curved_bend\\DFM_OUTPUT_cb_3d\\cb_3d_map.nc'), #sigmalayer
+                #os.path.join(dir_testinput,'DFM_3D_z_Grevelingen','computations','run01','DFM_OUTPUT_Grevelingen-FM','Grevelingen-FM_0000_map.nc'), #zlayer
                 #r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc', #fullgrid
                 #r'p:\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0000_map.nc', #2D model
                 #r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0000_map.nc',
@@ -32,6 +32,8 @@ def get_mapdata_atdepth(data_xr_map, depth, reference='z0', varname=None, zlayer
     data_xr_map:
         has to be Dataset (not a DataArray), otherwise mesh2d_flowelem_zw etc are not available (interface z values)
         in case of zsigma/sigma layers (or fullgrid), it is advisable to .sel()/.isel() the time dimension first, because that is less computationally heavy
+    depth:
+        depth w.r.t. reference level. If reference=='waterlevel', depth>0 returns only nans. If reference=='bedlevel', depth<0 returns only nans. 
     reference:
         compute depth w.r.t. z0/waterlevel/bed
         default: reference='z0'
@@ -81,8 +83,8 @@ def get_mapdata_atdepth(data_xr_map, depth, reference='z0', varname=None, zlayer
         zw_reference = data_xr_map.mesh2d_flowelem_zw - data_wl
         zcc_reference = data_xr_map.mesh2d_flowelem_zcc - data_wl
     elif reference=='bedlevel':
-        zw_reference = data_xr_map.mesh2d_flowelem_zw + data_bl
-        zcc_reference = data_xr_map.mesh2d_flowelem_zcc + data_bl
+        zw_reference = data_xr_map.mesh2d_flowelem_zw - data_bl
+        zcc_reference = data_xr_map.mesh2d_flowelem_zcc - data_bl
     else:
         raise Exception(f'unknown reference: {reference}')
     
@@ -96,10 +98,11 @@ def get_mapdata_atdepth(data_xr_map, depth, reference='z0', varname=None, zlayer
         
     if 'time' in data_xr_map.dims:
         warnings.warn(UserWarning('get_mapdata_onfixedepth() can be very slow when supplying dataset with time dimension for zsigma/sigma models'))
-    bool_valid = zw_reference.min(dim='nmesh2d_interface') <= depth #TODO suppress warning: C:\Users\veenstra\Anaconda3\envs\dfm_tools_env\lib\site-packages\dask\array\reductions.py:640: RuntimeWarning: All-NaN slice encountered. return np.nanmax(x_chunk, axis=axis, keepdims=keepdims)
+    bool_valid_wl = zw_reference.max(dim='nmesh2d_interface') >= depth #TODO suppress warning: C:\Users\veenstra\Anaconda3\envs\dfm_tools_env\lib\site-packages\dask\array\reductions.py:640: RuntimeWarning: All-NaN slice encountered. return np.nanmax(x_chunk, axis=axis, keepdims=keepdims)
+    bool_valid_bl = zw_reference.min(dim='nmesh2d_interface') <= depth #TODO suppress warning: C:\Users\veenstra\Anaconda3\envs\dfm_tools_env\lib\site-packages\dask\array\reductions.py:640: RuntimeWarning: All-NaN slice encountered. return np.nanmax(x_chunk, axis=axis, keepdims=keepdims)
     bool_mindist = data_xr_map.nmesh2d_layer==abs(zcc_reference - depth).argmin(dim='nmesh2d_layer').load()
     print('performing .where() on fixed depth for zsigma/fullgrid model')
-    data_xr_map_ondepth = data_xr_map_var.where(bool_valid&bool_mindist).max(dim='nmesh2d_layer',keep_attrs=True) #set all layers but one to nan, followed by an arbitrary reduce (max in this case)
+    data_xr_map_ondepth = data_xr_map_var.where(bool_valid_wl&bool_valid_bl&bool_mindist).max(dim='nmesh2d_layer',keep_attrs=True) #set all layers but one to nan, followed by an arbitrary reduce (max in this case)
     #add zvalue as coordinate
     data_xr_map_ondepth[depth_name] = depth
     data_xr_map_ondepth[depth_name] = data_xr_map_ondepth[depth_name].assign_attrs({'units':'m'})
@@ -119,15 +122,25 @@ for file_nc in file_nc_list:
     vars_pd = dfmt.get_ncvarproperties(file_nc=file_nc)
     timestep = 3
     clim_sal = [28, 36]
-    depth = -1#-4
+    depth = -0.1#-4
 
     print('plot grid and values from mapdata (salinity on layer, 3dim, on cell centers) >> on fixed depth')
     data_frommap_timesel = data_frommap_merged.isel(time=timestep) #select data for all layers
     data_frommap_timesel_ondepth = get_mapdata_atdepth(data_xr_map=data_frommap_timesel, depth=depth, 
-                                                       reference='z0', zlayer_z0_interp=True)
+                                                       reference='bedlevel', zlayer_z0_interp=True)
     fig, ax = plt.subplots()
     pc = data_frommap_timesel_ondepth['mesh2d_sa1'].ugrid.plot(edgecolor='face',cmap='jet')
     pc.set_clim(clim_sal)
     ax.set_aspect('equal')
     fig.tight_layout()
+
+    #reference for bottom layer
+    fig, ax = plt.subplots()
+    data_var = data_frommap_timesel.ffill(dim='nmesh2d_layer').bfill(dim='nmesh2d_layer') #replace nans in toplayer with first non-nan value in layers below (and for bottomlayer with first non-nan value in layers above)
+    data_var_sel = data_var.isel(nmesh2d_layer=0) #since nans are now filled, -1 provides the toplayer of the model and 0 the bottomlayer
+    pc = data_var_sel['mesh2d_sa1'].ugrid.plot(edgecolor='face',cmap='jet')
+    pc.set_clim(clim_sal)
+    ax.set_aspect('equal')
+    fig.tight_layout()
+
     
