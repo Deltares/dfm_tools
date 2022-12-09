f02c7fec59111a74a29e4706d88aa0e8efb2683c
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-09 11:29:14 +0100

    added plot_edges example script

--- tests/examples/postprocess_mapnc_ugrid.py
+++ tests/examples/postprocess_mapnc_ugrid.py
@@ -22,9 +22,6 @@ file_nc_list = [os.path.join(dir_testinput,'DFM_sigma_curved_bend\\DFM_OUTPUT_cb
                 r'p:\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0000_map.nc', #2D model
                 r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0000_map.nc',
                 ]
-file_nc_list = [r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc', #fullgrid
-                r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0000_map.nc',
-                ]
 
 for file_nc in file_nc_list:
     print('processing %s'%(os.path.basename(file_nc)))
