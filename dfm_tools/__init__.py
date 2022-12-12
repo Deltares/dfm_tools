ca471df8b826130782c75e0557ae636b2722ff47
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-12 11:50:57 +0100

    Bump version: 0.8.29 → 0.8.30

--- dfm_tools/__init__.py
+++ dfm_tools/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Jelmer Veenstra"""
 __email__ = 'jelmer.veenstra@deltares.nl'
-__version__ = '0.8.29'
+__version__ = '0.8.30'
 
 from dfm_tools.download import *
 from dfm_tools.get_nc import *
