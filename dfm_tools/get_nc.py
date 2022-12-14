d943010855a9700abce7fce3de590c10f0fa71cb
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-14 21:59:37 +0100

    improved comments

--- dfm_tools/get_nc.py
+++ dfm_tools/get_nc.py
@@ -572,7 +572,7 @@ def get_Dataset_atdepths(data_xr, depths, reference='z0', zlayer_z0_selnearest=F
     
     #TODO: zmodel gets depth in figure title, because of .set_index() in open_partitioned_dataset(). Sigmamodel gets percentage/fraction in title. >> set_index was removed there, so check again.
     #TODO: check if attributes should be passed/altered
-    #TODO: build in check whether layers/interfaces are coherent (len(int)=len(lay)+1), since one could also supply a uds.isel(layer=range(10)) where there are still 51 interfaces.
+    #TODO: build in check whether layers/interfaces are coherent (len(int)=len(lay)+1), since one could also supply a uds.isel(layer=range(10)) where there are still 51 interfaces. (if not, raise error to remove layer selection)
     #TODO: also waterlevelvar in 3D model gets depth_fromref coordinate, would be nice to avoid.
     #TODO: clean up unneccesary variables (like pre-interp depth values and interface dim)
     """
