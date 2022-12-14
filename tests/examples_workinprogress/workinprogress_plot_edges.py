c498abff891fae9431d59539d25b79705bf525e4
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-14 23:44:06 +0100

    improved comments

--- tests/examples_workinprogress/workinprogress_plot_edges.py
+++ tests/examples_workinprogress/workinprogress_plot_edges.py
@@ -9,6 +9,7 @@ import os
 import matplotlib.pyplot as plt
 plt.close('all')
 import dfm_tools as dfmt
+import xugrid as xu
 
 dir_testinput = r'c:\DATA\dfm_tools_testdata'
 dir_output = '.'
@@ -16,8 +17,8 @@ dir_output = '.'
 
 file_nc_list = [os.path.join(dir_testinput,'DFM_sigma_curved_bend\\DFM_OUTPUT_cb_3d\\cb_3d_map.nc'), #sigmalayer
                 os.path.join(dir_testinput,'DFM_3D_z_Grevelingen','computations','run01','DFM_OUTPUT_Grevelingen-FM','Grevelingen-FM_0000_map.nc'), #zlayer
-                r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc', #fullgrid
-                r'p:\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0000_map.nc', #2D model
+                #r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0000_map.nc', #fullgrid
+                #r'p:\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0000_map.nc', #2D model
                 #r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0000_map.nc',
                 ]
 
@@ -25,7 +26,9 @@ for file_nc in file_nc_list:
     print('processing %s'%(os.path.basename(file_nc)))
     basename = os.path.basename(file_nc).replace('.','')
     
-    data_frommap_merged = dfmt.open_partitioned_dataset(file_nc.replace('_0000_','_0*_'))
+    data_frommap_merged = dfmt.open_partitioned_dataset(file_nc.replace('_0000_','_0*_')) #TODO: fails
+    #data_frommap_merged = dfmt.open_partitioned_dataset(file_nc)#.replace('_0000_','_0*_')) #TODO: fails
+    #data_frommap_merged = xu.open_dataset(file_nc) #works
     
     fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,figsize=(12,8))
     pc = data_frommap_merged.mesh2d_edge_x.ugrid.plot(ax=ax1,cmap='jet')
