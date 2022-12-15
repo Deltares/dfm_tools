e77a28c0f9b5293be7a5d25dc56cd2f2377a8a38
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-15 11:26:22 +0100

    updated comments

--- tests/examples_workinprogress/workinprogress_meshkernel_creategrid.py
+++ tests/examples_workinprogress/workinprogress_meshkernel_creategrid.py
@@ -14,6 +14,7 @@ import numpy as np
 
 
 
+
 def create_mk_instance_with_a_uniform_curvilinear_grid(num_columns =3, num_rows =3):
     r"""A local function for creating an instance of meshkernel with a uniform curvilinear grid.
     """
@@ -41,12 +42,11 @@ def create_mk_instance_with_a_uniform_curvilinear_grid(num_columns =3, num_rows 
 
 ## Make a uniform rectilinear grid
 """
-1.	Make a regular and rotated grid. This is possible in MeshKernelPy but I need to merge the related pull request in master. The same workflow of interactor is followed here: first generate a curvilinear grid than convert the curvilinear grid into unstructured grid. The steps are the following:
-a.	curvilinear_make_uniform, see the following notebook: https://github.com/Deltares/MeshKernelPy/blob/AddCurvilinearGridSupport/docs/examples/04_curvilineargrid_basics.ipynb
-b.	curvilinear_convert_to_mesh2d: https://github.com/Deltares/MeshKernelPy/blob/118cb4953c4e95d5b18ed283bb37f391134b2bb2/meshkernel/meshkernel.py#L1399 
+Make a regular and rotated grid. This is possible in MeshKernelPy but I need to merge the related pull request in master. The same workflow of interactor is followed here: first generate a curvilinear grid than convert the curvilinear grid into unstructured grid. The steps are the following:
+- curvilinear_make_uniform, see the following notebook: https://github.com/Deltares/MeshKernelPy/blob/AddCurvilinearGridSupport/docs/examples/04_curvilineargrid_basics.ipynb
+- curvilinear_convert_to_mesh2d: https://github.com/Deltares/MeshKernelPy/blob/118cb4953c4e95d5b18ed283bb37f391134b2bb2/meshkernel/meshkernel.py#L1399 
 
-    If you want to write the resulting unstructured grid to file, you can use UGridPy. See the following example notebook: https://github.com/Deltares/UGridPy/blob/main/docs/examples/05_working_with_meshkernel.ipynb
-
+#TODO: not working since some functions are missing from the meshkernelpy release version
 """
 mk = create_mk_instance_with_a_uniform_curvilinear_grid()
 curvilinear_grid = mk.curvilineargrid_get()
@@ -78,16 +78,18 @@ mk_ugrid = mk.curvilinear_convert_to_mesh() #TODO: does not exist in release cod
 
 
 
-
-# refine with samples. #TODO: this does not seem to be related to bathymetry/courant refinement
+# refine with samples. 
 """
-3.	Mesh refinement in MeshKernelPy (where the samples parameter contains the sample coordinates and the sample depths used for mesh refinement):
+Mesh refinement in MeshKernelPy (where the samples parameter contains the sample coordinates and the sample depths used for mesh refinement):
+- https://github.com/Deltares/MeshKernelPy/blob/6a5110d75ea2a86a27f63da7c7196e008bbce3dc/meshkernel/meshkernel.py#L451
 
-https://github.com/Deltares/MeshKernelPy/blob/6a5110d75ea2a86a27f63da7c7196e008bbce3dc/meshkernel/meshkernel.py#L451
+#TODO: this does not seem to be related to bathymetry/courant refinement (instead int sample set to refine each cell n times)
 
 """
 mk.mesh2d_refine_based_on_samples() #'samples', 'relative_search_radius', 'minimum_num_samples', and 'mesh_refinement_params'
 
+
+#TODO: write to netcdf with: xugrid or ugridpy (https://github.com/Deltares/UGridPy/blob/main/docs/examples/05_working_with_meshkernel.ipynb)
 
 
 
