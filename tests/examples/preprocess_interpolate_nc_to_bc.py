539fb81a35a1564472040b8c74755e4c370e4e7d
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-16 10:14:00 +0100

    cleanups preprocess_interpolate_nc_to_bc.py

--- tests/examples/preprocess_interpolate_nc_to_bc.py
+++ tests/examples/preprocess_interpolate_nc_to_bc.py
@@ -4,7 +4,7 @@ Created on Thu Aug 18 16:37:38 2022
 
 @author: veenstra
 
-This script can be used to interpolate pre-downloaded CMEMS data (or other netcdf files) to a boundary locations in a pli-file
+This script can be used to interpolate pre-downloaded CMEMS data (or other netcdf files) to points of a pli-file, resulting in a boundary forcingfile (bc file)
 """
 
 import os
@@ -19,49 +19,40 @@ try: #0.3.1 release
 except: #main branch and next release #TODO: move to easy imports after https://github.com/Deltares/HYDROLIB-core/issues/410
     from hydrolib.core.io.dflowfm.ext.models import Boundary, ExtModel
 
-#TODO: put all functions under dfm_tools init for more convenience
-#TODO: discuss structure with arthur/stendert (new modularity, naming of functions)
-#TODO: make both interpolate functions accept tstart/tstop as datestrings
-#TODO: add coordinate conversion of pli-coordinates (for nesting RD models)
+#TODO: add coordinate conversion of pli-coordinates? (for nesting RD models in oceanmodels)
 #TODO: additional models/sources for download/interpolate (evt xESMF for CMCC, climate forcing cmip6 procedure (=calendarconversion) and others)
 
-#copied plifile from DCSM folder: r'p:\1204257-dcsmzuno\data\CMEMS\bnd\NorthSeaAndBaltic_1993-2019_20210510'
-#list_plifiles = [Path(r'c:\DATA\dfm_tools_testdata\hydrolib_bc\DCSM\DCSM-FM_OB_all_20181108.pli')] #TODO: reading this file results in empty Polyfile, should raise an error. https://github.com/Deltares/HYDROLIB-core/issues/320
-list_plifiles = [Path(r'c:\DATA\dfm_tools_testdata\hydrolib_bc\DCSM\DCSM-FM_OB_all_20181108_nocomments.pli')]
 nPoints = 3# None #amount of Points to process per PolyObject in the plifile (use int for testing, use None for all Points)
-
-dir_output = './test_interpolate_nc_to_bc'
-bc_type = 'bc' #currently only 'bc' supported #TODO: add netcdf bc support. https://github.com/Deltares/HYDROLIB-core/issues/318
-
 refdate_str = 'minutes since 2011-12-22 00:00:00 +00:00' # if None, xarray uses ds.time.encoding['units'] as refdate_str
+dir_output = './test_interpolate_nc_to_bc'
 
 #quantities should be in conversion_dict.keys(). waterlevelbnd is steric/zos, tide is tidal components from FES/EOT
-list_quantities = ['waterlevelbnd','salinitybnd','temperaturebnd','uxuy','tracerbndNO3']#,'tide']
+list_quantities = ['waterlevelbnd','salinitybnd','temperaturebnd','uxuy','tracerbndNO3','tide']
 #list_quantities = ['waterlevelbnd','salinitybnd','temperaturebnd','tracerbndNO3']
 list_quantities = ['tracerbndNO3']
 
 model = 'CMEMS' #CMEMS GFDL CMCC HYCOM
 
 #The {ncvarname} wildcard in dir_pattern_hydro/dir_patern_waq is used to replace it with conversion_dict[quantity]['ncvarname'] by using str(dir_pattern).format(ncvarname)
-reverse_depth = False #to compare with coastserv files, this argument will be phased out
-kdtree_k = 3
 if model=='CMEMS': #2012-01-06 12:00:00 to 2013-01-03 12:00:00
     conversion_dict = dfmt.get_conversion_dict()
-    tstart = dt.datetime(2012, 1, 16, 12, 0)
-    tstop = dt.datetime(2012, 4, 1, 12, 0)
-    #tstop = dt.datetime(2013, 1, 1, 12, 0)
+    tstart = '2012-01-16 12:00'
+    tstop = '2012-04-01 12:00'
+    #tstop = '2013-01-01 12:00'
+    list_plifiles = [Path(r'p:\11208054-004-dcsm-fm\models\model_input\bnd_cond\pli\DCSM-FM_OB_all_20181108_nocomments.pli')] #TODO: reading this file without '_nocomments' results in empty Polyfile, should raise an error. https://github.com/Deltares/HYDROLIB-core/issues/320
     dir_sourcefiles_hydro = r'p:\1204257-dcsmzuno\data\CMEMS\nc\DCSM_allAvailableTimes' #CMEMS hydro: bottomT, so, thetao, uo, vo, zos (2012-01-06 12:00:00 to 2013-01-03 12:00:00) (daily values at noon, not at midnight)
     dir_pattern_hydro = Path(dir_sourcefiles_hydro,'{ncvarname}_2012*.nc') # later remove 2012 from string, but this is faster for testing #TODO: it is quite slow, maybe speed up possible?
     dir_sourcefiles_waq = r'p:\11206304-futuremares\python_scripts\ocean_boundaryCMEMS\data_monthly' #CMEMS waq: no3, o2, phyc, so4, si (2011-12-16 12:00:00 to 2019-01-16 12:00:00)
     dir_pattern_waq = Path(dir_sourcefiles_waq,'cmems_mod_glo_bgc_my_0.25_P1M-m_{ncvarname}_*.nc') 
-    #to reproduce old CMEMS data (icw reverse_depth=True)
+    #to reproduce old CMEMS data (icw reverse_depth=True) (from p:\1204257-dcsmzuno\data\CMEMS\bnd\NorthSeaAndBaltic_1993-2019_20210510)
     #tstart = dt.datetime(1993,1,1,12,0)
     #tstop = tstart + dt.timedelta(days=5)
     #dir_pattern_hydro = Path(dir_sourcefiles_hydro,'{ncvarname}_1993*.nc')
 elif model=='GFDL':
     conversion_dict = dfmt.get_conversion_dict()
-    tstart = dt.datetime(2012, 1, 16, 12, 0)
-    tstop = dt.datetime(2012, 4, 1, 12, 0)
+    tstart = '2012-01-16 12:00'
+    tstop = '2012-04-01 12:00'
+    list_plifiles = [Path(r'p:\11208054-004-dcsm-fm\models\model_input\bnd_cond\pli\DCSM-FM_OB_all_20181108_nocomments.pli')] #TODO: reading this file without '_nocomments' results in empty Polyfile, should raise an error. https://github.com/Deltares/HYDROLIB-core/issues/320
     dir_sourcefiles_hydro = None
     dir_pattern_hydro = None
     dir_sourcefiles_waq = r'p:\11206304-futuremares\data\CMIP6_BC\GFDL-ESM4' #GFDL waq: no3 (1850-01-16 12:00:00 to 2014-12-16 12:00:00)
@@ -70,22 +61,22 @@ elif model=='CMCC': #TODO: check method, now finding nearest points (so always h
     #TODO: time_bnds/lev_bnds are available, take into account in bc file?
     conversion_dict = dfmt.get_conversion_dict(ncvarname_updates={'salinitybnd':'sos', 'temperaturebnd':'tos'})
     conversion_dict['tracerbndNO3'] = {'ncvarname':'no3', 'unit':'g/m3', 'conversion':14.0} #other vars also have different conversion than cmems
-    tstart = dt.datetime(2015, 6, 16, 12, 0)
-    tstop = dt.datetime(2015, 12, 1, 12, 0)
+    tstart = '2015-06-16 12:00'
+    tstop = '2015-12-01 12:00'
+    list_plifiles = [Path(r'p:\11208054-004-dcsm-fm\models\model_input\bnd_cond\pli\DCSM-FM_OB_all_20181108_nocomments.pli')] #TODO: reading this file without '_nocomments' results in empty Polyfile, should raise an error. https://github.com/Deltares/HYDROLIB-core/issues/320
     dir_sourcefiles_hydro = r'p:\11206304-futuremares\data\CMIP6_BC\CMCC-ESM2'
     dir_pattern_hydro = Path(dir_sourcefiles_hydro,'{ncvarname}_Omon_CMCC-ESM2_ssp126_r1i1p1f1_gn_*.nc')
     dir_sourcefiles_waq = dir_sourcefiles_hydro #CMCC waq: (2015-01-16 12:00:00 to 2100-12-16 12:00:00)
     dir_pattern_waq = dir_pattern_hydro
-    KDTree_invdistweigth = True #only relevant for CMCC
 elif model=='HYCOM':
     conversion_dict = dfmt.get_conversion_dict(ncvarname_updates={'salinitybnd':'salinity', 'temperaturebnd':'water_temp'})
-    tstart = dt.datetime(2016, 4, 20, 0, 0) #HYCOM
-    tstop = dt.datetime(2016, 5, 3, 0, 0)
+    tstart = '2016-04-20'
+    tstop = '2016-05-03'
+    list_plifiles = [Path(r'c:\DATA\dfm_tools_testdata\GLBu0.08_expt_91.2\bcline.pli')] #HYCOM not available in DCSM area, so use other pli-file
     dir_sourcefiles_hydro = 'c:\\DATA\\dfm_tools_testdata\\GLBu0.08_expt_91.2' #HYCOM hydro: salinity/so, water_temp/thetao (2016-04-19 00:00:00 to 2016-05-06 00:00:00)
     dir_pattern_hydro = Path(dir_sourcefiles_hydro,'HYCOM_ST_GoO_*.nc')
     dir_sourcefiles_waq = None
     dir_pattern_waq = None
-    list_plifiles = [Path(r'c:\DATA\dfm_tools_testdata\GLBu0.08_expt_91.2\bcline.pli')] #HYCOM not available in DCSM area, so use other pli-file
 else:
     raise Exception(f'invalid model: {model}')
 
@@ -122,12 +113,10 @@ for file_pli in list_plifiles:
             data_xr_vars = dfmt.open_dataset_extra(dir_pattern=dir_pattern, quantity=quantity, #TODO: maybe replace renaming part with package CMCC/Lisa?
                                                    tstart=tstart, tstop=tstop,
                                                    conversion_dict=conversion_dict,
-                                                   refdate_str=refdate_str, 
-                                                   reverse_depth=reverse_depth) #temporary argument to compare easier with old coastserv files            
+                                                   refdate_str=refdate_str)
             #interpolate regulargridDataset to plipointsDataset
             data_interp = dfmt.interp_regularnc_to_plipoints(data_xr_reg=data_xr_vars, file_pli=file_pli,
-                                                             nPoints=nPoints, #argument for testing
-                                                             kdtree_k=kdtree_k) #argument for curvi grids like CMCC
+                                                             nPoints=nPoints) #argument for testing
             #data_interp = data_interp.ffill(dim="plipoints").bfill(dim="plipoints") #to fill allnan plipoints with values from the neighbour point
             
             #convert plipointsDataset to hydrolib ForcingModel
@@ -138,25 +127,23 @@ for file_pli in list_plifiles:
             file_bc_out = Path(dir_output,f'{quantity}_{file_bc_basename}_{tidemodel}.bc')
         else:
             file_bc_out = Path(dir_output,f'{quantity}_{file_bc_basename}_{model}.bc')
+        
         print(f'writing ForcingModel to bc file with hydrolib ({file_bc_out.name})')
+        bc_type = 'bc' #TODO: add netcdf bc support. https://github.com/Deltares/HYDROLIB-core/issues/318
         if bc_type=='bc':
             #ForcingModel_object.serializer_config.float_format = '.3f' #TODO SOLVED: improve formatting of bc file: https://github.com/Deltares/HYDROLIB-core/issues/308
             #ForcingModel_object.serializer_config.float_format_datablock = '.5f' #maybe move this to interp_regularnc_to_plipoints/interpolate_tide_to_bc?
             ForcingModel_object.save(filepath=file_bc_out)
-        else:
-            raise Exception(f'invalid bc_type: {bc_type}')
         
-        #make paths relative (sort of) (also necessary for locationfile) /../ should also be supported? 
-        #ForcingModel_object.filepath = Path(str(ForcingModel_object.filepath).replace(dir_out,'')) #TODO: convert to relative paths in ext file possible? This path is the same as file_bc_out
-        
+        #TODO: support for relative paths?
         #generate boundary object for the ext file (quantity, pli-filename, bc-filename)
         boundary_object = Boundary(quantity=quantity.replace('tide','waterlevelbnd'), #the FM quantity for tide is also waterlevelbnd
                                    locationfile=file_pli,
-                                   forcingfile=ForcingModel_object,
-                                   )
+                                   forcingfile=ForcingModel_object)
         ext_bnd.boundary.append(boundary_object)
 
-        if 1 and quantity!='tide': #plotting dataset and polyline (is wrong for CMCC)
+        if 1 and quantity!='tide': #TODO: data_xr_vars/data_interp does not exist for tide yet
+            #plotting dataset and polyline (is wrong for CMCC)
             varname0 = list(data_xr_vars.data_vars)[0] 
             fig,ax = plt.subplots()
             if 'depth' in data_xr_vars[varname0].dims:
@@ -169,34 +156,13 @@ for file_pli in list_plifiles:
             fig.tight_layout()
             fig.savefig(str(file_bc_out).replace('.bc','_polyline'))
             
-        if 1: #plotting example data point
-            # for iF in [2]:#range(nPoints): #from data_interp TODO: this is slightly easier, but negative depth is not done yet (is in Dataset_to_T3D/Dataset_to_TimeSeries functions)
-            #     data_vars = list(data_interp.data_vars)
-            #     fig,ax1 = plt.subplots(figsize=(10, 6))
-            #     data_interp[data_vars[0]].isel(plipoints=iF).T.plot()
-            #     fig.tight_layout()
-            for iF in [2]:#range(nPoints): #from ForcingModel_object
-                forcingobject_one = ForcingModel_object.forcing[iF]
-                forcingobject_one_xr = dfmt.forcinglike_to_Dataset(forcingobject_one,convertnan=True)
-                data_vars = list(forcingobject_one_xr.data_vars)
+            #plotting example data point
+            for iF in [2]:#range(nPoints): 
+                data_vars = list(data_interp.data_vars)
                 fig,ax1 = plt.subplots(figsize=(10, 6))
-                if hasattr(forcingobject_one,'vertpositions'): #3D quantity (time/depth dimensions)
-                    if hasattr(forcingobject_one.quantityunitpair[1],'elementname'): #uxuy vector
-                        plt.close(fig)
-                        fig, axes = plt.subplots(2,1,figsize=(10, 6),sharex=True,sharey=True)
-                        forcingobject_one_xr[data_vars[0]].T.plot(ax=axes[0])
-                        forcingobject_one_xr[data_vars[1]].T.plot(ax=axes[1])
-                    else:
-                        forcingobject_one_xr[data_vars[0]].T.plot(ax=ax1)
-                elif quantity=='tide':
-                    ax2 = ax1.twinx()
-                    forcingobject_one_xr[data_vars[0]].plot(ax=ax1)
-                    forcingobject_one_xr[data_vars[1]].plot(ax=ax2)
-                else:
-                    forcingobject_one_xr[data_vars[0]].plot(ax=ax1)
+                data_interp[data_vars[0]].isel(plipoints=iF).T.plot()
                 fig.tight_layout()
                 fig.savefig(str(file_bc_out).replace('.bc',''))
-
 
 file_ext_out = Path(dir_output,'example_bnd.ext')
 ext_bnd.save(filepath=file_ext_out)
