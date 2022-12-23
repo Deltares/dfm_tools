f0061eaae07162430127dae1b945f1772e9a6c5d
veenstrajelmer <veenstrajelmer@gmail.com>
2022-12-23 17:32:43 +0100

    updated meshkernelpy example script

--- tests/examples_workinprogress/workinprogress_meshkernel_creategrid.py
+++ tests/examples_workinprogress/workinprogress_meshkernel_creategrid.py
@@ -9,36 +9,11 @@ Created on Thu Dec  8 20:54:39 2022
 import meshkernel
 
 import matplotlib.pyplot as plt
-plt.close()
+plt.close('all')
 import numpy as np
 
 
 
-
-def create_mk_instance_with_a_uniform_curvilinear_grid(num_columns =3, num_rows =3):
-    r"""A local function for creating an instance of meshkernel with a uniform curvilinear grid.
-    """
-    mk = meshkernel.MeshKernel()
-    
-    # Create an instance of MakeGridParameters and set the values
-    make_grid_parameters = meshkernel.MakeGridParameters() #TODO: does not exist in release code
-    make_grid_parameters.num_columns = num_columns
-    make_grid_parameters.num_rows = num_rows
-    make_grid_parameters.angle = 0.0
-    make_grid_parameters.block_size = 0.0
-    make_grid_parameters.origin_x = 0.0
-    make_grid_parameters.origin_y = 0.0
-    make_grid_parameters.block_size_x = 10.0
-    make_grid_parameters.block_size_y = 10.0
-    
-    # A polygon must to be provided. If empty it will not be used.
-    node_x = np.empty(0, dtype=np.double)
-    node_y = np.empty(0, dtype=np.double)
-    geometry_list = meshkernel.GeometryList(node_x, node_y)
-
-    mk.curvilinear_make_uniform(make_grid_parameters, geometry_list)
-    
-    return mk
 
 ## Make a uniform rectilinear grid
 """
@@ -46,21 +21,12 @@ Make a regular and rotated grid. This is possible in MeshKernelPy but I need to 
 - curvilinear_make_uniform, see the following notebook: https://github.com/Deltares/MeshKernelPy/blob/AddCurvilinearGridSupport/docs/examples/04_curvilineargrid_basics.ipynb
 - curvilinear_convert_to_mesh2d: https://github.com/Deltares/MeshKernelPy/blob/118cb4953c4e95d5b18ed283bb37f391134b2bb2/meshkernel/meshkernel.py#L1399 
 
-#TODO: not working since some functions are missing from the meshkernelpy release version
 """
-mk = create_mk_instance_with_a_uniform_curvilinear_grid()
-curvilinear_grid = mk.curvilineargrid_get()
-fig, ax = plt.subplots()
-curvilinear_grid.plot_edges(ax)
 
-#If a polygon is provided it will be used in the generation of the curvilinear grid. The polygon must be closed
-node_x = np.array([2.5,5.5,3.5,0.5,2.5], dtype=np.double)
-node_y = np.array([0.5,3.0,5.0,2.5,0.5], dtype=np.double)
-geometry_list = meshkernel.GeometryList(node_x, node_y)
-
+# Create an instance of MakeGridParameters and set the values
 make_grid_parameters = meshkernel.MakeGridParameters()
-make_grid_parameters.num_columns = 10
-make_grid_parameters.num_rows = 10
+make_grid_parameters.num_columns = 20
+make_grid_parameters.num_rows = 20
 make_grid_parameters.angle = 0.0
 make_grid_parameters.block_size = 0.0
 make_grid_parameters.origin_x = 0.0
@@ -68,13 +34,33 @@ make_grid_parameters.origin_y = 0.0
 make_grid_parameters.block_size_x = 0.2
 make_grid_parameters.block_size_y = 0.2
 
+
+
+# A polygon must to be provided. If empty it will not be used.
+node_x = np.empty(0, dtype=np.double)
+node_y = np.empty(0, dtype=np.double)
+geometry_list = meshkernel.GeometryList(node_x, node_y)
+
+mk = meshkernel.MeshKernel()
 mk.curvilinear_make_uniform(make_grid_parameters, geometry_list)
 curvilinear_grid = mk.curvilineargrid_get()
 fig, ax = plt.subplots()
 curvilinear_grid.plot_edges(ax)
+mk.curvilinear_convert_to_mesh2d() #convert to ugrid/mesh2d
 
-#convert to ugrid/mesh2d
-mk_ugrid = mk.curvilinear_convert_to_mesh() #TODO: does not exist in release code: https://github.com/Deltares/MeshKernelPy/blob/118cb4953c4e95d5b18ed283bb37f391134b2bb2/meshkernel/meshkernel.py#L1399
+
+
+#If a polygon is provided it will be used in the generation of the curvilinear grid. The polygon must be closed
+node_x = np.array([2.5,5.5,3.5,0.5,2.5], dtype=np.double)
+node_y = np.array([0.5,3.0,5.0,2.5,0.5], dtype=np.double)
+geometry_list = meshkernel.GeometryList(node_x, node_y)
+
+mk = meshkernel.MeshKernel()
+mk.curvilinear_make_uniform(make_grid_parameters, geometry_list)
+curvilinear_grid = mk.curvilineargrid_get()
+fig, ax = plt.subplots()
+curvilinear_grid.plot_edges(ax)
+mk.curvilinear_convert_to_mesh2d() #convert to ugrid/mesh2d
 
 
 
@@ -83,35 +69,40 @@ mk_ugrid = mk.curvilinear_convert_to_mesh() #TODO: does not exist in release cod
 Mesh refinement in MeshKernelPy (where the samples parameter contains the sample coordinates and the sample depths used for mesh refinement):
 - https://github.com/Deltares/MeshKernelPy/blob/6a5110d75ea2a86a27f63da7c7196e008bbce3dc/meshkernel/meshkernel.py#L451
 
-#TODO: this does not seem to be related to bathymetry/courant refinement (instead int sample set to refine each cell n times)
-
-enum class RefinementType
-{
-    WaveCourant = 1, #>> this one we need
-    RefinementLevels = 2
-};
-
-class MeshRefinementParameters:
-    refine_intersected: bool
-    use_mass_center_when_refining: bool
-    min_face_size: float
-    refinement_type: RefinementType #>> set this to Wavecourant/1
-    connect_hanging_nodes: bool
-    account_for_samples_outside_face: bool
-    max_refinement_iterations: int = 10
-
-
-  def mesh2d_refine_based_on_samples(
-        self,
-        samples: GeometryList,
-        relative_search_radius: float,
-        minimum_num_samples: int,
-        mesh_refinement_params: MeshRefinementParameters, #>> set this to above
-    ) -> None:
 
 """
-mk.mesh2d_refine_based_on_samples() #'samples', 'relative_search_radius', 'minimum_num_samples', and 'mesh_refinement_params'
 
+mesh_refinement_parameters = meshkernel.MeshRefinementParameters(refine_intersected=True, #TODO: possible to make it not deliberate
+                                                                 use_mass_center_when_refining=False,
+                                                                 min_face_size=0.05,
+                                                                 refinement_type=meshkernel.RefinementType(1), #Wavecourant/1,
+                                                                 connect_hanging_nodes=False,
+                                                                 account_for_samples_outside_face=False,
+                                                                 max_refinement_iterations=3,
+                                                                 )
+
+
+samp_x = np.arange(0,5,0.1)
+samp_y = np.arange(0,5,0.1)
+samp_x,samp_y = np.meshgrid(samp_x,samp_y)
+samp_z = np.random.uniform(low=0.5, high=13.3, size=samp_y.shape)
+
+samp_x = samp_x.ravel()
+samp_y = samp_y.ravel()
+samp_z = samp_z.ravel()
+
+geomlist = meshkernel.GeometryList(x_coordinates=samp_x, y_coordinates=samp_y, values=samp_z)
+
+mk.mesh2d_refine_based_on_samples(samples=geomlist, #TODO: provide sample set (GeometryList type is expected)
+                                  relative_search_radius=1,
+                                  minimum_num_samples=3,
+                                  mesh_refinement_params=mesh_refinement_parameters,
+                                  )
+
+mk.curvilinear_make_uniform(make_grid_parameters, geometry_list)
+curvilinear_grid = mk.curvilineargrid_get()
+fig, ax = plt.subplots()
+curvilinear_grid.plot_edges(ax)
 
 #TODO: write to netcdf with: xugrid or ugridpy (https://github.com/Deltares/UGridPy/blob/main/docs/examples/05_working_with_meshkernel.ipynb)
 
