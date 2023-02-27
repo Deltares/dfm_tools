225ddbfae1d1dc98526e5bdf35c473fe5488d20f
veenstrajelmer <veenstrajelmer@gmail.com>
2023-02-27 15:38:43 +0100

    preprocess_ini_rst_nc_to_xyz.py is failing, added some commented lines with a first step towards solution

--- tests/examples/preprocess_ini_rst_nc_to_xyz.py
+++ tests/examples/preprocess_ini_rst_nc_to_xyz.py
@@ -8,16 +8,21 @@ Created on Tue Jun 15 15:32:15 2021
 import os
 import numpy as np
 import dfm_tools as dfmt
+#import xarray as xr
 
+file_nc_ug = os.path.join(r'c:\DATA\dfm_tools_testdata','DFM_3D_z_Grevelingen','computations','run01','DFM_OUTPUT_Grevelingen-FM','Grevelingen-FM_0*_map.nc')
 file_nc = r'p:\11206811-002-kpp-veerse-meer\model\runs_2011-2012\VM_WQ_3D_run9_c\DFM_OUTPUT_VM_WQ_3D\VM_WQ_3D_0000_20130101_000000_rst.nc'
 dir_output = '.' #r'p:\11206811-002-kpp-veerse-meer\model\initial_conditions\VM_WQ_3D_run9c'
 
 if not os.path.exists(dir_output):
     os.makedirs(dir_output)
 
+# ds_ug = xr.open_dataset(file_nc_ug.replace('0*','0000'))
+# ds = xr.open_dataset(file_nc)
+# ds['mesh2d'] = xr.DataArray().assign_attrs({'aa':1})
+# uds = dfmt.open_partitioned_dataset(file_nc)
+
 # define map variables for VM
-times_pd = dfmt.get_timesfromnc(file_nc=file_nc)
-vars_pd = dfmt.get_ncvarproperties(file_nc=file_nc)
 subs = ['DetCS1','DetNS1','DetPS1','DetSiS1']
 x_coords = dfmt.get_ncmodeldata(file_nc=file_nc, varname='FlowElem_xzw', multipart=True)
 y_coords = dfmt.get_ncmodeldata(file_nc=file_nc, varname='FlowElem_yzw', multipart=True)
@@ -30,4 +35,4 @@ for sub in subs:
     xyz = np.stack([x_coords,y_coords,data_frommap.transpose().reshape(-1)])
     
     xyz_out = xyz.transpose()
-    np.savetxt(os.path.join(dir_output,sub+'.xyz'),xyz_out,delimiter='\t')
+    np.savetxt(os.path.join(sub+'.xyz'),xyz_out,delimiter='\t')
