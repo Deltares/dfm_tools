d7af728b5a870602c24b2a4d20e100f8b16948a0
veenstrajelmer <veenstrajelmer@gmail.com>
2023-03-31 18:00:34 +0200

    solved code smells in get_nc.py

--- dfm_tools/get_nc.py
+++ dfm_tools/get_nc.py
@@ -224,7 +224,7 @@ def get_formula_terms(uds, varn_contains='interface'):
     osz_varnames = list(uds.filter_by_attrs(formula_terms=lambda v: v is not None).variables) #names of variables containing attribute "formula_terms"
     osz_varnames_contains = [x for x in osz_varnames if varn_contains in x] #TODO: to get the layer/interface ocean_*_coordinate. Not too pretty, but it works
     if len(osz_varnames_contains) != 1: #should be 1 exactly, none is the case in zlayer models
-        raise Exception(f'no or more than one {varn_contains} variable found with formula_terms attribute: {osz_varnames}')
+        raise ValueError(f'no or more than one {varn_contains} variable found with formula_terms attribute: {osz_varnames}')
     osz_varn = osz_varnames_contains[0]
     osz_formulaterms = uds[osz_varn].attrs['formula_terms']
     tokens = re.split('[:\\s]+', osz_formulaterms)
