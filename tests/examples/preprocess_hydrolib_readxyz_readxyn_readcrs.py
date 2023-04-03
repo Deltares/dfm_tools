6ac9c8f7d097a7e783cc5963abc4ccf5f05b341f
veenstrajelmer <veenstrajelmer@gmail.com>
2023-04-03 12:06:58 +0200

    updated comments

--- tests/examples/preprocess_hydrolib_readxyz_readxyn_readcrs.py
+++ tests/examples/preprocess_hydrolib_readxyz_readxyn_readcrs.py
@@ -27,13 +27,13 @@ fig.savefig(os.path.join(dir_output,os.path.basename(file_xyz).replace('.','')))
 
 file_xyn = r'p:\archivedprojects\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\geometry_j19_6-v2\output_locations\rmm_vzm-j19_6-v2b_3_measurement_obs.xyn'
 file_xyn = r'c:\Users\veenstra\Downloads\test_obs.xyn' #contains: 1.23  3.45  'some text with spaces' And everything beyond does not matter \n 1.23 3.45 Only_the_first_word and the test is ignored
-data_xyn1 = hcdfm.ObservationPointModel(Path(file_xyn)) #TODO: this should raise an error, but it returns an empy list
-data_xyn2 = hcdfm.XYNModel(Path(file_xyn)) #TODO: test_obs.xyn should raise an error
+data_xyn1 = hcdfm.ObservationPointModel(Path(file_xyn)) #TODO: this should raise an error, but it returns an empy list: https://github.com/Deltares/HYDROLIB-core/issues/502
+data_xyn2 = hcdfm.XYNModel(Path(file_xyn)) #TODO: test_obs.xyn should raise an error: https://github.com/Deltares/HYDROLIB-core/issues/459
 data_xyn2_pd = dfmt.pointlike_to_DataFrame(data_xyn2) #works as expected #TODO: add DataFrame_to_xyn() conversion?
-data_xyn2.save(file_xyn.replace('_obs.xyn','_out_obs.xyn')) #TODO: "TypeError: serialize() takes 3 positional arguments but 4 were given"
+data_xyn2.save(file_xyn.replace('_obs.xyn','_out_obs.xyn')) #TODO: "TypeError: serialize() takes 3 positional arguments but 4 were given",  https://github.com/Deltares/HYDROLIB-core/issues/459
 
 file_crs = r'p:\archivedprojects\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\geometry_j19_6-v2\cross_sections\rmm_vzm-j19_6-v2b_3_measurement_crs.pli'
-data_crs1 = hcdfm.CrossLocModel(Path(file_crs)) #TODO: this should raise an error, but it returns an empty list.
+data_crs1 = hcdfm.CrossLocModel(Path(file_crs)) #TODO: this should raise an error, but it returns an empty list: https://github.com/Deltares/HYDROLIB-core/issues/502
 data_crs2 = hcdfm.CrossDefModel(Path(file_crs)) #TODO: https://github.com/Deltares/HYDROLIB-core/issues/364
 data_crs3 = hcdfm.PolyFile(Path(file_crs)) #works with polyfile
 polyobject_pd = [dfmt.pointlike_to_DataFrame(x) for x in data_crs3.objects] #TODO: how to get a useful+plottable object?
