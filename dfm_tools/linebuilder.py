3c52babf40f2f02b4a8f62f6f4ae4cec265cac95
veenstrajelmer <veenstrajelmer@gmail.com>
2023-04-03 15:35:21 +0200

    removed duplicate LineBuilder class

--- dfm_tools/linebuilder.py
+++ dfm_tools/linebuilder.py
@@ -33,18 +33,3 @@ class LineBuilder:
         self.line_array = np.c_[self.xs, self.ys]
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
-
-class NOT_LineBuilder:
-    def __init__(self, line):
-        self.line = line
-        self.xs = list(line.get_xdata())
-        self.ys = list(line.get_ydata())
-        self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
-
-    def __call__(self, event):
-        print('click', event)
-        if event.inaxes!=self.line.axes: return
-        self.xs.append(event.xdata)
-        self.ys.append(event.ydata)
-        self.line.set_data(self.xs, self.ys)
-        self.line.figure.canvas.draw()
