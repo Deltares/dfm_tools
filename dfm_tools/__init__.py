f4d70aeca1bb9cebd8cd95066a757f39eb63a1e5
veenstrajelmer <veenstrajelmer@gmail.com>
2023-04-23 23:31:37 +0200

    Bump version: 0.10.54 → 0.10.55

--- dfm_tools/__init__.py
+++ dfm_tools/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Jelmer Veenstra"""
 __email__ = 'jelmer.veenstra@deltares.nl'
-__version__ = '0.10.54'
+__version__ = '0.10.55'
 
 from dfm_tools.deprecated import *
 from dfm_tools.errors import *
