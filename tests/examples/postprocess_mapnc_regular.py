67fb00a7e3983503cecc9db0bb157ec594b28bf7
veenstrajelmer <veenstrajelmer@gmail.com>
2023-04-26 19:52:52 +0200

    updated example scripts

--- tests/examples/postprocess_mapnc_regular.py
+++ tests/examples/postprocess_mapnc_regular.py
@@ -10,10 +10,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 plt.close('all')
-from pathlib import Path
 import xarray as xr
 import dfm_tools as dfmt
-import hydrolib.core.dflowfm as hcdfm
 
 
 dir_testinput = r'c:\DATA\dfm_tools_testdata'
@@ -34,8 +32,7 @@ for file_nc in file_nc_list:
         data_xr = data_xr.drop(['height_10m','height_2m']) #gives cleaner figure title
         clim_wl = [0,0.15]
         clim_uv = [0,5]
-        
-        file_ldb = Path(r'p:\archivedprojects\1220688-lake-kivu\3_modelling\1_FLOW\4_CH4_CO2_included\008\lake_kivu_geo.ldb')
+        add_ldb = True
         fig_args = dict(nrows=1,ncols=3,figsize=(14,6))
     elif 'sfincs' in file_nc:
         name_wl_x,name_wl_y,name_wl = 'x','y','zs'
@@ -44,14 +41,9 @@ for file_nc in file_nc_list:
         data_xr = data_xr.set_coords(['x','y','edge_x','edge_y']) #set coordinates for sfincs. TODO: request x/y/etc as coords in sfincs mapfile https://github.com/Deltares/SFINCS/issues/10
         clim_wl = [0,0.15]
         clim_uv = [0,0.6]
-        file_ldb = None
+        add_ldb = False
         fig_args = dict(nrows=3,ncols=1,figsize=(10,8))
     
-    if file_ldb is not None:
-        polyfile_object = hcdfm.PolyFile(file_ldb)
-        data_ldb = dfmt.pointlike_to_DataFrame(polyfile_object.objects[0])
-        data_ldb[data_ldb==999.999] = np.nan
-
     
     if name_wl is not None:
         data_fromnc_zs = data_xr[name_wl]
@@ -81,9 +73,8 @@ for file_nc in file_nc_list:
         pc = data_magn.plot.pcolormesh(x=name_uv_x,y=name_uv_y, cmap='jet',ax=ax)
         #pc = data_uv_tsel.plot.quiver(x='lon',y='lat',u='U_10M',v='V_10M', cmap='jet',ax=ax)
         pc.set_clim(clim_uv)
-        ax.set_aspect('equal')
-        if file_ldb is not None:
-            ax.plot(data_ldb['x'], data_ldb['y'], 'k', linewidth=0.5)
+        if add_ldb:
+            dfmt.plot_coastlines(ax=ax)
         data_u_thin = data_u_tsel.loc[::thinning,::thinning]
         data_v_thin = data_v_tsel.loc[::thinning,::thinning]
         ax.quiver(data_u_thin[name_uv_x], data_u_thin[name_uv_y], data_u_thin, data_v_thin, #raises warnings for some reason
@@ -115,9 +106,8 @@ for file_nc in file_nc_list:
         cbar = fig.colorbar(quiv_curved.lines, ax=ax)
         cbar.set_label('velocity magnitude (%s)'%(data_v.attrs['units']))
         ax.set_title(data_v_tsel.time.to_pandas())
-        #ax.set_aspect('equal')
-        if file_ldb is not None:
-            ax.plot(data_ldb['x'], data_ldb['y'], 'k', linewidth=0.5)
+        if add_ldb:
+            dfmt.plot_coastlines(ax=ax)
     fig.tight_layout()
     plt.savefig(os.path.join(dir_output,f'{basename}_magn_curvedquiver'))
 
