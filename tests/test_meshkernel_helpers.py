f21d1552f75400d2df57ff82163834053e4a9ef5
veenstrajelmer <veenstrajelmer@gmail.com>
2023-07-05 13:36:52 +0200

    added testcase for add_crs_to_dataset

---
+++ tests/test_meshkernel_helpers.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 12:59:23 2023
+
+@author: veenstra
+"""
+
+import pytest
+import xugrid as xu
+from dfm_tools.meshkernel_helpers import add_crs_to_dataset
+
+
+@pytest.mark.unittest
+def test_add_crs_to_dataset():
+    uds = xu.data.adh_san_diego()
+    crs='EPSG:26946' #TODO: this is not the correct crs for this model (plotting fails)
+    add_crs_to_dataset(uds,crs=crs,is_geographic=False)
+    crs_attrs = uds.projected_coordinate_system.attrs
+    
+    # import contextily as ctx
+    # import matplotlib.pyplot as plt
+    # plt.close('all')
+    # fig,ax = plt.subplots()
+    # uds.elevation.ugrid.plot(ax=ax)
+    # ctx.add_basemap(ax=ax,crs=crs)
+    
+    assert 'projected_coordinate_system' in uds.data_vars
+    assert crs_attrs['EPSG_code'] == 'EPSG:26946'
+    assert crs_attrs['epsg'] == 26946
+    assert crs_attrs['grid_mapping_name'] == 'Unknown projected'
