1276dae5f5db96c998f8d01e5f34e385e6ad4e28
veenstrajelmer <veenstrajelmer@gmail.com>
2023-07-05 14:02:57 +0200

    added add_crs_to_dataset spherical testcase

--- tests/test_meshkernel_helpers.py
+++ tests/test_meshkernel_helpers.py
@@ -11,11 +11,10 @@ from dfm_tools.meshkernel_helpers import add_crs_to_dataset
 
 
 @pytest.mark.unittest
-def test_add_crs_to_dataset():
+def test_add_crs_to_dataset_cartesian():
     uds = xu.data.adh_san_diego()
     crs='EPSG:26946' #TODO: this is not the correct crs for this model (plotting fails)
     add_crs_to_dataset(uds,crs=crs,is_geographic=False)
-    crs_attrs = uds.projected_coordinate_system.attrs
     
     # import contextily as ctx
     # import matplotlib.pyplot as plt
@@ -25,6 +24,25 @@ def test_add_crs_to_dataset():
     # ctx.add_basemap(ax=ax,crs=crs)
     
     assert 'projected_coordinate_system' in uds.data_vars
+    crs_attrs = uds.projected_coordinate_system.attrs
     assert crs_attrs['EPSG_code'] == 'EPSG:26946'
     assert crs_attrs['epsg'] == 26946
     assert crs_attrs['grid_mapping_name'] == 'Unknown projected'
+
+def test_add_crs_to_dataset_spherical():
+    uds = xu.data.adh_san_diego()
+    crs='EPSG:4326' #TODO: this is not the correct crs for this model (plotting fails)
+    add_crs_to_dataset(uds,crs=crs,is_geographic=True)
+    
+    # import contextily as ctx
+    # import matplotlib.pyplot as plt
+    # plt.close('all')
+    # fig,ax = plt.subplots()
+    # uds.elevation.ugrid.plot(ax=ax)
+    # ctx.add_basemap(ax=ax,crs=crs)
+    
+    assert 'wgs84' in uds.data_vars
+    crs_attrs = uds.wgs84.attrs
+    assert crs_attrs['EPSG_code'] == 'EPSG:4326'
+    assert crs_attrs['epsg'] == 4326
+    assert crs_attrs['grid_mapping_name'] == 'latitude_longitude'
