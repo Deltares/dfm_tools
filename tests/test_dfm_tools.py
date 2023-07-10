cd3b4b32a2897db8f7707abe82737ca10953caa3
veenstrajelmer <veenstrajelmer@gmail.com>
2023-07-10 15:16:22 +0200

    fixed testcase

--- tests/test_dfm_tools.py
+++ tests/test_dfm_tools.py
@@ -21,13 +21,32 @@ def test_facenodecoordinates_shape(file_nc, expected_size):
 
 
 @pytest.mark.unittest
-def test_getmapdata(file_nc, varname, expected_size):
+def test_getmapdata():
     """
     Checks whether ghost cells are properly taken care of (by asserting shape). And also whether varname can be found from attributes in case of Chlfa.
     
     """
     file_nc = dfmt.data.fm_grevelingen_map(return_filepath=True)
     expected_size = (44796, 36)
+    varname = 'mesh2d_sa1'
+    
+    data_xr_map = dfmt.open_partitioned_dataset(file_nc)
+    data_xr_map = dfmt.rename_waqvars(data_xr_map)
+    data_varsel = data_xr_map[varname].isel(time=2)
+    
+    assert data_varsel.shape == expected_size
+
+
+@pytest.mark.requireslocaldata
+@pytest.mark.unittest
+def test_getmapdata_waq():
+    """
+    Checks whether ghost cells are properly taken care of (by asserting shape). And also whether varname can be found from attributes in case of Chlfa.
+    
+    """
+    file_nc = os.path.join(r'p:\archivedprojects\11203850-coastserv\06-Model\waq_model\simulations\run0_20200319\DFM_OUTPUT_kzn_waq', 'kzn_waq_0*_map.nc')
+    expected_size = (17385, 39)
+    varname = 'mesh2d_Chlfa'
     
     data_xr_map = dfmt.open_partitioned_dataset(file_nc)
     data_xr_map = dfmt.rename_waqvars(data_xr_map)
