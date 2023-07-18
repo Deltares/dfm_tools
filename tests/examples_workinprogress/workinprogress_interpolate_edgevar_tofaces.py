a918a3159a1cd8491e55770b9d9dc47838dc638b
veenstrajelmer <veenstrajelmer@gmail.com>
2023-07-18 22:22:13 +0200

    edge-to-face example script simplifications
    Former-commit-id: ca1930b6fe066ee2d0429415fca7cfbd03100341

--- tests/examples_workinprogress/workinprogress_interpolate_edgevar_tofaces.py
+++ tests/examples_workinprogress/workinprogress_interpolate_edgevar_tofaces.py
@@ -9,6 +9,7 @@ import dfm_tools as dfmt
 import matplotlib.pyplot as plt
 plt.close('all')
 import xarray as xr
+import numpy as np
 
 # file_nc = dfmt.data.fm_curvedbend_map(return_filepath=True) #sigmalayer
 file_nc = dfmt.data.fm_grevelingen_map(return_filepath=True) #zlayer
@@ -17,11 +18,11 @@ file_nc = dfmt.data.fm_grevelingen_map(return_filepath=True) #zlayer
 # file_nc = r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0*_map.nc' #fullgrid
 # file_nc = r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0*_map.nc'
 
-uds = dfmt.open_partitioned_dataset(file_nc.replace('0*','0000'))
-if 'RMM_dflowfm' in file_nc:
+uds = dfmt.open_partitioned_dataset(file_nc.replace('0*','0002'))
+if 'RMM_dflowfm' in file_nc: #interpolation is slow for many timesteps, so subsetting first
     uds = uds.isel(time=[-1])
 
-uds_edges = dfmt.Dataset_varswithdim(uds, uds.grid.edge_dimension)
+#uds_edges = dfmt.Dataset_varswithdim(uds, uds.grid.edge_dimension)
 
 #TODO: can also be done for all edge variables, re-add to original dataset as *onfaces variables?
 if 'mesh2d_vicwwu' in uds.data_vars:
@@ -34,43 +35,46 @@ else:
 fig,ax = plt.subplots()
 uds[varn_onedges].isel(time=-1,nmesh2d_interface=-2,mesh2d_nInterfaces=-2,missing_dims='ignore').ugrid.plot()
 
-mesh2d_var = uds.grid.to_dataset().mesh2d
-
 print('construct indexer')
 dimn_faces = uds.grid.face_dimension
-dimn_maxfn = mesh2d_var.attrs['max_face_nodes_dimension']
+dimn_maxfn = 'nMax_face_nodes' #arbitrary dimname that is reduced anyway
 dimn_layer, dimn_interface = dfmt.get_vertical_dimensions(uds)
 dimn_edges = uds.grid.edge_dimension
 fill_value = uds.grid.fill_value
 
-data_fec = xr.DataArray(uds.grid.face_edge_connectivity,dims=(dimn_faces,dimn_maxfn)) # (8355, 4)
+data_fec = xr.DataArray(uds.grid.face_edge_connectivity,dims=(dimn_faces,dimn_maxfn))
 data_fec_validbool = data_fec!=fill_value
-data_fec = data_fec.where(data_fec_validbool,-1).astype(int)
-data_fec = data_fec.compute()
+data_fec = data_fec.where(data_fec_validbool,-1)
 
-#TODO: interpolation is slow for many timesteps, so maybe use .sel() on time dimension first
 print('interpolation with indexer: step 1 (for each face, select all corresponding edge values)')
 edgevar_tofaces_onint_step1 = uds[varn_onedges].isel({dimn_edges:data_fec})
 print('interpolation with indexer: step 2 (replace nonexistent edges with nan)')
-if hasattr(data_fec,'_FillValue'):
-    edgevar_tofaces_onint_step2 = edgevar_tofaces_onint_step1.where(data_fec_validbool) #replace all values for fillvalue edges (-1) with nan
-else:
-    edgevar_tofaces_onint_step2 = edgevar_tofaces_onint_step1
+edgevar_tofaces_onint_step2 = edgevar_tofaces_onint_step1.where(data_fec_validbool) #replace all values for fillvalue edges (-1) with nan
 print('interpolation with indexer: step 3 (average edge values per face)')
-edgevar_tofaces_onint = edgevar_tofaces_onint_step2.mean(dim=dimn_maxfn)
+edgevar_tofaces_onint = edgevar_tofaces_onint_step2.mean(dim=dimn_maxfn,keep_attrs=True)
 print('interpolation with indexer: done')
 
+#update attrs from edge to face
+face_attrs = {'location': 'face', 'cell_methods': 'nmesh2d_face: mean'}
+edgevar_tofaces_onint = edgevar_tofaces_onint.assign_attrs(face_attrs)
 
 if dimn_interface in edgevar_tofaces_onint.dims:
-    print('average from interfaces to layers (so in z-direction) in case of a 3D model')
-    #select all top interfaces and all bottom interfaces, sum, divide by two (same as average)
-    edgevar_tofaces_topint = edgevar_tofaces_onint.isel({dimn_interface:slice(1,None)})
-    edgevar_tofaces_botint = edgevar_tofaces_onint.isel({dimn_interface:slice(None,-1)})
-    edgevar_tofaces = (edgevar_tofaces_topint + edgevar_tofaces_botint)/2
-    #rename int to lay dimension and re-assign variable attributes
-    edgevar_tofaces = edgevar_tofaces.rename({dimn_interface:dimn_layer}).assign_attrs(edgevar_tofaces_onint_step1.attrs)
+    if 0:
+        print('average from interfaces to layers (so in z-direction) in case of a 3D model')
+        #select all top interfaces and all bottom interfaces, sum, divide by two (same as average)
+        edgevar_tofaces_topint = edgevar_tofaces_onint.isel({dimn_interface:slice(1,None)})
+        edgevar_tofaces_botint = edgevar_tofaces_onint.isel({dimn_interface:slice(None,-1)})
+        edgevar_tofaces = (edgevar_tofaces_topint + edgevar_tofaces_botint)/2
+        # rename int to lay dimension
+        edgevar_tofaces = edgevar_tofaces.rename({dimn_interface:dimn_layer})
+        # re-assign variable attributes
+        edgevar_tofaces = edgevar_tofaces.assign_attrs(edgevar_tofaces_onint.attrs)
+    else: #TODO: this only works if nmesh2d_interface does not accidentally has different index set (like depth)
+        nlayers = uds.dims[dimn_layer]
+        array_shift_half = xr.DataArray(np.arange(0.5,nlayers),dims=dimn_layer)
+        edgevar_tofaces = edgevar_tofaces_onint.interp({dimn_interface:array_shift_half})
 else:
-    edgevar_tofaces = edgevar_tofaces_onint.assign_attrs(edgevar_tofaces_onint_step1.attrs)
+    edgevar_tofaces = edgevar_tofaces_onint.assign_attrs(edgevar_tofaces_onint.attrs)
 
 fig,ax = plt.subplots()
 edgevar_tofaces.isel(time=-1,nmesh2d_layer=-1,mesh2d_nLayers=-1,missing_dims='ignore').ugrid.plot()
