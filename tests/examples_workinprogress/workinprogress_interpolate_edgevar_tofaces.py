398c1cf9a7d57658d556e813c61b4fd72078233d
veenstrajelmer <veenstrajelmer@gmail.com>
2023-07-18 17:54:52 +0200

    minor bugfixes in example script

--- tests/examples_workinprogress/workinprogress_interpolate_edgevar_tofaces.py
+++ tests/examples_workinprogress/workinprogress_interpolate_edgevar_tofaces.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 plt.close('all')
 import xarray as xr
 
-file_nc = dfmt.data.fm_curvedbend_map(return_filepath=True) #sigmalayer
-#file_nc = dfmt.data.fm_grevelingen_map(return_filepath=True) #zlayer
-file_nc = r'p:\dflowfm\maintenance\JIRA\05000-05999\05477\c103_ws_3d_fourier\DFM_OUTPUT_westerscheldt01_0subst\westerscheldt01_0subst_map.nc' #zsigma model without fullgrid output but with new ocean_sigma_z_coordinate variable
-file_nc = r'p:\archivedprojects\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0*_map.nc' #2D model
+# file_nc = dfmt.data.fm_curvedbend_map(return_filepath=True) #sigmalayer
+file_nc = dfmt.data.fm_grevelingen_map(return_filepath=True) #zlayer
+# file_nc = r'p:\dflowfm\maintenance\JIRA\05000-05999\05477\c103_ws_3d_fourier\DFM_OUTPUT_westerscheldt01_0subst\westerscheldt01_0subst_map.nc' #zsigma model without fullgrid output but with new ocean_sigma_z_coordinate variable
+# file_nc = r'p:\archivedprojects\11206813-006-kpp2021_rmm-2d\C_Work\31_RMM_FMmodel\computations\model_setup\run_207\results\RMM_dflowfm_0*_map.nc' #2D model
 # file_nc = r'p:\1204257-dcsmzuno\2006-2012\3D-DCSM-FM\A18b_ntsu1\DFM_OUTPUT_DCSM-FM_0_5nm\DCSM-FM_0_5nm_0*_map.nc' #fullgrid
 # file_nc = r'p:\archivedprojects\11203379-005-mwra-updated-bem\03_model\02_final\A72_ntsu0_kzlb2\DFM_OUTPUT_MB_02\MB_02_0*_map.nc'
 
@@ -35,10 +35,10 @@ uds[varn_onedges].isel(time=-1,nmesh2d_interface=-2,mesh2d_nInterfaces=-2,missin
 mesh2d_var = uds.grid.to_dataset().mesh2d
 
 print('construct indexer')
-varn_fnc = mesh2d_var.attrs['face_node_connectivity']
-#varn_fec = mesh2d_var.attrs['face_edge_connectivity']
 dimn_faces = uds.grid.face_dimension
 dimn_maxfn = mesh2d_var.attrs['max_face_nodes_dimension']
+dimn_interface = mesh2d_var.attrs['interface_dimension']
+dimn_layer = mesh2d_var.attrs['layer_dimension']
 dimn_edges = uds.grid.edge_dimension
 fill_value = uds.grid.fill_value
 
@@ -49,7 +49,7 @@ data_fec = data_fec.compute()
 
 #TODO: interpolation is slow for many timesteps, so maybe use .sel() on time dimension first
 print('interpolation with indexer: step 1 (for each face, select all corresponding edge values)')
-edgevar_tofaces_onint_step1 = uds[varn_onedges].isel({dimn_edges:data_fec}) #TODO: fails for cb_3d_map.nc, westernscheldt
+edgevar_tofaces_onint_step1 = uds[varn_onedges].isel({dimn_edges:data_fec})
 print('interpolation with indexer: step 2 (replace nonexistent edges with nan)')
 if hasattr(data_fec,'_FillValue'):
     edgevar_tofaces_onint_step2 = edgevar_tofaces_onint_step1.where(data_fec_validbool) #replace all values for fillvalue edges (-1) with nan
@@ -60,10 +60,8 @@ edgevar_tofaces_onint = edgevar_tofaces_onint_step2.mean(dim=dimn_maxfn)
 print('interpolation with indexer: done')
 
 
-if hasattr(mesh2d_var,'interface_dimension'):
+if dimn_interface in edgevar_tofaces_onint.dims:
     print('average from interfaces to layers (so in z-direction) in case of a 3D model')
-    dimn_interface = mesh2d_var.attrs['interface_dimension']
-    dimn_layer = mesh2d_var.attrs['layer_dimension']
     #select all top interfaces and all bottom interfaces, sum, divide by two (same as average)
     edgevar_tofaces_topint = edgevar_tofaces_onint.isel({dimn_interface:slice(1,None)})
     edgevar_tofaces_botint = edgevar_tofaces_onint.isel({dimn_interface:slice(None,-1)})
