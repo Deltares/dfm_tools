77bcd6268c38b5c3011c4d736362a705945f8358
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2023-09-30 21:13:59 +0200

    553 fix pandas deprecationwarnings (#556)
    * remove empty gdf before concatenation to avoid pandas futurewarning
    
    * fixed additional pandas futurewarning: ser.iloc[pos] instead of ser[pos]
    
    * update modelbuilder notebook
    
    Former-commit-id: 90e601c4dc4969385fce0626be300073c8edd7c6

--- tests/examples/preprocess_merge_meteofiles.py
+++ tests/examples/preprocess_merge_meteofiles.py
@@ -72,9 +72,9 @@ data_xr_tsel = dfmt.merge_meteofiles(file_nc=file_nc, time_slice=time_slice,
 #write to netcdf file
 print('>> writing file (can take a while): ',end='')
 dtstart = dt.datetime.now()
-times_np = data_xr_tsel['time'].to_series() #used to not work for woa 360_day calendar, .to_numpy() did the trick
-time_start_str = times_np[0].strftime("%Y%m%d")
-time_stop_str = times_np[-1].strftime("%Y%m%d")
+times_pd = data_xr_tsel['time'].to_series() #used to not work for woa 360_day calendar, .to_numpy() did the trick
+time_start_str = times_pd.iloc[0].strftime("%Y%m%d")
+time_stop_str = times_pd.iloc[-1].strftime("%Y%m%d")
 file_out = os.path.join(dir_output, f'{file_out_prefix}{time_start_str}to{time_stop_str}_{mode}.nc')
 data_xr_tsel.to_netcdf(file_out)
 print(f'{(dt.datetime.now()-dtstart).total_seconds():.2f} sec')
