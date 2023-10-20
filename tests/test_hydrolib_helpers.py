498956242af9fc86529cf8bee3e132a932b92bd8
veenstrajelmer <veenstrajelmer@gmail.com>
2023-10-20 16:40:53 +0200

    cleanup
    Former-commit-id: 29f9488fe9aba5993ea588fe3979bbc8eb6557b2

--- tests/test_hydrolib_helpers.py
+++ tests/test_hydrolib_helpers.py
@@ -17,7 +17,6 @@ def test_geodataframe_with_Polygon_to_PolyFile():
     converting a geodataframe with Polygon geometries to hcdfm.PolyFile
     """
     lon_min, lon_max, lat_min, lat_max = -6, 2, 48.5, 51.2
-
     bbox = (lon_min, lat_min, lon_max, lat_max)
     
     # gdf containing shapely.Polygon geometries
@@ -41,7 +40,6 @@ def test_geodataframe_with_LineString_to_PolyFile():
     polyfile.save(file_pol)
     
     # read polygon file
-    # file_pol = r'p:\archivedprojects\11208054-004-dcsm-fm\models\model_input\bnd_cond\pli\DCSM-FM_OB_all_20181108.pli'
     polyfile_object = hcdfm.PolyFile(file_pol)
 
     # gdf containing shapely.LineString geometries
