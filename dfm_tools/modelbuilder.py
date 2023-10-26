b1914cc2b809a672b32a1385d75868e6ededa4d8
veenstrajelmer <veenstrajelmer@gmail.com>
2023-10-26 12:31:01 +0200

    second step

--- dfm_tools/modelbuilder.py
+++ dfm_tools/modelbuilder.py
@@ -61,6 +61,7 @@ def cmems_nc_to_ini(ext_old, dir_output, list_quantities, tstart, dir_pattern, c
     for quan_bnd in list_quantities:
         
         if quan_bnd=="salinitybnd":
+            ncvarname = None
             file_nc_list_so = glob.glob(dir_pattern.format(ncvarname='so'))
             file_nc_list_thetao = glob.glob(dir_pattern.format(ncvarname='thetao'))
             file_nc_list = file_nc_list_so + file_nc_list_thetao
@@ -71,7 +72,7 @@ def cmems_nc_to_ini(ext_old, dir_output, list_quantities, tstart, dir_pattern, c
             interp = False
         elif quan_bnd in ["temperaturebnd","uxuyadvectionvelocitybnd"]:
             # silently skipped, temperature is handled with salinity, uxuy not supported
-            pass
+            continue
         else:
             ncvarname = conversion_dict[quan_bnd]["ncvarname"]
             file_nc_list = glob.glob(dir_pattern.format(ncvarname=ncvarname))
@@ -80,6 +81,12 @@ def cmems_nc_to_ini(ext_old, dir_output, list_quantities, tstart, dir_pattern, c
         
         print(f'opening {len(file_nc_list)} datasets')
         data_xr = xr.open_mfdataset(file_nc_list)
+        tstart_round, tstop_round = dfmt.round_timestamp_to_outer_noon(tstart,tstart)
+        
+        data_xr = dfmt.open_dataset_extra(dir_pattern=dir_pattern, quantity=quan_bnd,
+                                          tstart=tstart_round, tstop=tstop_round,
+                                          conversion_dict=conversion_dict)
+        data_xr = data_xr.rename_vars({quan_bnd:quantity}) #TODO: prevent for inisalnudge
         
         # fill nans
         # start with lat/lon to avoid values from shallow coastal areas in deep layers
@@ -94,7 +101,7 @@ def cmems_nc_to_ini(ext_old, dir_output, list_quantities, tstart, dir_pattern, c
         if interp: #TODO: test bounds error (or load)
             data_xr_ontime = data_xr.interp(time=[tstart_pd],kwargs=dict(bounds_error=True)) #bounds_error makes sure, outofbounds time results in "ValueError: A value in x_new is below the interpolation range."
         else:
-            data_xr_ontime = data_xr.sel(time=slice(tstart_pd-dt.timedelta(days=1),tstart_pd+dt.timedelta(days=1)))
+            data_xr_ontime = data_xr.sel(time=slice(tstart_round, tstop_round))
 
         print('writing file')
         file_output = os.path.join(dir_output,f"{quantity}_{tstart_str}.nc")
@@ -102,6 +109,7 @@ def cmems_nc_to_ini(ext_old, dir_output, list_quantities, tstart, dir_pattern, c
         
         #append forcings to ext
         forcing_saltem = hcdfm.ExtOldForcing(quantity=quantity,
+                                             varname=quantity,
                                              filename=file_output,
                                              filetype=hcdfm.ExtOldFileType.NetCDFGridData,
                                              method=hcdfm.ExtOldMethod.InterpolateTimeAndSpaceSaveWeights, #3
