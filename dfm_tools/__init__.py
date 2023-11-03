0b11e13a68f35b3299873c2e9dfde9a770dff1ab
veenstrajelmer <veenstrajelmer@gmail.com>
2023-11-03 21:34:10 +0100

    added py38 deprecationwarning

--- dfm_tools/__init__.py
+++ dfm_tools/__init__.py
@@ -32,6 +32,6 @@ version_info = sys.version_info
 if (version_info.major == 3) & (version_info.minor <= 8):
     warnings.warn(
         DeprecationWarning("You are using Python 3.8 or lower, please note that "
-                           "support will soon be dropped by dfm_tools: "
+                           "dfm_tools will soon drop support for Python 3.8: "
                            "https://github.com/Deltares/dfm_tools/issues/267")
         )
