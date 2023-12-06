0fcf8041d4cc38090bd56c467eb96aacd0bd8623
veenstrajelmer <jelmer.veenstra@deltares.nl>
2023-12-06 17:28:35 +0100

    added download tests for cds and cmems

--- tests/test_download.py
+++ tests/test_download.py
@@ -10,6 +10,7 @@ import pytest
 import pandas as pd
 import numpy as np
 from dfm_tools.download import round_timestamp_to_outer_noon, cds_credentials, copernicusmarine_credentials
+import dfm_tools as dfmt
 
 
 @pytest.mark.unittest
@@ -47,3 +48,38 @@ def test_cds_credentials():
 @pytest.mark.unittest
 def test_copernicusmarine_credentials():
     copernicusmarine_credentials()
+
+
+#TODO: properly set environment variables in github would prevent localness
+@pytest.mark.requireslocaldata
+@pytest.mark.unittest
+def test_download_era5():
+    date_min = '2010-01-01'
+    date_max = '2010-01-02'
+    longitude_min, longitude_max, latitude_min, latitude_max =    2,   3,  51, 52 #test domain
+    variables_era5 = ['msl']#'v10n'] # check variables_dict in dfmt.download_ERA5() for valid names
+    dir_output = './era5_temp'
+    for varkey in variables_era5:
+        os.makedirs(dir_output, exist_ok=True)
+        
+        dfmt.download_ERA5(varkey, 
+                           longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
+                           date_min=date_min, date_max=date_max,
+                           dir_output=dir_output, overwrite=True)
+
+
+#TODO: properly set environment variables in github would prevent localness
+@pytest.mark.requireslocaldata
+@pytest.mark.unittest
+def test_download_cmems():
+    date_min = '2010-01-01'
+    date_max = '2010-01-02'
+    longitude_min, longitude_max, latitude_min, latitude_max =    2,   3,  51, 52 #test domain
+    varlist_cmems = ['bottomT','no3'] # avaliable variables differ per product, examples are ['bottomT','mlotst','siconc','sithick','so','thetao','uo','vo','usi','vsi','zos','no3']. More info on https://data.marine.copernicus.eu/products
+    dir_output = './cmems_temp'
+    for varkey in varlist_cmems:
+        file_prefix = 'cmems_'
+        dfmt.download_CMEMS(varkey=varkey,
+                            longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
+                            date_min=date_min, date_max=date_max,
+                            dir_output=dir_output, file_prefix=file_prefix, overwrite=True)
