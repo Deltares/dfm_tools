108a0c04cc1259ebef72102609316cf2e298c679
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-01-24 11:01:24 +0100

    pass to other testbanks and cleanup code

--- tests/test_download.py
+++ tests/test_download.py
@@ -12,8 +12,6 @@ from dfm_tools.download import cds_credentials, copernicusmarine_credentials
 import dfm_tools as dfmt
 
 
-@pytest.mark.requiressecrets
-@pytest.mark.requireslocaldata
 @pytest.mark.unittest
 def test_cds_credentials():
     # check if the credentials are present on this system
@@ -24,17 +22,11 @@ def test_cds_credentials():
     assert "CDSAPI_URL" in os.environ.keys()
 
 
-#TODO: properly set environment variables in github would prevent localness
-@pytest.mark.requiressecrets
-@pytest.mark.requireslocaldata
 @pytest.mark.unittest
 def test_copernicusmarine_credentials():
     copernicusmarine_credentials()
 
 
-#TODO: properly set environment variables in github would prevent localness
-@pytest.mark.requiressecrets
-@pytest.mark.requireslocaldata
 @pytest.mark.unittest
 def test_download_era5(tmp_path):
     date_min = '2010-01-01'
@@ -48,9 +40,6 @@ def test_download_era5(tmp_path):
                            dir_output=tmp_path, overwrite=True)
 
 
-#TODO: properly set environment variables in github would prevent localness
-@pytest.mark.requiressecrets
-@pytest.mark.requireslocaldata
 @pytest.mark.unittest
 def test_download_cmems_my(tmp_path):
     date_min = '2010-01-01'
@@ -65,9 +54,6 @@ def test_download_cmems_my(tmp_path):
                             dir_output=tmp_path, file_prefix=file_prefix, overwrite=True)
 
 
-#TODO: properly set environment variables in github would prevent localness
-@pytest.mark.requiressecrets
-@pytest.mark.requireslocaldata
 @pytest.mark.unittest
 def test_download_cmems_forecast(tmp_path):
     date_min = pd.Timestamp.today()
