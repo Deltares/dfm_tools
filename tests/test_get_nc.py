67aaafd6733d34872766543d2a52987874462649
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-01-25 15:50:33 +0100

    remove mesh2d hardcoding in layer reconstruction (#747)
    * remove mesh2d hardcoding in layer reconstruction
    
    * updated whatsnew
    
    * prevent calling reconstruct_zw_zcc from get_Dataset_atdepths for hisfiles

--- tests/test_get_nc.py
+++ tests/test_get_nc.py
@@ -9,6 +9,7 @@ Created on Wed Dec 13 12:17:41 2023
 import pytest
 import numpy as np
 import dfm_tools as dfmt
+import xarray as xr
 
 
 @pytest.mark.unittest
@@ -25,3 +26,22 @@ def test_polyline_mapslice():
     assert np.isclose(uds_crs.grid.node_x.max(), 1737.0864257281437)
     assert np.isclose(uds_crs.grid.node_y.min(), -5)
     assert np.isclose(uds_crs.grid.node_y.max(), 0.9261683648147339)
+
+
+def test_get_dataset_atdepths_hisfile():
+    
+    file_nc = dfmt.data.fm_grevelingen_his(return_filepath=True)
+    ds = xr.open_dataset(file_nc)#, preprocess=dfmt.preprocess_hisnc)
+
+    depths = [-1,-4,0,-6]
+    data_fromhis_atdepths = dfmt.get_Dataset_atdepths(data_xr=ds, depths=depths, reference='z0')
+    data_xr_selzt = data_fromhis_atdepths.isel(stations=2).isel(time=slice(40,100))
+
+    tem_values = data_xr_selzt.temperature.isel(time=-1).to_numpy()
+    exp_values = np.array([5.81065253, 5.43289777, 5.36916911, 5.36916911])
+
+    assert data_xr_selzt.temperature.shape == (60,4)
+    assert np.allclose(tem_values, exp_values)
+    assert np.isclose(data_xr_selzt.temperature.sum(), 1295.56826688)
+
+
