01b064c051c71b9f82c3cf20e20d0f21f7535a16
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-01-26 17:55:53 +0100

    add plot_borders

--- dfm_tools/coastlines.py
+++ dfm_tools/coastlines.py
@@ -13,6 +13,8 @@ from dfm_tools.data import gshhs_coastlines_shp
 
 __all__ = ["get_coastlines_gdb",
            "plot_coastlines",
+           "get_borders_gdb",
+           "plot_borders",
     ]
 
 
@@ -81,6 +83,49 @@ def get_coastlines_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), min_area:
     return coastlines_gdb
 
 
+def get_borders_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), crs:str = None) -> gpd.geoseries.GeoSeries:
+    """
+    GSHHS coastlines: https://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/readme.txt
+    geopandas docs https://geopandas.org/en/stable/docs/reference/api/geopandas.read_file.html
+    
+    Parameters
+    ----------
+    res : str, optional
+        f(ull), h(igh), i(ntermediate), l(ow), c(oarse) resolution. The default is 'h'.
+    bbox : tuple, optional
+        (minx, miny, maxx, maxy), also includes shapes that are partly in the bbox. The default is (-180, -90, 180, 90).
+    crs : str, optional
+    
+    Returns
+    -------
+    coastlines_gdb : TYPE
+        DESCRIPTION.
+
+    """
+    
+    if res not in 'fhilc':
+        raise KeyError(f'invalid res="{res}", resolution options are f(ull), h(igh), i(ntermediate), l(ow), c(oarse)')
+    if crs is not None: #convert bbox from input crs to WGS84
+        bbox_points = gpd.points_from_xy(x=[bbox[0],bbox[2]], y=[bbox[1],bbox[3]], crs=crs)
+        bbox_points = bbox_points.to_crs('EPSG:4326') #convert to WGS84
+        bbox = (bbox_points.x[0], bbox_points.y[0], bbox_points.x[1], bbox_points.y[1])
+        
+    # download gshhs data if not present and return dir
+    dir_gshhs = gshhs_coastlines_shp()
+
+    file_shp_L1 = os.path.join(dir_gshhs,'WDBII_shp',res,f'WDBII_border_{res}_L1.shp') #borders
+    
+    print('>> reading coastlines: ',end='')
+    dtstart = dt.datetime.now()
+    coastlines_gdb = gpd.read_file(file_shp_L1, bbox=bbox)
+    print(f'{(dt.datetime.now()-dtstart).total_seconds():.2f} sec')
+    
+    if crs:
+        coastlines_gdb = coastlines_gdb.to_crs(crs)
+    
+    return coastlines_gdb
+
+
 def plot_coastlines(ax, res:str='h', min_area:float = 0, crs=None, **kwargs):
     """
     get coastlines with get_coastlines_gdb and bbox depending on axlims, plot on ax and set axlims back to original values
@@ -99,6 +144,33 @@ def plot_coastlines(ax, res:str='h', min_area:float = 0, crs=None, **kwargs):
         kwargs['linewidth'] = 0.7
     
     coastlines_gdb = get_coastlines_gdb(bbox=bbox, res=res, min_area=min_area, crs=crs)
+    if coastlines_gdb.empty:
+        return
+    
+    coastlines_gdb.plot(ax=ax, **kwargs)
+    
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+
+def plot_borders(ax, res:str='h', crs=None, **kwargs):
+    """
+    get borders with get_borders_gdb and bbox depending on axlims, plot on ax and set axlims back to original values
+    """
+    #TODO: if ax is GeoAxis, get crs from ax
+    
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    bbox = (xlim[0], ylim[0], xlim[1], ylim[1])
+    
+    if 'edgecolor' not in kwargs:
+        kwargs['edgecolor'] = 'k'
+    if 'facecolor' not in kwargs:
+        kwargs['facecolor'] = 'none'
+    if 'linewidth' not in kwargs:
+        kwargs['linewidth'] = 0.7
+    
+    coastlines_gdb = get_borders_gdb(bbox=bbox, res=res, crs=crs)
     if coastlines_gdb.empty:
         return
     
