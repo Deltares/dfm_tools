45756af6d838dae174b9e7aff477b0942806d616
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-01-26 18:37:32 +0100

    add plot_borders (#750)
    * add plot_borders
    
    * updated whatsnew
    
    * auto-derive axis with plt.gca()
    
    * add testcases
    
    * decrease linewidth

--- dfm_tools/coastlines.py
+++ dfm_tools/coastlines.py
@@ -7,13 +7,26 @@ Created on Mon Apr 24 03:46:44 2023
 
 import os
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import pandas as pd
 import datetime as dt
 from dfm_tools.data import gshhs_coastlines_shp
 
 __all__ = ["get_coastlines_gdb",
            "plot_coastlines",
+           "get_borders_gdb",
+           "plot_borders",
     ]
+
+
+def bbox_convert_crs(bbox, crs):
+    """
+    convert bbox from input crs to WGS84
+    """
+    bbox_points = gpd.points_from_xy(x=[bbox[0],bbox[2]], y=[bbox[1],bbox[3]], crs=crs)
+    bbox_points = bbox_points.to_crs('EPSG:4326') #convert to WGS84
+    bbox = (bbox_points.x[0], bbox_points.y[0], bbox_points.x[1], bbox_points.y[1])
+    return bbox
 
 
 def get_coastlines_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), min_area:float = 0, crs:str = None, include_fields:list = ['area']) -> gpd.geoseries.GeoSeries:
@@ -43,10 +56,8 @@ def get_coastlines_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), min_area:
     
     if res not in 'fhilc':
         raise KeyError(f'invalid res="{res}", resolution options are f(ull), h(igh), i(ntermediate), l(ow), c(oarse)')
-    if crs is not None: #convert bbox from input crs to WGS84
-        bbox_points = gpd.points_from_xy(x=[bbox[0],bbox[2]], y=[bbox[1],bbox[3]], crs=crs)
-        bbox_points = bbox_points.to_crs('EPSG:4326') #convert to WGS84
-        bbox = (bbox_points.x[0], bbox_points.y[0], bbox_points.x[1], bbox_points.y[1])
+    if crs is not None:
+        bbox = bbox_convert_crs(bbox, crs)
         
     # download gshhs data if not present and return dir
     dir_gshhs = gshhs_coastlines_shp()
@@ -81,11 +92,55 @@ def get_coastlines_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), min_area:
     return coastlines_gdb
 
 
-def plot_coastlines(ax, res:str='h', min_area:float = 0, crs=None, **kwargs):
+def get_borders_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), crs:str = None) -> gpd.geoseries.GeoSeries:
+    """
+    GSHHS coastlines: https://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/readme.txt
+    geopandas docs https://geopandas.org/en/stable/docs/reference/api/geopandas.read_file.html
+    
+    Parameters
+    ----------
+    res : str, optional
+        f(ull), h(igh), i(ntermediate), l(ow), c(oarse) resolution. The default is 'h'.
+    bbox : tuple, optional
+        (minx, miny, maxx, maxy), also includes shapes that are partly in the bbox. The default is (-180, -90, 180, 90).
+    crs : str, optional
+    
+    Returns
+    -------
+    coastlines_gdb : TYPE
+        DESCRIPTION.
+
+    """
+    
+    if res not in 'fhilc':
+        raise KeyError(f'invalid res="{res}", resolution options are f(ull), h(igh), i(ntermediate), l(ow), c(oarse)')
+    if crs is not None:
+        bbox = bbox_convert_crs(bbox, crs)
+        
+    # download gshhs data if not present and return dir
+    dir_gshhs = gshhs_coastlines_shp()
+
+    file_shp_L1 = os.path.join(dir_gshhs,'WDBII_shp',res,f'WDBII_border_{res}_L1.shp') #borders
+    
+    print('>> reading coastlines: ',end='')
+    dtstart = dt.datetime.now()
+    coastlines_gdb = gpd.read_file(file_shp_L1, bbox=bbox)
+    print(f'{(dt.datetime.now()-dtstart).total_seconds():.2f} sec')
+    
+    if crs:
+        coastlines_gdb = coastlines_gdb.to_crs(crs)
+    
+    return coastlines_gdb
+
+
+def plot_coastlines(ax=None, res:str='h', min_area:float = 0, crs=None, **kwargs):
     """
     get coastlines with get_coastlines_gdb and bbox depending on axlims, plot on ax and set axlims back to original values
     """
     #TODO: if ax is GeoAxis, get crs from ax
+    
+    if ax is None:
+        ax = plt.gca()
     
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
@@ -96,9 +151,37 @@ def plot_coastlines(ax, res:str='h', min_area:float = 0, crs=None, **kwargs):
     if 'facecolor' not in kwargs:
         kwargs['facecolor'] = 'none'
     if 'linewidth' not in kwargs:
-        kwargs['linewidth'] = 0.7
+        kwargs['linewidth'] = 0.5
     
     coastlines_gdb = get_coastlines_gdb(bbox=bbox, res=res, min_area=min_area, crs=crs)
+    if coastlines_gdb.empty:
+        return
+    
+    coastlines_gdb.plot(ax=ax, **kwargs)
+    
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+
+def plot_borders(ax=None, res:str='h', crs=None, **kwargs):
+    """
+    get borders with get_borders_gdb and bbox depending on axlims, plot on ax and set axlims back to original values
+    """
+    #TODO: if ax is GeoAxis, get crs from ax
+    
+    if ax is None:
+        ax = plt.gca()
+    
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    bbox = (xlim[0], ylim[0], xlim[1], ylim[1])
+    
+    if 'edgecolor' not in kwargs:
+        kwargs['edgecolor'] = 'grey'
+    if 'linewidth' not in kwargs:
+        kwargs['linewidth'] = 0.5
+    
+    coastlines_gdb = get_borders_gdb(bbox=bbox, res=res, crs=crs)
     if coastlines_gdb.empty:
         return
     
--- tests/test_coastlines.py
+++ tests/test_coastlines.py
@@ -8,14 +8,14 @@ Created on Sun Jul  9 22:35:28 2023
 import pytest
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from shapely import Polygon
+from shapely import Polygon, LineString
 import dfm_tools as dfmt
 
 
 @pytest.mark.unittest
 def test_get_coastlines_gdb_global():
     """
-    opens global coarse coastlines, so includes L6/Antarctic
+    opens global coarse coastlines at lower 30 degrees, so includes L6/Antarctic
     """
     min_area = 1000 # km2
     bbox = (-180, -90, 180, -60) # xmin, ymin, xmax, ymax
@@ -59,4 +59,39 @@ def test_plot_coastlines():
     fig, ax = plt.subplots()
     ax.set_xlim(-10,0)
     ax.set_ylim(50,60)
-    dfmt.plot_coastlines(ax=ax, res='l', min_area=500)
+    dfmt.plot_coastlines(res='l', min_area=500)
+
+
+@pytest.mark.unittest
+def test_get_borders_gdb_global():
+    """
+    opens global coarse borders
+    """
+    bbox = (-180, -90, 180, 90) # xmin, ymin, xmax, ymax
+    res = 'c'
+    gdf = dfmt.get_borders_gdb(res=res, bbox=bbox)
+    
+    assert len(gdf) == 451
+
+
+@pytest.mark.unittest
+def test_get_borders_gdb_allres():
+    """
+    opens borders gdb of Ireland and UK
+    """
+    bbox = (-10, 40, 5, 60) # xmin, ymin, xmax, ymax
+    for res in 'fhilc':
+        gdf = dfmt.get_borders_gdb(res=res, bbox=bbox)
+        assert len(gdf) == 9
+    assert isinstance(gdf,gpd.GeoDataFrame)
+    assert isinstance(gdf.geometry[0],LineString)
+    assert 'geometry' in gdf.columns
+
+
+@pytest.mark.unittest
+def test_plot_borders():
+    
+    fig, ax = plt.subplots()
+    ax.set_xlim(-10,5)
+    ax.set_ylim(40,60)
+    dfmt.plot_borders(res='l')
