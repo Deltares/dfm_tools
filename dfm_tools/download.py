15dc4b8a048f193a9495dc7ce9638841763590aa
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-02-22 16:58:05 +0100

    add no_metadata_cache argument to copernicusmarine_credentials() (#787)
    * add no_metadata_cache argument to copernicusmarine_credentials() to support copernicusmarine>=1.0.2
    
    * updated whatsnew
    
    * modularity in copernicusmarine_reset()
    
    * update minimal cmc version

--- dfm_tools/download.py
+++ dfm_tools/download.py
@@ -316,26 +316,29 @@ def copernicusmarine_credentials():
         InvalidUsernameOrPassword,
         get_and_check_username_password,
     )
+    login_kwargs = dict(username=None, password=None, credentials_file=DEFAULT_CLIENT_CREDENTIALS_FILEPATH, no_metadata_cache=False)
     if not DEFAULT_CLIENT_CREDENTIALS_FILEPATH.is_file():
         print("Downloading CMEMS data requires a Copernicus Marine username and password, sign up for free at: https://data.marine.copernicus.eu/register.")
-        username, password = get_and_check_username_password(username=None, password=None, credentials_file=DEFAULT_CLIENT_CREDENTIALS_FILEPATH)
+        username, password = get_and_check_username_password(**login_kwargs)
         success = copernicusmarine.login(username, password)
         if not success:
             raise InvalidUsernameOrPassword("invalid credentials")
     else:
-        username, password = get_and_check_username_password(username=None, password=None, credentials_file=DEFAULT_CLIENT_CREDENTIALS_FILEPATH)
+        username, password = get_and_check_username_password(**login_kwargs)
     return username, password
 
 
-def copernicusmarine_reset():
-    print("resetting Copernicus Marine Toolbox, you will have to login again.")
-    dir_copernicusmarine = os.path.expanduser("~/.copernicusmarine")
-    print(f"- removing {dir_copernicusmarine}")
-    shutil.rmtree(dir_copernicusmarine, ignore_errors=True)
-    print("- overwriting copernicusmarine metadata cache (takes some time)")
-    subprocess.run("copernicusmarine describe --overwrite-metadata-cache")
-    print("- updating copernicusmarine")
-    subprocess.run("pip install copernicusmarine -U")
+def copernicusmarine_reset(remove_folder=False, overwrite_cache=True, update_package=False):
+    if remove_folder:
+        dir_copernicusmarine = os.path.expanduser("~/.copernicusmarine")
+        print(f"reset copernicusmarine: removing {dir_copernicusmarine}, you will have to login again.")
+        shutil.rmtree(dir_copernicusmarine, ignore_errors=True)
+    if overwrite_cache:
+        print("reset copernicusmarine: overwriting copernicusmarine metadata cache (takes some time)")
+        subprocess.run("copernicusmarine describe --overwrite-metadata-cache")
+    if update_package:
+        print("reset copernicusmarine: updating copernicusmarine")
+        subprocess.run("pip install copernicusmarine -U")
 
 
 def copernicusmarine_dataset_timerange(dataset_id):
--- docs/whats-new.md
+++ docs/whats-new.md
@@ -1,5 +1,8 @@
 ## UNRELEASED
 
+### Fix
+- solve xarray FutureWarning about `ds.dims[dim]` by [@veenstrajelmer](https://github.com/veenstrajelmer) in [#779](https://github.com/Deltares/dfm_tools/pull/779)
+- support copernicusmarine>=1.0.2 in `copernicusmarine_credentials()` and made `copernicusmarine_reset()` modular by [@veenstrajelmer](https://github.com/veenstrajelmer) in [#787](https://github.com/Deltares/dfm_tools/pull/787)
 
 
 ## 0.19.0 (2024-02-08)
