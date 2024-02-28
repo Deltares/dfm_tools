46c7727a714cffbce81dca5375498a8417d0be89
veenstrajelmer <veenstrajelmer@gmail.com>
2024-02-28 15:10:20 +0100

    added print per station

--- dfm_tools/observations.py
+++ dfm_tools/observations.py
@@ -828,13 +828,17 @@ def ddl_ssh_retrieve_data(ssh_catalog_gpd, dir_output, time_min, time_max, meta_
     
     cat_locatielijst = ssh_catalog_gpd.set_index("Code", drop=False)
     
+    # print(f"retrieving data for {len(ssh_catalog_gpd)} ddl stations:", end=" ")
     for station_id, row in cat_locatielijst.iterrows():
+        irow = cat_locatielijst.index.tolist().index(station_id)
+        # print(irow+1, end=" ")
+
         station_dict = row[['Locatie_MessageID','X','Y','Naam','Code']] #station query
         
         allow_multipleresultsfor = ['WaardeBepalingsmethode'] # necessary for retrieving very long timeseries
         
         #retrieving waterlevels
-        print(f'retrieving measwl data from DDL for {row["Code"]}')
+        print(f'retrieving measwl data from DDL for station {irow+1} of {len(cat_locatielijst)}: {row["Code"]}')
         request_output = hatyan.get_DDL_data(station_dict=station_dict,
                                              tstart_dt=time_min, tstop_dt=time_max, tzone='UTC+00:00', 
                                              meta_dict=meta_dict, 
