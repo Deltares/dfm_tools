0a3e1ac2e5a88a22491eac0aa626e8bfdd3535f4
veenstrajelmer <veenstrajelmer@gmail.com>
2024-02-28 17:35:45 +0100

    update example insitu notebook with ddl

--- tests/test_observations.py
+++ tests/test_observations.py
@@ -64,12 +64,7 @@ def test_ssh_retrieve_data(source, tmp_path):
     
     ssc_catalog_gpd = dfmt.ssh_catalog_subset(source=source)
     ssc_catalog_gpd_sel = ssc_catalog_gpd.iloc[:1]
-    if source=="ssc":
-        # ssc does not contain data, only station locations
-        pass
-    elif source=="cmems": #TODO: remove this exception when the cmems API works for insitu data
-        dfmt.ssh_retrieve_data(ssc_catalog_gpd_sel, dir_output=tmp_path)
-    else:
+    if source!="ssc": # ssc does not contain data, only station locations
         dfmt.ssh_retrieve_data(ssc_catalog_gpd_sel, dir_output=tmp_path, 
                                time_min=time_min, time_max=time_max)
 
