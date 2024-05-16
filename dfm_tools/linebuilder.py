dcccc859c9e32d02dd043c738b71adec2751d0a7
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-05-16 10:58:33 +0200

    add more features to linebuilder

--- dfm_tools/linebuilder.py
+++ dfm_tools/linebuilder.py
@@ -15,30 +15,50 @@ class LineBuilder:
         self.ys = list(line.get_ydata())
         self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
         self.cid = line.figure.canvas.mpl_connect('key_press_event', self)
+        print("Draw a line interactively:\n"
+              "- ctrl+leftmouseclick to draw a line\n"
+              "- TODO: rightmouseclick to undo\n"
+              "- TODO: ctrl+doublemouseclick to wrap up and return line_array\n"
+              )
         
 
+    @property
+    def line_array(self):
+        line_array = np.c_[self.xs, self.ys]
+        return line_array
+    
     def __call__(self, event):
-        print('click', event)
-        print(event.key)
+        # print('click', event)
+        
+        if not hasattr(event, "button"):
+            # don't do anything no mouse button is pressed
+            # key event has no button: AttributeError: 'KeyEvent' object has no attribute 'button'
+            return
         
         if event.key != "control":
             # don't do anything if ctrl is not pressed
             return
-            
+        
+        if event.button == 3:
+            print('WARNING: right mouse click is ignored')
+            return            
+        
         if event.dblclick:
-            print('WARNING: double click is interpreted as single click')
+            print('WARNING: double click is ignored')
             return
+        
         if event.inaxes!=self.line.axes:
-            print('WARNING: that click was outside the figure axis')
+            print('WARNING: that click was outside the figure axis, it is ignored')
             return
         
-        
-        # if event.click:
+        # otherwise append to line
+        print(f"x={event.xdata}m y={event.ydata}")
         self.xs.append(event.xdata)
         self.ys.append(event.ydata)
-        self.line_array = np.c_[self.xs, self.ys]
+        # self.line_array = 
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
+
         
 # key_press_event: 'canvas', 'guiEvent', 'inaxes', 'key', 'lastevent', 'modifiers', 'name', 'x', 'xdata', 'y', 'ydata']
 # button_press_event: 'button', 'canvas', 'dblclick', 'guiEvent', 'inaxes', 'key', 'lastevent', 'modifiers', 'name', 'step', 'x', 'xdata', 'y', 'ydata']
