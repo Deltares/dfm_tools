e9d0f6369a8fe70a0e51f7f45d74ba54147a110e
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-05-17 07:19:41 +0200

    updated linebuilder docs

--- dfm_tools/linebuilder.py
+++ dfm_tools/linebuilder.py
@@ -6,7 +6,8 @@ __all__ = ["LineBuilder"]
 
 class LineBuilder:
     """
-    To interactively draw a line in a figure axis, for instance to use as cross-section.
+    To interactively draw a line in a figure axis, for instance to use as cross-section line for `dfmt.polyline_mapslice()`.
+    
     - ctrl+leftmouseclick to add a point to the line
     - ctrl+rightmouseclick to remove the last point of the line
     - ctrl+doublemouseclick to finish and let the script continue
@@ -25,31 +26,34 @@ class LineBuilder:
         self.line = line
         
         # register both button press events and key press events
-        self.cid_button = self.line.figure.canvas.mpl_connect('button_press_event', self.on_press)
-        self.cid_key = self.line.figure.canvas.mpl_connect('key_press_event', self.on_press)
+        self.cid_button = self.line.figure.canvas.mpl_connect('button_press_event', self)
+        self.cid_key = self.line.figure.canvas.mpl_connect('key_press_event', self)
         # start a blocking event loop to prevent continuation of the script where LineBuilder was called
         self.line.figure.canvas.start_event_loop()
     
     @property
     def line_array(self):
+        """
+        numpy array of the x/y coordinates of the interactively clicked line.
+        """
         line_array = np.c_[self.xs, self.ys]
         return line_array
     
-    def add_xy_to_line(self, event):
+    def _add_xy_to_line(self, event):
         print(f"adding point: x={event.xdata:.6f}, y={event.ydata:.6f}")
         self.xs.append(event.xdata)
         self.ys.append(event.ydata)
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
 
-    def remove_last_xy_from_line(self, event):
+    def _remove_last_xy_from_line(self, event):
         print("removing last point if present")
         self.xs = self.xs[:-1]
         self.ys = self.ys[:-1]
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
 
-    def finish_linebuilder(self):
+    def _finish_linebuilder(self):
         # disconnect the interactive line drawing in the figure
         self.line.figure.canvas.mpl_disconnect(self.cid_button)
         self.line.figure.canvas.mpl_disconnect(self.cid_key)
@@ -57,7 +61,7 @@ class LineBuilder:
         self.line.figure.canvas.stop_event_loop()
         print("interactive line drawing finished")
     
-    def on_press(self, event):
+    def __call__(self, event):
         # do nothing if ctrl is not pressed
         if event.key != "control":
             return
@@ -74,13 +78,13 @@ class LineBuilder:
         
         # add new point to line and wrap up line upon "ctrl + left mouse double click"
         if event.dblclick:
-            self.finish_linebuilder()
+            self._finish_linebuilder()
             return
         
         # add new point to line upon "ctrl + left mouse click"
         if event.button == 1:
-            self.add_xy_to_line(event)
+            self._add_xy_to_line(event)
         
         # remove last point from line upon "ctrl + right mouse click"
         if event.button == 3:
-            self.remove_last_xy_from_line(event)
+            self._remove_last_xy_from_line(event)
