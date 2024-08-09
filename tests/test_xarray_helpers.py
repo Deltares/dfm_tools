577e17930820fa663bd6ff003160549625054164
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-08-09 16:24:59 +0200

    942 fix test prevent dtype int (#943)
    * added extra dtype assertions to test_download_era5
    
    * simplified prevent_dtype_int() and merged with dfmt.preprocess_ERA5()
    
    * removed prevent_dtype_int() also in merge_meteofiles()
    
    Former-commit-id: ed77eb0a0e32338857f648da409ac89acc23ef03

--- tests/test_xarray_helpers.py
+++ tests/test_xarray_helpers.py
@@ -5,45 +5,11 @@ Created on Tue Jan 23 17:12:48 2024
 @author: veenstra
 """
 
-
-import os
 import pytest
-import xarray as xr
-import numpy as np
-from dfm_tools.xarray_helpers import prevent_dtype_int
 import dfm_tools as dfmt
 import pandas as pd
 
 #TODO: many xarray_helpers tests are still in test_dfm_tools.py
-
-
-@pytest.mark.requiressecrets
-@pytest.mark.unittest
-@pytest.mark.timeout(60) # useful since CDS downloads are terribly slow sometimes, so skip in that case
-def test_prevent_dtype_int(tmp_path, file_nc_era5_pattern):
-    # file_nc_era5_pattern comes from file_nc_era5_pattern() in conftest.py
-    varn = "msl"
-    file_nc = os.path.join(tmp_path,f'era5_{varn}_*.nc')
-    
-    #optional encoding
-    for zlib, size_expected in zip([False, True], [480000, 250000]):
-        data_xr = xr.open_mfdataset(file_nc)
-        prevent_dtype_int(data_xr, zlib=zlib)
-        
-        #write to netcdf file
-        file_out = os.path.join(tmp_path, f'era5_prevent_dtype_int_{varn}_zlib_{zlib}.nc')
-        data_xr.to_netcdf(file_out)
-        data_xr_check = xr.open_dataset(file_out)
-        print(data_xr_check[varn].encoding)
-        
-        assert np.allclose(data_xr[varn], data_xr_check[varn])
-        
-        del data_xr
-        del data_xr_check
-        
-        file_size = os.path.getsize(file_out)
-        print(file_size)
-        assert file_size < size_expected
 
 
 @pytest.mark.requiressecrets
