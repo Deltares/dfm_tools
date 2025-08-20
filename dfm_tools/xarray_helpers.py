cfa929d7b332f5d951e2476f8fb84a4280ae7da3
veenstrajelmer <veenstrajelmer@gmail.com>
2025-08-20 11:55:14 +0200

    formatting

--- dfm_tools/xarray_helpers.py
+++ dfm_tools/xarray_helpers.py
@@ -73,7 +73,8 @@ def preprocess_hisnc(ds):
         if ds_coord_dtype.str.startswith('|S'):
             dim_coord_dict[ds_coord_dim] = ds_coord
     
-    #loop over dimensions and set corresponding coordinates/variables from dim_coord_dict as their index
+    # loop over dimensions and set corresponding coordinates/variables from
+    # dim_coord_dict as their index
     for dim in dim_coord_dict.keys():
         coord = dim_coord_dict[dim]
         # .load() is essential to convert not only first letter of string.
@@ -209,12 +210,14 @@ def merge_meteofiles(file_nc:str,
         Merged meteo dataset.
 
     """
-    #TODO: add ERA5 conversions and features from hydro_tools\ERA5\ERA52DFM.py (except
-    # for varRhoair_alt, request FM support for varying airpressure: https://issuetracker.deltares.nl/browse/UNST-6593)
-    #TODO: add coordinate conversion (only valid for models with multidimensional 
-    # lat/lon variables like HARMONIE and HIRLAM). This should work: ds_reproj = ds.set_crs(4326).to_crs(28992)
-    #TODO: add CMCC etc from gtsmip repos (mainly calendar conversion)
-    #TODO: maybe add renaming like {'salinity':'so', 'water_temp':'thetao'} for hycom
+    # TODO: add ERA5 conversions and features from hydro_tools\ERA5\ERA52DFM.py (except
+    # for varRhoair_alt), request FM support for varying airpressure:
+    # https://issuetracker.deltares.nl/browse/UNST-6593)
+    # TODO: add coordinate conversion (only valid for models with multidimensional 
+    # lat/lon variables like HARMONIE and HIRLAM). This should work:
+    # ds_reproj = ds.set_crs(4326).to_crs(28992)
+    # TODO: add CMCC etc from gtsmip repos (mainly calendar conversion)
+    # TODO: maybe add renaming like {'salinity':'so', 'water_temp':'thetao'} for hycom
 
     if 'chunks' not in kwargs:
         # enable dask chunking
@@ -284,10 +287,14 @@ def merge_meteofiles(file_nc:str,
     
     #convert 0to360 sourcedata to -180to+180
     convert_360to180 = (data_xr['longitude'].to_numpy()>180).any()
-    if convert_360to180: #TODO: make more flexible for models that eg pass -180/+180 crossing (add overlap at lon edges).
+    if convert_360to180:
+        # TODO: make more flexible for models that eg pass -180/+180 crossing (add
+        # overlap at lon edges).
         lon_newvar = (data_xr.coords['longitude'] + 180) % 360 - 180
         # this re-adds original attrs
-        data_xr.coords['longitude'] = lon_newvar.assign_attrs(data_xr['longitude'].attrs)
+        data_xr.coords['longitude'] = lon_newvar.assign_attrs(
+            data_xr['longitude'].attrs
+            )
         data_xr = data_xr.sortby(data_xr['longitude'])
 
     return data_xr
@@ -317,7 +324,10 @@ def convert_meteo_units(data_xr):
             continue
         current_unit = get_unit(ds[varkey_sel])
         new_unit = 'C'
-        print(f'converting {varkey_sel} unit from Kelvin to Celcius: [{current_unit}] to [{new_unit}]')
+        print(
+            'converting {varkey_sel} unit from Kelvin to Celcius: [{current_unit}] to '
+            '[{new_unit}]'
+            )
         ds[varkey_sel] = ds[varkey_sel] - 273.15
         ds[varkey_sel].attrs['units'] = new_unit
     #convert fraction to percentage
@@ -326,26 +336,39 @@ def convert_meteo_units(data_xr):
             continue
         current_unit = get_unit(ds[varkey_sel])
         new_unit = '%' #unit is soms al %
-        print(f'converting {varkey_sel} unit from fraction to percentage: [{current_unit}] to [{new_unit}]')
+        print(
+            f'converting {varkey_sel} unit from fraction to percentage: [{current_unit}'
+            f'] to [{new_unit}]'
+            )
         ds[varkey_sel] = ds[varkey_sel] * 100
         ds[varkey_sel].attrs['units'] = new_unit
     #convert kg/m2/s to mm/day
-    for varkey_sel in ['mer','mtpr']: #mean evaporation rate / mean total precipitation rate
+    for varkey_sel in ['mer','mtpr']:
+        # mean evaporation rate / mean total precipitation rate
         if varkey_sel not in varkeys:
             continue
         current_unit = get_unit(ds[varkey_sel])
         new_unit = 'mm/day'
-        print(f'converting {varkey_sel} unit from kg/m2/s to mm/day: [{current_unit}] to [{new_unit}]')
-        ds[varkey_sel] = ds[varkey_sel] * 86400 # kg/m2/s to mm/day (assuming rho_water=1000)
+        print(
+            f'converting {varkey_sel} unit from kg/m2/s to mm/day: [{current_unit}] to '
+            f'[{new_unit}]'
+            )
+        # kg/m2/s to mm/day (assuming rho_water=1000)
+        ds[varkey_sel] = ds[varkey_sel] * 86400
         ds[varkey_sel].attrs['units'] = new_unit
     #convert J/m2 to W/m2
-    for varkey_sel in ['ssr','strd']: #solar influx (surface_net_solar_radiation) / surface_thermal_radiation_downwards
+    for varkey_sel in ['ssr','strd']:
+        # solar influx (surface_net_solar_radiation)/surface_thermal_radiation_downwards
         if varkey_sel not in varkeys:
             continue
         current_unit = get_unit(ds[varkey_sel])
         new_unit = 'W m**-2'
-        print(f'converting {varkey_sel} unit from J/m2 to W/m2: [{current_unit}] to [{new_unit}]')
-        ds[varkey_sel] = ds[varkey_sel] / 3600 # 3600s/h #TODO: 1W = 1J/s, so does not make sense?
+        print(
+            f'converting {varkey_sel} unit from J/m2 to W/m2: [{current_unit}] to '
+            f'[{new_unit}]'
+            )
+        # 3600s/h #TODO: 1W = 1J/s, so does not make sense?
+        ds[varkey_sel] = ds[varkey_sel] / 3600
         ds[varkey_sel].attrs['units'] = new_unit
     #solar influx increase for beta=6% subtraction in DFM
     if 'ssr' in varkeys:
@@ -367,7 +390,9 @@ def Dataset_varswithdim(ds,dimname):
     # TODO: dit zit ook in xugrid, wordt nu gebruikt in hisfile voorbeeldscript en kan
     # handig zijn, maar misschien die uit xugrid gebruiken?
     if dimname not in ds.dims:
-        raise KeyError(f'dimension {dimname} not in dataset, available are: {list(ds.dims)}')
+        raise KeyError(
+            f'dimension {dimname} not in dataset, available are: {list(ds.dims)}'
+            )
     
     varlist_keep = []
     for varname in ds.variables.keys():
