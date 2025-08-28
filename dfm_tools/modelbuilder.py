bf17a44addc00260147e293b664c81d50be2017d
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2025-08-28 16:02:58 +0200

    1207 improve speed of cmems ini file generation (#1258)
    * add support for filelist to dfmt.cmems_nc_to_ini()
    
    * update whatsnew

--- dfm_tools/modelbuilder.py
+++ dfm_tools/modelbuilder.py
@@ -150,6 +150,13 @@ def cmems_nc_to_ini(
     model results.
     """
     
+    def format_path(path, ncvarname):
+        if isinstance(dir_pattern, list):
+            path = [x.format(ncvarname=ncvarname) for x in path]
+        else:
+            path = path.format(ncvarname=ncvarname)
+        return path
+            
     xr_kwargs = {"join":"exact", "data_vars":"minimal"}
     
     if conversion_dict is None:
@@ -175,7 +182,7 @@ def cmems_nc_to_ini(
             quantity=quan_bnd,
             conversion_dict=conversion_dict,
             )
-        dir_pattern_one = dir_pattern.format(ncvarname=ncvarname)
+        dir_pattern_one = format_path(dir_pattern, ncvarname=ncvarname)
         
         if quan_bnd=="salinitybnd":
             # this also handles temperaturebnd
@@ -187,7 +194,7 @@ def cmems_nc_to_ini(
                 quantity="temperaturebnd",
                 conversion_dict=conversion_dict,
                 )
-            dir_pattern_tem = dir_pattern.format(ncvarname=ncvarname_tem)
+            dir_pattern_tem = format_path(dir_pattern, ncvarname=ncvarname_tem)
             data_xr_tem = xr.open_mfdataset(dir_pattern_tem, **xr_kwargs)
             data_xr["thetao"] = data_xr_tem["thetao"]
             quantity = "nudge_salinity_temperature"
